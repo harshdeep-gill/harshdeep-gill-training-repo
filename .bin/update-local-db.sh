74c6860d7ffad8878f7eb1604f87c254c0c8fbc7
Junaid Bhura <junaid.bhura@travelopia.com>
2024-01-03 11:10:59 +1100

    update db update script

--- .bin/update-local-db.sh
+++ .bin/update-local-db.sh
@@ -6,11 +6,11 @@
 # Usage: sh update-local-db.sh /path/to/quark-expeditions_live_xxxx-xx-xxT19-00-00_UTC_database.sql
 
 echo 'Importing database...'
-pv $1 | mysql -uroot -proot -h 0.0.0.0 tcs
+pv $1 | mysql -uroot -proot -h 0.0.0.0 quark
 
 echo 'Replacing paths...'
 #wp search-replace www.quarkexpeditions local.quarkexpeditions --all-tables
-wp search-replace wp.quarkexpeditions local.quarkexpeditions --all-tables
+wp search-replace staging.quarkexpeditions local.quarkexpeditions --all-tables
 
 echo 'Flushing cache...'
 wp cache flush
@@ -20,5 +20,5 @@ wp plugin deactivate s3-uploads change-wp-admin-login two-factor stream pantheon
 wp plugin activate query-monitor
 
 echo 'Cleaning PII...'
-mysql -u root -proot -h 0.0.0.0 tcs -e 'UPDATE wp_users SET user_email = CONCAT( "user", wp_users.ID, "@travelopia.com" )' # Reset all users' email address.
-mysql -u root -proot -h 0.0.0.0 tcs -e 'UPDATE wp_users SET user_pass="$P$BlKV.RDIfk0iBJeyy0m1e5A.kIleIF."' # All users will have the password "password".
+mysql -u root -proot -h 0.0.0.0 quark -e 'UPDATE wp_users SET user_email = CONCAT( "user", wp_users.ID, "@travelopia.com" )' # Reset all users' email address.
+mysql -u root -proot -h 0.0.0.0 quark -e 'UPDATE wp_users SET user_pass="$P$BlKV.RDIfk0iBJeyy0m1e5A.kIleIF."' # All users will have the password "password".
