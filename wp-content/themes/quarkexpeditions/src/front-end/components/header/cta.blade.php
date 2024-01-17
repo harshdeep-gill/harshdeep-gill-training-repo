aaad2485464622558a8aef2cb7cc141b51a1e2e1
Prithwiraj Ghosh <prithwiraj10@gmail.com>
2024-01-17 13:31:46 +0530

    QE-1 Fix indentation and update markup for header

--- wp-content/themes/quarkexpeditions/src/front-end/components/header/cta.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/header/cta.blade.php
@@ -1,19 +1,19 @@
 @php
-$image_args = [
-	'size' => [
-		'width'   => 120,
-		'height'  => 120,
-	],
-	'transform' => [
-		'crop' => 'fit',
-	],
-];
+	$image_args = [
+		'size' => [
+			'width'   => 120,
+			'height'  => 120,
+		],
+		'transform' => [
+			'crop' => 'fit',
+		],
+	];
 @endphp
 
 <a href="tel:+1-877-585-1235" class="header__cta">
-	<span class="header__cta-avatar">
+	<figure class="header__cta-avatar">
 		<x-image image_id="18" :args="$image_args" />
-	</span>
+	</figure>
 	<span class="header__cta-content">
 		<span class="header__cta-content-text">
 			<x-escape content="Talk to a Polar Expert" />
