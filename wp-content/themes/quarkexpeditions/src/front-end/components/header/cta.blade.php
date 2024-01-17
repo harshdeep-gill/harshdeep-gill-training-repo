84f7deda0dcbb4f282d195d075dd97750f152aca
Prithwiraj Ghosh <prithwiraj10@gmail.com>
2024-01-17 13:15:51 +0530

    Update markup and styling in header component

--- wp-content/themes/quarkexpeditions/src/front-end/components/header/cta.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/header/cta.blade.php
@@ -1,17 +1,26 @@
-<div class="header__cta">
-	<div class="header__cta-avatar">
-		<!-- TODO: Update to dynamic image. -->
-		<img src="https://i.pravatar.cc/55?img=43">
-	</div>
-	<div class="header__cta-content">
+@php
+$image_args = [
+	'size' => [
+		'width'   => 120,
+		'height'  => 120,
+	],
+	'transform' => [
+		'crop' => 'fit',
+	],
+];
+@endphp
+
+<a href="tel:+1-877-585-1235" class="header__cta">
+	<span class="header__cta-avatar">
+		<x-image image_id="18" :args="$image_args" />
+	</span>
+	<span class="header__cta-content">
 		<span class="header__cta-content-text">
 			<x-escape content="Talk to a Polar Expert" />
 		</span>
-		<a href="tel:+1-877-585-1235" class="header__cta-content-phone-number">
+		<span class="header__cta-content-phone-number">
 			<span><x-escape content="+1-877-585-1235" /></span>
-		</a>
-	</div>
-	<a href="tel:+1-877-585-1235" class="header__cta-icon">
-		<x-svg name="call" />
-	</a>
-</div>
+			<x-svg name="call" />
+		</span>
+	</span>
+</a>
