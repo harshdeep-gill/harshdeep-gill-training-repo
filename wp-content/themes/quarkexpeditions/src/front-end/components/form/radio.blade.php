f569b6dd18f2bc90e62e863d672ee5893da109c0
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-01-18 12:07:45 +0530

    QE-2 Remove unnecessary form elements

--- wp-content/themes/quarkexpeditions/src/front-end/components/form/radio.blade.php
+++
@@ -1,8 +0,0 @@
-@props( [
-	'label' => '',
-] )
-
-<x-form.label class="radio">
-	<input type="radio" {{ $attributes->filter( fn ( $value, $key ) => $key !== 'label' ) }}>
-	<x-escape :content="$label"/>
-</x-form.label>
--- wp-content/themes/quarkexpeditions/src/front-end/components/form/toggle.blade.php
+++
@@ -1,9 +0,0 @@
-@props( [
-	'label' => '',
-] )
-
-<x-form.label class="toggle">
-	<input type="checkbox" {{ $attributes->filter( fn ( $value, $key ) => $key !== 'label' ) }}>
-	<span class="toggle__slider"></span>
-	<x-escape :content="$label"/>
-</x-form.label>
