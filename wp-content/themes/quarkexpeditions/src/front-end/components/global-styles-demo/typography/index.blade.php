5fcbd0e635e8936ff6715ff9ecb8b613fa2107fe
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-01-19 11:14:04 +0530

    QE-2 Fix typography demo styling

--- wp-content/themes/quarkexpeditions/src/front-end/components/global-styles-demo/typography/index.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/global-styles-demo/typography/index.blade.php
@@ -1,260 +1,266 @@
-<div class="typography__fonts">
-	<div class="typography__headlines">
-		<div class="typography__row">
-			<div class="typography__style-type">
-				<div class="typography__style-details">
-					<p>H1</p>
-				</div>
-				<div class="typography__style-box">
-					<div class="typography__style-specification">
-						<p>L: <span>Source Serif 4 400</span></p>
-						<p>Font Size: <span>56</span></p>
-						<p>Line Height: <span>1.28</span></p>
-					</div>
-					<div class="typography__style-specification">
-						<p>M: <span>Source Serif 4 400</span></p>
-						<p>Font Size: <span>48</span></p>
-						<p>Line Height: <span>1.25</span></p>
-					</div>
-					<div class="typography__style-specification">
-						<p>S: <span>Source Serif 4 400</span></p>
-						<p>Font Size: <span>36</span></p>
-						<p>Line Height: <span>1.22</span></p>
-					</div>
+<div class="typography">
+	<div class="typography__row typography-spacing">
+		<div class="typography__view">
+			<h1>H1 - Headline</h1>
+		</div>
+		<div class="typography__details">
+			<div class="typography__style-specification">
+				<p>L&nbsp;<span>Source Serif 4 400</span></p>
+				<p>Font Size: <span>56</span></p>
+				<p>Line Height: <span>1.28</span></p>
+			</div>
+			<div class="typography__style-specification">
+				<p>M&nbsp;<span>Source Serif 4 400</span></p>
+				<p>Font Size: <span>48</span></p>
+				<p>Line Height: <span>1.25</span></p>
+			</div>
+			<div class="typography__style-specification">
+				<p>S&nbsp;<span>Source Serif 4 400</span></p>
+				<p>Font Size: <span>36</span></p>
+				<p>Line Height: <span>1.22</span></p>
+			</div>
+			</div>
+		</div>
 
-				</div>
-			</div>
-			<div class="typography__style-example">
-				<h1>H1 Regular Headline</h1>
-			</div>
+	<div class="typography__row typography-spacing">
+		<div class="typography__view">
+			<h2>H2 - Headline</h2>
 		</div>
-		<div class="typography__row">
-			<div class="typography__style-type">
-				<div class="typography__style-details">
-					<p>H2</p>
-				</div>
-				<div class="typography__style-box">
-					<div class="typography__style-specification">
-						<p>L: <span>Source Serif 4 400</span></p>
-						<p>Font Size: <span>48</span></p>
-						<p>Line Height: <span>1</span></p>
-					</div>
-					<div class="typography__style-specification">
-						<p>M: <span>Source Serif 4 400</span></p>
-						<p>Font Size: <span>32</span></p>
-						<p>Line Height: <span>1.25</span></p>
-					</div>
-					<div class="typography__style-specification">
-						<p>S: <span>Source Serif 4 400</span></p>
-						<p>Font Size: <span>28</span></p>
-						<p>Line Height: <span>1.28</span></p>
-					</div>
-				</div>
+		<div class="typography__details">
+			<div class="typography__style-specification">
+				<p>L&nbsp;<span>Source Serif 4 400</span></p>
+				<p>Font Size: <span>48</span></p>
+				<p>Line Height: <span>1</span></p>
 			</div>
-			<div class="typography__style-example">
-				<h2>H2 Regular Headline</h2>
+			<div class="typography__style-specification">
+				<p>M&nbsp;<span>Source Serif 4 400</span></p>
+				<p>Font Size: <span>32</span></p>
+				<p>Line Height: <span>1.25</span></p>
 			</div>
-		</div>
-		<div class="typography__row">
-			<div class="typography__style-type">
-				<div class="typography__style-details">
-					<p>H3</p>
-				</div>
-				<div class="typography__style-box">
-					<div class="typography__style-specification">
-						<p>L: <span>Source Serif 4 400</span></p>
-						<p>Font Size: <span>32</span></p>
-						<p>Line Height: <span>1.25</span></p>
-					</div>
-					<div class="typography__style-specification">
-						<p>M: <span>Source Serif 4 400</span></p>
-						<p>Font Size: <span>28</span></p>
-						<p>Line Height: <span>1.28</span></p>
-					</div>
-					<div class="typography__style-specification">
-						<p>S: <span>Source Serif 4 400</span></p>
-						<p>Font Size: <span>24</span></p>
-						<p>Line Height: <span>1.33</span></p>
-					</div>
-				</div>
-			</div>
-			<div class="typography__style-example">
-				<h3>H3 Regular Headline</h3>
-			</div>
-		</div>
-		<div class="typography__row">
-			<div class="typography__style-type">
-				<div class="typography__style-details">
-					<p>H4</p>
-				</div>
-				<div class="typography__style-box">
-					<div class="typography__style-specification">
-						<p>L: <span>Nunito Sans 700</span></p>
-						<p>Font Size: <span>24</span></p>
-						<p>Line Height: <span>1.33</span></p>
-					</div>
-				</div>
-				<div class="typography__style-box">
-					<div class="typography__style-specification">
-						<p>M: <span>Nunito Sans 700</span></p>
-						<p>Font Size: <span>22</span></p>
-						<p>Line Height: <span>1.36</span></p>
-					</div>
-				</div>
-				<div class="typography__style-box">
-					<div class="typography__style-specification">
-						<p>S: <span>Nunito Sans 700</span></p>
-						<p>Font Size: <span>20</span></p>
-						<p>Line Height: <span>1.40</span></p>
-					</div>
-				</div>
-			</div>
-			<div class="typography__style-example">
-				<h4>H4 Bold Headline</h4>
-			</div>
-		</div>
-		<div class="typography__row">
-			<div class="typography__style-type">
-				<div class="typography__style-details">
-					<p>H5</p>
-				</div>
-				<div class="typography__style-box">
-					<div class="typography__style-specification">
-						<p>L: <span>Nunito Sans 700</span></p>
-						<p>Font Size: <span>20</span></p>
-						<p>Line Height: <span>1.4</span></p>
-					</div>
-					<div class="typography__style-specification">
-						<p>M: <span>Nunito Sans 700</span></p>
-						<p>Font Size: <span>18</span></p>
-						<p>Line Height: <span>1.44</span></p>
-					</div>
-					<div class="typography__style-specification">
-						<p>S: <span>Nunito Sans 700</span></p>
-						<p>Font Size: <span>18</span></p>
-						<p>Line Height: <span>1.33</span></p>
-					</div>
-				</div>
-			</div>
-			<div class="typography__style-example">
-				<h5>H5 Bold Headline</h5>
+			<div class="typography__style-specification">
+				<p>S&nbsp;<span>Source Serif 4 400</span></p>
+				<p>Font Size: <span>28</span></p>
+				<p>Line Height: <span>1.28</span></p>
 			</div>
 		</div>
 	</div>
 
-	<div class="typography__text">
-		<div class="typography__row">
-			<div class="typography__style-type">
-				<div class="typography__style-details">
-					<p>Body Text Std</p>
-				</div>
-				<div class="typography__style-box">
-					<div class="typography__style-specification">
-						<p>L, M, S: <span>Nunito Sans 400</span></p>
-						<p>Font Size:<span>16</span></p>
-						<p>Line Height:<span>1.75</span></p>
-					</div>
-				</div>
+	<div class="typography__row typography-spacing">
+		<div class="typography__view">
+			<h3>H3 - Headline</h3>
+		</div>
+		<div class="typography__details">
+			<div class="typography__style-specification">
+				<p>L&nbsp;<span>Source Serif 4 400</span></p>
+				<p>Font Size: <span>32</span></p>
+				<p>Line Height: <span>1.25</span></p>
 			</div>
-			<div class="typography__style-example">
-				<p class="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-					lobortis tincidunt nunc, et volutpat orci sagittis in. Nunc sem risus, pulvinar in gravida in,
-					sodales at turpis. Donec malesuada semper est, id lobortis sapien lobortis ac. Nullam pretium
-					cursus ornare. Proin hendrerit ultricies placerat. Praesent eu neque convallis, bibendum purus
-					eu, finibus dui. Nulla facilisi.</p>
+			<div class="typography__style-specification">
+				<p>M&nbsp;<span>Source Serif 4 400</span></p>
+				<p>Font Size: <span>28</span></p>
+				<p>Line Height: <span>1.28</span></p>
+			</div>
+			<div class="typography__style-specification">
+				<p>S&nbsp;<span>Source Serif 4 400</span></p>
+				<p>Font Size: <span>24</span></p>
+				<p>Line Height: <span>1.33</span></p>
 			</div>
 		</div>
-		<div class="typography__row">
-			<div class="typography__style-type">
-				<div class="typography__style-details">
-					<p>Body Text Small</p>
-				</div>
-				<div class="typography__style-box">
-					<div class="typography__style-specification">
-						<p>L, M, S: <span>Nunito Sans 400</span></p>
-						<p>Font Size:<span>14</span></p>
-						<p>Line Height:<span>1.57</span></p>
+	</div>
 
-					</div>
-				</div>
-
+	<div class="typography__row typography-spacing">
+		<div class="typography__view">
+			<h4>H4 - Headline</h4>
 			</div>
-			<div class="typography__style-example">
-				<p class="body-small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-					lobortis tincidunt nunc, et volutpat orci sagittis in. Nunc sem risus, pulvinar in gravida in,
-					sodales at turpis. Donec malesuada semper est, id lobortis sapien lobortis ac. Nullam pretium
-					cursus ornare. Proin hendrerit ultricies placerat. Praesent eu neque convallis, bibendum purus
-					eu, finibus dui. Nulla facilisi.</p>
+		<div class="typography__details">
+			<div class="typography__style-specification">
+				<p>L&nbsp;<span>Nunito Sans 700</span></p>
+				<p>Font Size: <span>24</span></p>
+				<p>Line Height: <span>1.33</span></p>
 			</div>
-		</div>
-		<div class="typography__row">
-			<div class="typography__style-type">
-				<div class="typography__style-details">
-					<p>Body Text Small Bold</p>
-				</div>
-				<div class="typography__style-box">
-					<div class="typography__style-specification">
-						<p>L, M, S: <span>Nunito Sans 700</span></p>
-						<p>Font Size:<span>14</span></p>
-						<p>Line Height:<span>1.57</span></p>
-
-					</div>
-				</div>
-
+			<div class="typography__style-specification">
+				<p>M&nbsp;<span>Nunito Sans 700</span></p>
+				<p>Font Size: <span>22</span></p>
+				<p>Line Height: <span>1.36</span></p>
 			</div>
-			<div class="typography__style-example">
-				<p class="body-small--bold">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-					lobortis tincidunt nunc, et volutpat orci sagittis in. Nunc sem risus, pulvinar in gravida in,
-					sodales at turpis. Donec malesuada semper est, id lobortis sapien lobortis ac. Nullam pretium
-					cursus ornare. Proin hendrerit ultricies placerat. Praesent eu neque convallis, bibendum purus
-					eu, finibus dui. Nulla facilisi.</p>
+			<div class="typography__style-specification">
+				<p>S&nbsp;<span>Nunito Sans 700</span></p>
+				<p>Font Size: <span>20</span></p>
+				<p>Line Height: <span>1.40</span></p>
 			</div>
 		</div>
-		<div class="typography__row">
-			<div class="typography__style-type">
-				<div class="typography__style-details">
-					<p>Caption</p>
-				</div>
-				<div class="typography__style-box">
-					<div class="typography__style-specification">
-						<p>L, M, S: <span>Nunito Sans 400</span></p>
-						<p>Font Size:<span>14</span></p>
-						<p>Line Height:<span>1.57</span></p>
+	</div>
 
-						<p>Style:<span>italic</span></p>
+	<div class="typography__row typography-spacing">
+		<div class="typography__view">
+			<h5>H5 - Headline</h5>
+		</div>
+		<div class="typography__details">
+					<div class="typography__style-specification">
+						<p>L&nbsp;<span>Nunito Sans 700</span></p>
+						<p>Font Size: <span>20</span></p>
+						<p>Line Height: <span>1.4</span></p>
+					</div>
+					<div class="typography__style-specification">
+						<p>M&nbsp;<span>Nunito Sans 700</span></p>
+						<p>Font Size: <span>18</span></p>
+						<p>Line Height: <span>1.44</span></p>
+					</div>
+					<div class="typography__style-specification">
+						<p>S&nbsp;<span>Nunito Sans 700</span></p>
+						<p>Font Size: <span>18</span></p>
+						<p>Line Height: <span>1.33</span></p>
 					</div>
 				</div>
-
+	</div>
+	<div class="typography__row typography-spacing">
+		<div class="typography__view">
+			<h6>Body Text Standard</h6>
+			<p>Lorem ipsum dolor sit, consectetur adipiscing elit. Vestibulum sit amet massa eget orciiaculis
+				ultricies alas non ante. Vivamus tempor purus quam, ac iaculis diam. Sed congue justo ac nisl
+				efficitur ultricies. Sed at lobortis eros. Pellentesque metus arcu, sodales non ullamcorper sed,
+				ullamcorper a sapien tempor.</p>
+			<p>Donec malesuada semper est, id lobortis sapien lobortis ac. Nullam pretium cursus ornare. Proin
+				hendrerit ultricies placerat. Praesent eu neque convallis, bibendum purus eu, finibus dui. Nulla
+				facilisi.</p>
+		</div>
+		<div class="typography__details">
+			<div class="typography__style-specification">
+				<p>L&nbsp;<span>Nunito Sans 400</span></p>
+				<p>Font Size:<span>16</span></p>
+				<p>Line Height:<span>1.75</span></p>
 			</div>
-			<div class="typography__style-example">
-				<p class="caption">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-					lobortis tincidunt nunc, et volutpat orci sagittis in. Nunc sem risus, pulvinar in gravida in,
-					sodales at turpis. Donec malesuada semper est, id lobortis sapien lobortis ac. Nullam pretium
-					cursus ornare. Proin hendrerit ultricies placerat. Praesent eu neque convallis, bibendum purus
-					eu, finibus dui. Nulla facilisi.</p>
+			<div class="typography__style-specification">
+				<p>M&nbsp;<span>Nunito Sans 400</span></p>
+				<p>Font Size:<span>16</span></p>
+				<p>Line Height:<span>1.75</span></p>
+			</div>
+			<div class="typography__style-specification">
+				<p>S&nbsp;<span>Nunito Sans 400</span></p>
+				<p>Font Size:<span>16</span></p>
+				<p>Line Height:<span>1.75</span></p>
 			</div>
 		</div>
-		<div class="typography__row">
-			<div class="typography__style-type">
-				<div class="typography__style-details">
-					<p>Overline</p>
-				</div>
-				<div class="typography__style-box">
-					<div class="typography__style-specification">
-						<p>L, M, S: <span>Nunito Sans 400</span></p>
-						<p>Font Size:<span>14</span></p>
-						<p>Line Height:<span>1.57</span></p>
-						<p>Transform:<span>Uppercase</span></p>
-					</div>
-				</div>
-
+	</div>
+	<div class="typography__row typography-spacing">
+		<div class="typography__view">
+			<h6>Body Text Small</h6>
+			<p class="body-small">Lorem ipsum dolor sit, consectetur adipiscing elit. Vestibulum sit amet
+				massa
+				eget orci iaculis ultricies alas non ante. Vivamus tempor purus quam, ac iaculis diam. Sed
+				congue
+				justo ac nisl efficitur ultricies. Sed at lobortis eros. Pellentesque metus arcu, sodales non
+				ullamcorper sed, ullamcorper a sapien tempor.</p>
+			<p class="body-small">Donec malesuada semper est, id lobortis sapien lobortis ac. Nullam
+				pretium
+				cursus ornare. Proin hendrerit ultricies placerat. Praesent eu neque convallis, bibendum purus
+				eu,
+				finibus dui. Nulla facilisi.</p>
 			</div>
-			<div class="typography__style-example">
-				<p class="overline">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-					lobortis tincidunt nunc, et volutpat orci sagittis in. Nunc sem risus, pulvinar in gravida in,
-					sodales at turpis. Donec malesuada semper est, id lobortis sapien lobortis ac. Nullam pretium
+		<div class="typography__details">
+			<div class="typography__style-specification">
+				<p>L&nbsp;<span>Nunito Sans 400</span></p>
+				<p>Font Size:<span>14</span></p>
+				<p>Line Height:<span>1.57</span></p>
+			</div>
+			<div class="typography__style-specification">
+				<p>M&nbsp;<span>Nunito Sans 400</span></p>
+				<p>Font Size:<span>14</span></p>
+				<p>Line Height:<span>1.57</span></p>
+				</div>
+			<div class="typography__style-specification">
+				<p>S&nbsp;<span>Nunito Sans 400</span></p>
+				<p>Font Size:<span>14</span></p>
+				<p>Line Height:<span>1.57</span></p>
+			</div>
+		</div>
+	</div>
+
+	<div class="typography__row typography-spacing">
+		<div class="typography__view">
+			<h6>Body Text Small Bold</h6>
+			<p class="body-small--bold">Lorem ipsum dolor sit, consectetur adipiscing elit. Vestibulum sit amet
+				massa
+				eget orci iaculis ultricies alas non ante. Vivamus tempor purus quam, ac iaculis diam. Sed
+				congue
+				justo ac nisl efficitur ultricies. Sed at lobortis eros. Pellentesque metus arcu, sodales non
+				ullamcorper sed, ullamcorper a sapien tempor.</p>
+			<p class="body-small--bold">Donec malesuada semper est, id lobortis sapien lobortis ac. Nullam pretium
 					cursus ornare. Proin hendrerit ultricies placerat. Praesent eu neque convallis, bibendum purus
-					eu, finibus dui. Nulla facilisi.</p>
+				eu,
+				finibus dui. Nulla facilisi.</p>
+			</div>
+		<div class="typography__details">
+			<div class="typography__style-specification">
+				<p>L&nbsp;<span>Nunito Sans 700</span></p>
+				<p>Font Size:<span>14</span></p>
+				<p>Line Height:<span>1.57</span></p>
+			</div>
+			<div class="typography__style-specification">
+				<p>M&nbsp;<span>Nunito Sans 700</span></p>
+				<p>Font Size:<span>14</span></p>
+				<p>Line Height:<span>1.57</span></p>
+				</div>
+			<div class="typography__style-specification">
+				<p>S&nbsp;<span>Nunito Sans 700</span></p>
+				<p>Font Size:<span>14</span></p>
+				<p>Line Height:<span>1.57</span></p>
+			</div>
+		</div>
+	</div>
+
+	<div class="typography__row typography-spacing">
+		<div class="typography__view">
+			<h6>Caption</h6>
+			<p class="caption">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+			</div>
+		<div class="typography__details">
+			<div class="typography__style-specification">
+				<p>L&nbsp;<span>Nunito Sans 400</span></p>
+				<p>Font Size:<span>14</span></p>
+				<p>Line Height:<span>1.57</span></p>
+
+				<p>Style:<span>italic</span></p>
+				</div>
+			<div class="typography__style-specification">
+				<p>M&nbsp;<span>Nunito Sans 400</span></p>
+				<p>Font Size:<span>14</span></p>
+				<p>Line Height:<span>1.57</span></p>
+
+				<p>Style:<span>italic</span></p>
+				</div>
+			<div class="typography__style-specification">
+				<p>S&nbsp;<span>Nunito Sans 400</span></p>
+				<p>Font Size:<span>14</span></p>
+				<p>Line Height:<span>1.57</span></p>
+				<p>Style:<span>italic</span></p>
+			</div>
+		</div>
+	</div>
+
+	<div class="typography__row typography-spacing">
+		<div class="typography__view">
+			<h6>Overline</h6>
+			<p class="overline">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+			</div>
+		<div class="typography__details">
+			<div class="typography__style-specification">
+				<p>L&nbsp;<span>Nunito Sans 400</span></p>
+				<p>Font Size:<span>14</span></p>
+				<p>Line Height:<span>1.57</span></p>
+				<p>Transform:<span>Uppercase</span></p>
+			</div>
+			<div class="typography__style-specification">
+				<p>M&nbsp;<span>Nunito Sans 400</span></p>
+				<p>Font Size:<span>14</span></p>
+				<p>Line Height:<span>1.57</span></p>
+				<p>Transform:<span>Uppercase</span></p>
+				</div>
+			<div class="typography__style-specification">
+				<p>S&nbsp;<span>Nunito Sans 400</span></p>
+				<p>Font Size:<span>14</span></p>
+				<p>Line Height:<span>1.57</span></p>
+				<p>Transform:<span>Uppercase</span></p>
 			</div>
 		</div>
 	</div>
