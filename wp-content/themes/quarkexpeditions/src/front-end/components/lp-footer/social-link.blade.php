a94d630d806e79e2e771e52cd94a4ca313d28720
Prithwiraj Ghosh <prithwiraj10@gmail.com>
2024-01-24 17:23:37 +0530

    QE-10 Minor code updates

--- wp-content/themes/quarkexpeditions/src/front-end/components/lp-footer/social-link.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/lp-footer/social-link.blade.php
@@ -3,7 +3,13 @@
 	'url'  => '#',
 ] )
 
+@php
+	if ( empty( $type ) ) {
+		return;
+	}
+@endphp
 
-<a href="{{ $url }}" class="lp-footer__social-link" title="{{ $type }}">
+
+<a href="{{ $url }}" class="lp-footer__social-link" title="{{ $type }}" target="_blank" rel="nofollow noopener noreferrer">
 	<x-svg name="social/{{ $type }}" />
 </a>
