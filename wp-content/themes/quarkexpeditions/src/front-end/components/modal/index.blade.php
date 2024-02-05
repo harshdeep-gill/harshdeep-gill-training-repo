580df070e937a4fa5798b721e6b18d6e00a9d598
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-02-05 20:54:51 +0530

    QE-3 refactor Modal stylesheet

--- wp-content/themes/quarkexpeditions/src/front-end/components/modal/index.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/modal/index.blade.php
@@ -36,7 +36,7 @@
 		<div class="modal__body">
 			{!! $slot !!}
 		</div>
-		@if ( $form_id )
+		@if ( ! empty( $form_id ) )
 			<footer class="modal__footer">
 				<x-form.buttons>
 					<x-form.submit form="{{ $form_id }}">Request a Quote</x-form.submit>
