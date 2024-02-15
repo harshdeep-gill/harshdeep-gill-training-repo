6bc20c23778bbf119145afdc9faa4f1fa22fd15b
Prithwiraj Ghosh <prithwiraj10@gmail.com>
2024-02-15 23:01:47 +0530

    QE-50 feat: add collage block and related styling changes

--- wp-content/themes/quarkexpeditions/inc/blocks.php
+++ wp-content/themes/quarkexpeditions/inc/blocks.php
@@ -35,6 +35,7 @@ function register_blocks(): void {
 		'InquiryForm'     => 'inquiry-form.php',
 		'LogoGrid'        => 'logo-grid.php',
 		'LPFooter'        => 'lp-footer.php',
+		'Collage'         => 'collage.php',
 	];
 
 	// Register blocks.
--- wp-content/themes/quarkexpeditions/src/editor/blocks/index.ts
+++ wp-content/themes/quarkexpeditions/src/editor/blocks/index.ts
@@ -15,6 +15,7 @@ import * as hero from './hero';
 import * as inquiryForm from './inquiry-form';
 import * as logoGrid from './logo-grid';
 import * as lpFooter from './lp-footer';
+import * as collage from './collage';
 
 /**
  * Add blocks.
@@ -29,6 +30,7 @@ const blocks = [
 	inquiryForm,
 	logoGrid,
 	lpFooter,
+	collage,
 ];
 
 /**
