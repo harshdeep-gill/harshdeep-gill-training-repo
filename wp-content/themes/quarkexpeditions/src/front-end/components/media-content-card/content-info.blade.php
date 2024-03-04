5498e49f56a396ce4849d9496ce0db3679447823
Prithwiraj Ghosh <prithwiraj10@gmail.com>
2024-03-04 18:17:46 +0530

    QE-88 Add maybe-link component in content-info and styling changes

--- wp-content/themes/quarkexpeditions/src/front-end/components/media-content-card/content-info.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/media-content-card/content-info.blade.php
@@ -4,9 +4,9 @@
 	'url'   => '',
 ] )
 
-<a href="{{ $url }}" class="media-content-card__content-info">
+<x-maybe-link href="{{ $url }}" fallback_tag="div" class="media-content-card__content-info">
 	<span class="media-content-card__content-info-label"><x-escape :content="$label" /></span>
 	<span class="media-content-card__content-info-value"><x-escape :content="$value" /></span>
-</a>
+</x-maybe-link>
 
 
