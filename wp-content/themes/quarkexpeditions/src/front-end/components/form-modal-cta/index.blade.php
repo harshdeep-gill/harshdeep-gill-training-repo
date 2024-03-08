52ffd3a3d8da79d47e9b9b71f1b6ab1b4d1f465a
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-03-09 02:50:08 +0530

    QE-86 Extract form-modal-cta

---
+++ wp-content/themes/quarkexpeditions/src/front-end/components/form-modal-cta/index.blade.php
@@ -0,0 +1,29 @@
+@props( [
+	'class'          => '',
+	'form_id'        => '',
+	'thank_you_page' => '',
+] )
+
+@php
+	if ( empty( $slot ) || empty( $form_id ) ) {
+		return;
+	}
+
+	$modal_id = $form_id . '-modal';
+
+	$classes = [ 'form-modal-cta' ];
+
+	if ( ! empty( $class ) ) {
+		$classes[] = $class;
+	}
+@endphp
+
+<x-modal.modal-open @class( $classes ) :modal_id="$modal_id">
+	<x-content :content="$slot" />
+</x-modal.modal-open>
+
+@switch( $form_id )
+	@case( 'inquiry-form' )
+		<x-inquiry-form.modal thank_you_page="{{ $thank_you_page }}" />
+		@break
+@endswitch
