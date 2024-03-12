60304b36590b66cabed716893bae36c39559416e
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-03-12 16:00:52 +0530

    QE-86 Add hero variant demo

--- wp-content/themes/quarkexpeditions/src/front-end/layouts/components.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/layouts/components.blade.php
@@ -82,6 +82,25 @@
 		</x-review-cards>
 	</x-component-demo>
 
+	<x-component-demo :keys="[ 'hero', 'hero-refactor' ]">
+		<x-hero text_align="center" :immersive="false">
+			<x-hero.image image_id="26" />
+			<x-hero.content>
+				<x-hero.left>
+					<x-hero.title-container>
+						<x-hero.overline>Antarctic 2024</x-hero.overline>
+						<x-hero.title title="Antarctic Voyages" />
+						<x-hero.sub-title title="Choose the Leader in Polar Adventure" />
+					</x-hero.title-container>
+					<x-icon-badge class="hero__tag" background_color="attention-100" icon="alert" text="Limited Cabins Available" />
+					<x-hero.form-modal-cta>Get a Digital Brochure</x-hero.form-modal-cta>
+				</x-hero.left>
+				<x-hero.right>
+				</x-hero.right>
+			</x-hero.content>
+		</x-hero>
+	</x-component-demo>
+
 	<x-component-demo :keys="[ 'global', 'color-palette' ]">
 		<x-section title="Color Palette" heading_level="2">
 			<x-global-styles-demo.color-palette />
