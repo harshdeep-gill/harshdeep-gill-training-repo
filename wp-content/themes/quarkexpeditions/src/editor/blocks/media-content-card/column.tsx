2a6a1e9ebb87ed9315e39726af9d1cadb7e0d5b4
Prithwiraj Ghosh <prithwiraj10@gmail.com>
2024-03-13 09:48:50 +0530

    QE-91 chore: remove extra import

--- wp-content/themes/quarkexpeditions/src/editor/blocks/media-content-card/column.tsx
+++ wp-content/themes/quarkexpeditions/src/editor/blocks/media-content-card/column.tsx
@@ -2,7 +2,7 @@
  * WordPress dependencies.
  */
 import { __ } from '@wordpress/i18n';
-import { BlockConfiguration, registerBlockType } from '@wordpress/blocks';
+import { BlockConfiguration } from '@wordpress/blocks';
 import {
 	InnerBlocks,
 	useBlockProps,
