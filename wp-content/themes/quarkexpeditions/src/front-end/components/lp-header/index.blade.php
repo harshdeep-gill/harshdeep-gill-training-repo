0500ecf243e82225f80dd68133926566ffa334ee
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-03-25 15:45:51 +0530

    QE-107 Add dark-mode prop

--- wp-content/themes/quarkexpeditions/src/front-end/components/lp-header/index.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/lp-header/index.blade.php
@@ -3,11 +3,23 @@
 	'tc_image_id'  => 0,
 	'phone_number' => '',
 	'cta_text'     => '',
+	'dark_mode'    => false,
 ] )
 
-<quark-lp-header class="lp-header full-width">
+@php
+	$classes = [ 'lp-header', 'full-width' ];
+
+	if ( true === $dark_mode ) {
+		$classes[] = 'color-context--dark';
+	}
+@endphp
+
+<quark-lp-header @class( $classes )>
 	<div class="lp-header__wrap">
-		<x-lp-header.logo :url="$logo_url" />
+		<x-lp-header.logo
+			:url="$logo_url"
+			:dark_mode="true"
+		/>
 		<x-lp-header.cta
 			:image_id="$tc_image_id"
 			:phone_number="$phone_number"
--- wp-content/themes/quarkexpeditions/src/front-end/components/lp-header/logo.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/lp-header/logo.blade.php
@@ -1,9 +1,18 @@
 @props( [
-	'url' => '',
+	'url'       => '',
+	'dark_mode' => false,
 ] )
 
+@php
+	$logo_name = 'logo';
+
+	if ( true === $dark_mode ) {
+		$logo_name = $logo_name . '-dark';
+	}
+@endphp
+
 <a href="{{ $url }}" class="lp-header__logo">
-	<x-svg name="logo" />
+	<x-svg name="{{ $logo_name }}" />
 </a>
 
 {{--Logo displayed on compact version of lp-header.--}}
