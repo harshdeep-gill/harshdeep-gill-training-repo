5dc2db4ce904a03d04a41fe95c13375b7ee6496d
Prithwiraj Ghosh <prithwiraj10@gmail.com>
2024-04-03 12:42:08 +0530

    QE-134 feat: Add Contact Cover Card Block

---
+++ wp-content/themes/quarkexpeditions/src/front-end/components/parts/contact-cover-card.blade.php
@@ -0,0 +1,41 @@
+@props( [
+	'image_id'   => 0,
+	'content'    => [],
+] )
+
+@php
+	if ( empty( $image_id ) || empty( $content ) ) {
+		return;
+	}
+@endphp
+
+<x-contact-cover-card>
+	<x-contact-cover-card.image :image_id="$image_id ?? ''" />
+	<x-contact-cover-card.content>
+		@foreach ( $content as $item )
+			@if ( 'title' === $item['type'] )
+				<x-contact-cover-card.title :title="$item['title'] ?? ''" />
+			@endif
+
+			@if ( 'description' === $item['type'] )
+				<x-contact-cover-card.description>
+					{!! $item['description'] ?? '' !!}
+				</x-contact-cover-card.description>
+			@endif
+
+			@if ( 'contact-info' === $item['type'] )
+				<x-contact-cover-card.contact-info>
+					@if( ! empty( $item['children'] ) )
+						@foreach ( $item['children'] as $info_item )
+							<x-contact-cover-card.contact-info-item
+								:label="$info_item['label'] ?? ''"
+								:value="$info_item['value'] ?? ''"
+								:url="$info_item['url'] ?? ''"
+							/>
+						@endforeach
+					@endif
+				</x-contact-cover-card.contact-info>
+			@endif
+		@endforeach
+	</x-contact-cover-card.content>
+</x-contact-cover-card>
