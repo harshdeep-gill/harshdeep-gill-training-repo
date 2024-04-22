d186e8c456fb66612d517b06874f5550ff121a8a
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-04-22 16:14:47 +0530

    QE-138 Fix typo

--- wp-content/themes/quarkexpeditions/src/front-end/components/hero/description.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/hero/description.blade.php
@@ -9,7 +9,7 @@
 
 	$classes = [ 'hero__description' ];
 
-	if ( ! empty( $text_color ) 'white' === $text_color ) {
+	if ( ! empty( $text_color ) && 'white' === $text_color ) {
 		$classes[] = 'color-context--dark';
 	}
 @endphp
