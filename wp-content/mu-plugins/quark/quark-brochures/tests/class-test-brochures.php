77ce310cd91d7bec82d96f18c05152f3c796b368
Jignesh Nakrani <jignesh.nakrani@rtcamp.com>
2024-05-01 10:59:08 +0530

    QE-213 Add testCase for brochure/get method

--- wp-content/mu-plugins/quark/quark-brochures/tests/class-test-brochures.php
+++ wp-content/mu-plugins/quark/quark-brochures/tests/class-test-brochures.php
@@ -8,6 +8,7 @@
 namespace Quark\Brochures\Tests;
 
 use WP_UnitTestCase;
+use WP_Post;
 
 use const Quark\Brochures\POST_TYPE;
 
@@ -38,5 +39,43 @@ class Test_Brochures extends WP_UnitTestCase {
 	public function test_register_brochures_post_type(): void {
 		// Test if post type is actually registered.
 		$this->assertTrue( post_type_exists( POST_TYPE ) );
+	}
+
+	/**
+	 * Test getting a base.
+	 *
+	 * @covers \Quark\Brochures\get()
+	 *
+	 * @return void
+	 */
+	public function test_get(): void {
+		// Create post.
+		$post_1 = $this->factory()->post->create_and_get(
+			[
+				'post_type'   => POST_TYPE,
+				'post_title'  => 'Test Post',
+				'post_status' => 'publish',
+				'meta_input'  => [
+					'meta_1' => 'value_1',
+					'meta_2' => 'value_2',
+				],
+			]
+		);
+
+		// Make sure post is created.
+		$this->assertTrue( $post_1 instanceof WP_Post );
+
+		// Test getting post.
+		$this->assertEquals(
+			[
+				'post'           => $post_1,
+				'post_thumbnail' => 0,
+				'post_meta'      => [
+					'meta_1' => 'value_1',
+					'meta_2' => 'value_2',
+				],
+			],
+			\Quark\Brochures\get( $post_1->ID )
+		);
 	}
 }
