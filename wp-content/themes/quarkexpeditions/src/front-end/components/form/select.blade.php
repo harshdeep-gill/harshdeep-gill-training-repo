aad335745bf89dc8816481e1d841e5b99deec94b
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-05-02 11:38:32 +0530

    QE-187 Remove multi-select attribute

--- wp-content/themes/quarkexpeditions/src/front-end/components/form/select.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/form/select.blade.php
@@ -1,7 +1,6 @@
 @props( [
-	'id'       => '',
-	'label'    => '',
-	'multiple' => false,
+	'id'    => '',
+	'label' => '',
 ] )
 
 @php
@@ -18,25 +17,15 @@
 @endif
 
 <tp-multi-select
-	multiple="{{ $multiple ? 'yes' : 'no' }}"
-	close-on-select="{{ $multiple ? 'no' : 'yes' }}"
+	multiple="no"
+	close-on-select="yes"
 	{{ $attributes->filter( fn ( $value, $key ) => $key !== 'label' )->merge( [ 'id' => $id ] ) }}
 >
 	<tp-multi-select-field>
-		@if ( $multiple )
-			<tp-multi-select-pills></tp-multi-select-pills>
-			<tp-multi-select-search>
-				<input placeholder="Select...">
-			</tp-multi-select-search>
-		@else
-			<tp-multi-select-placeholder>Select...</tp-multi-select-placeholder>
-			<tp-multi-select-status format="$value"></tp-multi-select-status>
-		@endif
+		<tp-multi-select-placeholder>Select...</tp-multi-select-placeholder>
+		<tp-multi-select-status format="$value"></tp-multi-select-status>
 	</tp-multi-select-field>
 	<tp-multi-select-options>
-		@if ( $multiple )
-			<tp-multi-select-select-all select-text="Select All" unselect-text="Un-Select All">Select All</tp-multi-select-select-all>
-		@endif
 		{!! $slot !!}
 	</tp-multi-select-options>
 </tp-multi-select>
