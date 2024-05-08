175ee63bd2d84c7c66eaf965ae348dd98a912049
Jignesh Nakrani <jignesh.nakrani@rtcamp.com>
2024-05-08 18:10:52 +0530

    QE-192 Register Exclusion Sets CPT

---
+++ wp-content/mu-plugins/quark/quark-exclusion-sets/inc/namespace.php
@@ -0,0 +1,213 @@
+<?php
+/**
+ * Namespace functions.
+ *
+ * @package quark-exclusion-sets
+ */
+
+namespace Quark\ExclusionSets;
+
+use WP_Post;
+
+const POST_TYPE   = 'qrk_exclusion_set';
+const CACHE_KEY   = POST_TYPE;
+const CACHE_GROUP = POST_TYPE;
+
+/**
+ * Bootstrap plugin.
+ *
+ * @return void
+ */
+function bootstrap(): void {
+	// Post type and taxonomies.
+	add_action( 'init', __NAMESPACE__ . '\\register_exclusion_set_post_type' );
+
+	// Opt into stuff.
+	add_filter( 'qe_inclusion_exclusion_category_taxonomy_post_types', __NAMESPACE__ . '\\opt_in' );
+
+	// Other hooks.
+	add_action( 'save_post_' . POST_TYPE, __NAMESPACE__ . '\\bust_post_cache' );
+}
+
+/**
+ * Register Exclusion Set post type.
+ *
+ * @return void
+ */
+function register_exclusion_set_post_type(): void {
+	// Post type arguments.
+	$args = [
+		'labels'              => [
+			'name'               => 'Exclusion Sets',
+			'singular_name'      => 'Exclusion Set',
+			'add_new'            => 'Add New',
+			'add_new_item'       => 'Add New Exclusion Set',
+			'edit_item'          => 'Edit Exclusion Set',
+			'new_item'           => 'New Exclusion Set',
+			'view_item'          => 'View Exclusion Set',
+			'search_items'       => 'Search Exclusion Sets',
+			'not_found'          => 'No Exclusion Sets found',
+			'not_found_in_trash' => 'No Exclusion Sets found in Trash',
+			'parent_item_colon'  => 'Parent Exclusion Set:',
+			'menu_name'          => 'Exclusion Sets',
+		],
+		'public'              => false,
+		'show_in_rest'        => true,
+		'menu_icon'           => 'dashicons-networking',
+		'hierarchical'        => false,
+		'supports'            => [
+			'title',
+		],
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => false,
+		'publicly_queryable'  => false,
+		'exclude_from_search' => true,
+		'has_archive'         => false,
+		'query_var'           => true,
+		'can_export'          => true,
+		'rewrite'             => false,
+	];
+
+	// Register post type.
+	register_post_type( POST_TYPE, $args );
+}
+
+/**
+ * Opt into stuff.
+ *
+ * @param string[] $post_types Post types.
+ *
+ * @return string[]
+ */
+function opt_in( array $post_types = [] ): array {
+	// Append Exclusion Set post type for taxonomy.
+	$post_types[] = POST_TYPE;
+
+	// Return modified array.
+	return $post_types;
+}
+
+/**
+ * Busts cache for this post type.
+ *
+ * @param int $post_id Post ID.
+ *
+ * @return void
+ */
+function bust_post_cache( int $post_id = 0 ): void {
+	// Clear cache for this post.
+	wp_cache_delete( CACHE_KEY . "_$post_id", CACHE_GROUP );
+
+	// Trigger action to clear cache for this post.
+	do_action( 'qe_exclusion_sets_post_cache_busted', $post_id );
+}
+
+/**
+ * Get an Exclusion Set.
+ *
+ * @param int $post_id Post ID.
+ *
+ * @return array{
+ *     post: WP_Post|null,
+ *     post_meta: mixed[],
+ *     post_taxonomies: mixed[]
+ * }
+ */
+function get( int $post_id = 0 ): array {
+	// Get post ID.
+	if ( 0 === $post_id ) {
+		$post_id = absint( get_the_ID() );
+	}
+
+	// Check for cached version.
+	$cache_key    = CACHE_KEY . "_$post_id";
+	$cached_value = wp_cache_get( $cache_key, CACHE_GROUP );
+
+	// Check for cached value.
+	if ( is_array( $cached_value ) && ! empty( $cached_value['post'] ) && $cached_value['post'] instanceof WP_Post ) {
+		return [
+			'post'            => $cached_value['post'],
+			'post_meta'       => $cached_value['post_meta'] ?? [],
+			'post_taxonomies' => $cached_value['post_taxonomies'] ?? [],
+		];
+	}
+
+	// Get post.
+	$post = get_post( $post_id );
+
+	// Check for post.
+	if ( ! $post instanceof WP_Post || POST_TYPE !== $post->post_type ) {
+		return [
+			'post'            => null,
+			'post_meta'       => [],
+			'post_taxonomies' => [],
+		];
+	}
+
+	// Build data.
+	$data = [
+		'post'            => $post,
+		'post_meta'       => [],
+		'post_taxonomies' => [],
+	];
+
+	// Get all post meta.
+	$meta = get_post_meta( $post->ID );
+
+	// Check for post meta.
+	if ( ! empty( $meta ) && is_array( $meta ) ) {
+		$data['post_meta'] = array_filter(
+			array_map(
+				fn( $value ) => maybe_unserialize( $value[0] ?? '' ),
+				$meta
+			),
+			fn( $key ) => ! str_starts_with( $key, '_' ),
+			ARRAY_FILTER_USE_KEY
+		);
+	}
+
+	// Taxonomy terms.
+	global $wpdb;
+	$taxonomy_terms = $wpdb->get_results(
+		$wpdb->prepare(
+			"
+			SELECT
+				t.*,
+				tt.taxonomy,
+				tt.description,
+				tt.parent
+			FROM
+				$wpdb->term_relationships AS tr
+			LEFT JOIN
+				$wpdb->term_taxonomy AS tt ON tt.term_taxonomy_id = tr.term_taxonomy_id
+			LEFT JOIN
+				$wpdb->terms AS t ON t.term_id = tt.term_taxonomy_id
+			WHERE
+				tr.object_id = %d
+			ORDER BY
+				t.name ASC
+			",
+			[
+				$post->ID,
+			]
+		),
+		ARRAY_A
+	);
+
+	// Check for taxonomy terms.
+	if ( ! empty( $taxonomy_terms ) ) {
+		foreach ( $taxonomy_terms as $taxonomy_term ) {
+			if ( ! array_key_exists( $taxonomy_term['taxonomy'], $data['post_taxonomies'] ) ) {
+				$data['post_taxonomies'][ $taxonomy_term['taxonomy'] ] = [];
+			}
+			$data['post_taxonomies'][ $taxonomy_term['taxonomy'] ][] = $taxonomy_term;
+		}
+	}
+
+	// Set cache and return data.
+	wp_cache_set( $cache_key, $data, CACHE_GROUP );
+
+	// Return data.
+	return $data;
+}
