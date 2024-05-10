76b7321a7fdd6b31eb3244462f06653e2d60da1d
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-05-10 14:00:33 +0530

    QE-198 Add styles for cta

--- wp-content/themes/quarkexpeditions/src/front-end/components/info-cards/cta.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/info-cards/cta.blade.php
@@ -4,13 +4,22 @@
 ] )
 
 @php
-	if ( empty( $title ) ) {
+	if ( empty( $text ) || empty( $url ) ) {
 		return;
 	}
 
-	$classes = [ 'info-cards__card-title' ];
+	$classes = [ 'info-cards__card-cta' ];
 @endphp
 
-<h4 @class($classes)>
-	<x-escape :content="$title"/>
-</h4>
+<a
+	@class($classes)
+	href="{!! esc_url( $url ) !!}"
+>
+	<span class="info-cards__card-cta-text">
+		<x-escape :content="$text"/>
+	</span>
+
+	<span class="info-cards__card-cta-icon">
+		<x-svg name="chevron-left"/>
+	</span>
+</a>
