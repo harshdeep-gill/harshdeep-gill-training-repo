ad752040ec8b7bb0dae5f82547e1158b4ef5a8a8
Prithwiraj Ghosh <prithwiraj10@gmail.com>
2024-05-15 11:55:37 +0530

    QE-226 feat: add sidebar-grid block

--- wp-content/themes/quarkexpeditions/src/editor/blocks/index.ts
+++ wp-content/themes/quarkexpeditions/src/editor/blocks/index.ts
@@ -33,6 +33,7 @@ import * as mediaTextCta from './media-text-cta';
 import * as fancyVideo from './fancy-video';
 import * as formTwoStep from './form-two-step';
 import * as formTwoStepCompact from './form-two-step-compact';
+import * as sidebarGrid from './sidebar-grid';
 
 /**
  * Add blocks.
@@ -65,6 +66,7 @@ const blocks = [
 	fancyVideo,
 	formTwoStep,
 	formTwoStepCompact,
+	sidebarGrid,
 ];
 
 /**
