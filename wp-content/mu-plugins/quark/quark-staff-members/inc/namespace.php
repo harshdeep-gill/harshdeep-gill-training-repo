6c3adb01d5f2453043dc100e79c93c9410df68a5
Jignesh Nakrani <jignesh.nakrani@rtcamp.com>
2024-05-16 11:26:15 +0530

    QE-192 Fix - Frontend facing CPT view

--- wp-content/mu-plugins/quark/quark-staff-members/inc/namespace.php
+++ wp-content/mu-plugins/quark/quark-staff-members/inc/namespace.php
@@ -84,7 +84,7 @@ function register_staff_member_post_type(): void {
 		'show_ui'             => true,
 		'show_in_menu'        => true,
 		'show_in_nav_menus'   => false,
-		'publicly_queryable'  => false,
+		'publicly_queryable'  => true,
 		'exclude_from_search' => true,
 		'has_archive'         => false,
 		'query_var'           => true,
