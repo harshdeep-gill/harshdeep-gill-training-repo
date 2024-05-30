28bc050d52c6d3c9348084635bef1e5391fab9ba
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-05-30 11:17:54 +0530

    QE-242 Extract Logic and styling to the country-selector

--- wp-content/themes/quarkexpeditions/src/front-end/components/country-selector/index.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/country-selector/index.blade.php
@@ -4,22 +4,24 @@
 	'class_prefix' => '',
 ] )
 
-<x-form.field :validation="[ 'required' ]" class="{{ $class_prefix . '__country' }}">
-	<x-form.select label="Country" name="fields[Country_Code__c]">
-		<x-form.option value="">- Select -</x-form.option>
-		@foreach ( $countries as $country_code => $country_name )
-			<x-form.option value="{{ $country_code }}" label="{{ $country_name }}">{{ $country_name }}</x-form.option>
-		@endforeach
-	</x-form.select>
-</x-form.field>
-
-@foreach ( $states as $country_code => $country_states )
-	<x-form.field :validation="[ 'required' ]" data-country="{{ $country_code }}" class="{{ $class_prefix . '__state' }}" data-name="fields[State_Code__c]">
-		<x-form.select label="State/Province">
+<quark-country-selector class="country-selector">
+	<x-form.field :validation="[ 'required' ]" class="country-selector__country">
+		<x-form.select label="Country" name="fields[Country_Code__c]">
 			<x-form.option value="">- Select -</x-form.option>
-			@foreach ( $country_states as $state_code => $state_name )
-				<x-form.option value="{{ $state_code }}" label="{{ $state_name }}">{{ $state_name }}</x-form.option>
+			@foreach ( $countries as $country_code => $country_name )
+				<x-form.option value="{{ $country_code }}" label="{{ $country_name }}">{{ $country_name }}</x-form.option>
 			@endforeach
 		</x-form.select>
 	</x-form.field>
-@endforeach
+
+	@foreach ( $states as $country_code => $country_states )
+		<x-form.field :validation="[ 'required' ]" data-country="{{ $country_code }}" class="country-selector__state" data-name="fields[State_Code__c]">
+			<x-form.select label="State/Province">
+				<x-form.option value="">- Select -</x-form.option>
+				@foreach ( $country_states as $state_code => $state_name )
+					<x-form.option value="{{ $state_code }}" label="{{ $state_name }}">{{ $state_name }}</x-form.option>
+				@endforeach
+			</x-form.select>
+		</x-form.field>
+	@endforeach
+</quark-country-selector>
