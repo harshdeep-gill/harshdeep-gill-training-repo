70b8fa76c5c9d44757f1268f86a04cec639fe5ef
dishitpala <dishitpala@gmail.com>
2024-07-09 02:08:53 +0530

    QE-274 Refactor simple-cards block

---
+++ wp-content/themes/quarkexpeditions/src/editor/blocks/simple-cards/index.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Block: Simple Cards.
+ *
+ * @package quark
+ */
+
+namespace Quark\Theme\Blocks\SimpleCards;
+
+use WP_Block;
+
+const BLOCK_NAME = 'quark/simple-cards';
+const COMPONENT  = 'simple-cards';
+
+/**
+ * Bootstrap this block.
+ *
+ * @return void
+ */
+function bootstrap(): void {
+	// Register the block.
+	register_block_type_from_metadata(
+		__DIR__,
+		[
+			'render_callback' => __NAMESPACE__ . '\\render',
+		]
+	);
+}
+
+/**
+ * Render this block.
+ *
+ * @param mixed[]       $attributes Block attributes.
+ * @param string        $content Block default content.
+ * @param WP_Block|null $block Block instance.
+ *
+ * @return string
+ */
+function render( array $attributes = [], string $content = '', WP_Block $block = null ): string {
+	// Check if block is an instance of WP_Block.
+	if ( ! $block instanceof WP_Block ) {
+		return $content;
+	}
+
+	// Initialize slot.
+	$slot = '';
+
+	// Build slot.
+	foreach ( $block->inner_blocks as $inner_block ) {
+		// Check if we have an image.
+		if ( ! $inner_block instanceof \WP_Block || empty( $inner_block->attributes['image']['id'] ) ) {
+			continue;
+		}
+
+		// Add item.
+		$slot .= quark_get_component(
+			COMPONENT . '.card',
+			[
+				'title'    => $inner_block->attributes['title'] ?? '',
+				'image_id' => $inner_block->attributes['image']['id'],
+				'url'      => $inner_block->attributes['url']['url'] ?? '',
+				'target'   => ! empty( $inner_block->attributes['url']['newWindow'] ) ? '_blank' : '',
+			]
+		);
+	}
+
+	// Build attributes.
+	$attributes = [
+		'slot' => $slot,
+	];
+
+	// Return rendered component.
+	return quark_get_component( COMPONENT, $attributes );
+}
