3138d2a5cbd54c75f7440fbc918e43e36e7273d9
Takshil Kunadia <takshilkunadia@gmail.com>
2024-07-10 12:23:06 +0530

    QE-290 Refactor Blog Post Cards

--- wp-content/themes/quarkexpeditions/src/editor/blocks/blog-post-cards/index.php
+++ wp-content/themes/quarkexpeditions/src/editor/blocks/blog-post-cards/index.php
@@ -13,61 +13,18 @@ use function Quark\Blog\get_cards_data;
 
 use const Quark\Blog\POST_TYPE as BLOG_POST_TYPE;
 
-const BLOCK_NAME = 'quark/blog-post-cards';
-const COMPONENT  = 'parts.blog-post-cards';
+const COMPONENT = 'parts.blog-post-cards';
 
 /**
- * Block initialization.
+ * Bootstrap this block.
  *
  * @return void
  */
 function bootstrap(): void {
-	// Avoid registering in admin to fix a conflict with Blade views.
-	if ( ! is_admin() ) {
-		add_action( 'wp_loaded', __NAMESPACE__ . '\\register' );
-	}
-}
-
-/**
- * Register block on the front-end.
- *
- * @return void
- */
-function register(): void {
-	// Register block.
-	register_block_type(
-		BLOCK_NAME,
+	// Register the block.
+	register_block_type_from_metadata(
+		__DIR__,
 		[
-			'attributes'      => [
-				'selection'        => [
-					'type'    => 'string',
-					'default' => 'manual',
-				],
-				'layout'           => [
-					'type'    => 'string',
-					'default' => 'collage',
-				],
-				'ids'              => [
-					'type'    => 'array',
-					'default' => [],
-				],
-				'taxonomies'       => [
-					'type'    => 'array',
-					'default' => [],
-				],
-				'termIds'          => [
-					'type'    => 'array',
-					'default' => [],
-				],
-				'totalPosts'       => [
-					'type'    => 'number',
-					'default' => 5,
-				],
-				'isMobileCarousel' => [
-					'type'    => 'boolean',
-					'default' => true,
-				],
-			],
 			'render_callback' => __NAMESPACE__ . '\\render',
 		]
 	);
@@ -76,9 +33,9 @@ function register(): void {
 /**
  * Render this block.
  *
- * @param mixed[] $attributes Block attributes.
+ * @param mixed[] $attributes The block attributes.
  *
- * @return string
+ * @return string The block markup.
  */
 function render( array $attributes = [] ): string {
 	// Build query args.
@@ -149,8 +106,8 @@ function render( array $attributes = [] ): string {
 	return quark_get_component(
 		COMPONENT,
 		[
-			'layout'             => $attributes['layout'] ?? 'collage',
-			'is_mobile_carousel' => $attributes['isMobileCarousel'] ?: false,
+			'layout'             => $attributes['layout'],
+			'is_mobile_carousel' => $attributes['isMobileCarousel'],
 			'cards'              => $cards_data,
 		]
 	);
