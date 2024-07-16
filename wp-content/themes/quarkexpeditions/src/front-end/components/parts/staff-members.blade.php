e324255c5205c2187b66701e4ed103fa2f5805c3
Takshil Kunadia <takshilkunadia@gmail.com>
2024-07-16 15:22:13 +0530

    QE-207 Add part for Staff Members

---
+++ wp-content/themes/quarkexpeditions/src/front-end/components/parts/staff-members.blade.php
@@ -0,0 +1,30 @@
+@props( [
+	'cards'  => [],
+	'layout' => 'grid',
+] )
+
+@php
+	if ( empty( $cards ) ) {
+		return;
+	}
+@endphp
+
+<x-info-cards :layout="$layout">
+	@foreach ( $cards as $card )
+		<x-info-cards.card size="big" :url="$card['permalink']">
+			<x-info-cards.image :image_id="$card['featured_image']" />
+			<x-info-cards.content position="bottom">
+			<x-info-cards.overline>
+				@if ( ! empty( $card['season'] ) )
+					<x-escape :content="$card['season']" />
+				@endif
+			</x-info-cards.overline>
+				<x-info-cards.title :title="$card['title']" />
+				<x-info-cards.description>
+					<x-escape :content="$card['role']" />
+				</x-info-cards.description>
+				<x-info-cards.cta text="Read more" />
+			</x-info-cards.content>
+		</x-info-cards.card>
+	@endforeach
+</x-info-cards>
