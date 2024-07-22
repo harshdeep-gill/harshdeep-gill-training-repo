9cbb3421a521a9b2c2fb731cc4d70bc1e1b6678e
dishitpala <dishitpala@gmail.com>
2024-07-23 04:59:03 +0530

    QE-315 Add related-adventure-options block

---
+++ wp-content/themes/quarkexpeditions/src/front-end/components/parts/related-adventure-options.blade.php
@@ -0,0 +1,29 @@
+@props( [
+	'adventure_options' => [],
+	'show_title'        => true,
+	'show_description'  => false,
+] )
+
+@php
+	if ( empty( $adventure_options ) ) {
+		return;
+	}
+@endphp
+
+<x-info-cards layout="carousel">
+	@foreach( $adventure_options as $adventure_option )
+		<x-info-cards.card>
+			<x-info-cards.image image_id="{{ $adventure_option['featured_image'] ?? 0 }}" />
+			<x-info-cards.content position="bottom">
+				@if( $show_title )
+					<x-info-cards.title title="{{ $adventure_option['title'] ?? '' }}" />
+				@endif
+				@if( $show_description )
+					<x-info-cards.description>
+						{{ $adventure_option['description'] ?? '' }}
+					</x-info-cards.description>
+				@endif
+			</x-info-cards.content>
+		</x-info-cards.card>
+	@endforeach
+</x-info-cards>
