a507fd38e98e62d0a277fb7343acea35bc65ca0a
Jignesh <jignesh.nakrani@rtcamp.com>
2024-07-22 20:14:57 +0530

    QE-293 Minor fix

--- wp-content/mu-plugins/quark/quark-softrip/inc/class-softrip-sync.php
+++ wp-content/mu-plugins/quark/quark-softrip/inc/class-softrip-sync.php
@@ -10,7 +10,7 @@ namespace Quark\Softrip;
 use WP_Query;
 use WP_Error;
 
-use const Quark\Itineraries\POST_TYPE;
+use const Quark\Itineraries\POST_TYPE as ITINERARY_POST_TYPE;
 
 /**
  * Class Softrip_Sync.
@@ -27,19 +27,19 @@ class Softrip_Sync {
 	/**
 	 * Sync a softrip code.
 	 *
-	 * @param string  $softrip_id The softrip code to sync.
-	 * @param mixed[] $data       The data to sync to the code.
+	 * @param string  $softrip_code The softrip code to sync.
+	 * @param mixed[] $data         The data to sync to the code.
 	 *
 	 * @return bool
 	 */
-	public function sync_softrip_code( string $softrip_id = '', array $data = [] ): bool {
+	public function sync_softrip_code( string $softrip_code = '', array $data = [] ): bool {
 		// Check ID has been prepared.
-		if ( empty( $this->prepared_codes[ $softrip_id ] ) ) {
+		if ( empty( $this->prepared_codes[ $softrip_code ] ) ) {
 			return false;
 		}
 
 		// Get itinerary and update data.
-		$post_id   = $this->prepared_codes[ $softrip_id ];
+		$post_id   = $this->prepared_codes[ $softrip_code ];
 		$itinerary = new Itinerary( $post_id );
 		$itinerary->update_departures( (array) $data );
 
@@ -80,13 +80,17 @@ class Softrip_Sync {
 	/**
 	 * Split a list of itinerary ID's into batches of 5.
 	 *
-	 * @param int[] $ids Array of ID's to split.
+	 * @param int[] $ids        Array of ID's to split.
+	 * @param int   $batch_size The size of the batch.
 	 *
 	 * @return array<int, array<int, int|string>>
 	 */
-	public function prepare_batch_ids( array $ids = [] ): array {
+	public function prepare_batch_ids( array $ids = [], int $batch_size = 5 ): array {
 		// Start sync items.
 		$batches = [];
+
+		// Ensure batch size is at least 1.
+		$batch_size = max( 1, $batch_size );
 
 		// Create packages.
 		foreach ( $ids as $id ) {
@@ -100,7 +104,7 @@ class Softrip_Sync {
 		}
 
 		// Chunk to sync into packages.
-		return array_chunk( array_keys( $batches ), 5 );
+		return array_chunk( array_keys( $batches ), $batch_size );
 	}
 
 	/**
@@ -111,10 +115,15 @@ class Softrip_Sync {
 	public function get_itinerary_ids(): array {
 		// Args to get items.
 		$args = [
-			'post_type'      => POST_TYPE,
-			'posts_per_page' => 100,
-			'fields'         => 'ids',
-			'offset'         => 0,
+			'post_type'              => ITINERARY_POST_TYPE,
+			'posts_per_page'         => 100,
+			'fields'                 => 'ids',
+			'offset'                 => 0,
+			'post_status'            => 'publish', // Lets not get Departures for drafts Itinerary.
+			'no_found_rows'          => true,
+			'update_post_meta_cache' => false,
+			'update_post_term_cache' => false,
+			'ignore_sticky_posts'    => true,
 		];
 
 		// Run the query.
