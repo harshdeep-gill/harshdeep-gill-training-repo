1d72e367021477e570c26a950fc401a0c11fd662
Takshil Kunadia <takshilkunadia@gmail.com>
2024-07-25 18:07:55 +0530

    QE-291 Supply Block data to front-end

--- wp-content/themes/quarkexpeditions/src/editor/blocks/ships/index.php
+++ wp-content/themes/quarkexpeditions/src/editor/blocks/ships/index.php
@@ -7,9 +7,9 @@
 
 namespace Quark\Theme\Blocks\Ships;
 
-use WP_Query;
-
-use const Quark\Ships\POST_TYPE as SHIPS_POST_TYPE;
+use function Quark\CabinCategories\get as get_cabin_categories;
+use function Quark\ShipDecks\get as get_ship_deck;
+use function Quark\Ships\get as get_ships;
 
 const COMPONENT = 'parts.ships';
 
@@ -54,26 +54,79 @@ function render( array $attributes = [] ): string {
 	}
 
 	// Get blog post cards data.
-	$cards_data = [];
+	$ships_data = [];
 
 	// Query the posts.
 	foreach( $ships_ids as $ship_id ) {
-		$ship = get_post( $ship_id );
+		$ship = get_ships( $ship_id );
 
 		// Skip if no post.
 		if ( ! $ship ) {
 			continue;
 		}
 
+		// Get the post and post meta.
+		$ship_post = $ship['post'];
+		$ship_meta = $ship['post_meta'];
+
 		// Get Decks associated with the ship.
+		if ( ! empty( $ship_meta['related_decks'] ) ) {
+			$deck_ids = array_map( 'intval', $ship_meta['related_decks'] );
+		}
+
+		// Prepare deck data.
+		$decks_data = [];
+
+		// Check if we have deck IDs.
+		if ( ! empty( $deck_ids ) ) {
+			// Loop through the deck IDs.
+			foreach( $deck_ids as $deck_id ) {
+				// Get the deck.
+				$deck = get_ship_deck( $deck_id );
+
+				// Skip if no post.
+				if ( ! $deck ) {
+					continue;
+				}
+
+				// Get the post and post meta.
+				$deck_post = $deck['post'];
+				$deck_meta = $deck['post_meta'];
+
+				// Prepare public spaces data.
+				$public_spaces = [];
+				if ( ! empty( $deck_meta ) ) {
+					$public_spaces = prepare_public_spaces( $deck_meta );
+				}
+
+				// Prepare Cabin Options data.
+				$cabin_options = [];
+				if ( ! empty( $deck_meta['cabin_categories'] ) ) {
+					$cabin_options_ids = array_map( 'intval', $deck_meta['cabin_categories'] );
+					$cabin_options     = get_cabin_options( $cabin_options_ids );
+				}
+
+				// Get the post data.
+				$decks_data[] = [
+					'id'            => $deck_post->post_name ?? '',
+					'title'         => $deck_meta['deck_name'] ?? '',
+					'image_id'      => intval( $deck_meta['deck_plan_image'] ) ?? 0,
+					'description'   => apply_filters( 'the_content', $deck_post->post_content ),
+					'cabin_options' => $cabin_options,
+					'public_spaces' => $public_spaces,
+				];
+			}
+		}
 
 		// Get the post data.
-		$cards_data[] = [
-			'id'          => $ship->post_name,
-			'title'       => $ship->post_title,
-			'permalink'   => get_permalink( $ship ),
-			'description' => $ship->post_content,
-			'decks'       => [],
+		$ships_data[] = [
+			'id'          => $ship_post->post_name ?? '',
+			'title'       => $ship_post->post_title ?? '',
+			'permalink'   => $ship['permalink'] ?? '',
+			'description' => apply_filters( 'the_content', $ship_post->post_content ),
+			'content'     => '',
+			'decks_id'    => $ship_post->post_name . '_decks' ?? '',
+			'decks'       => $decks_data,
 		];
 	}
 
@@ -81,7 +134,146 @@ function render( array $attributes = [] ): string {
 	return quark_get_component(
 		COMPONENT,
 		[
-			'cards' => $cards_data,
+			'ships' => $ships_data,
 		]
 	);
+}
+
+/**
+ * Prepare public spaces data.
+ *
+ * @param mixed[] $deck_meta The deck meta.
+ *
+ * @return mixed[] The public spaces data.
+ */
+function prepare_public_spaces( array $deck_meta = [] ): array {
+	// Check if we have public spaces.
+	if ( empty( $deck_meta ) ) {
+		return [];
+	}
+
+	// Prepare public spaces data.
+	$public_spaces = [];
+
+	// Search for public spaces meta keys and store its values.
+	foreach( $deck_meta as $key => $value ) {
+		$public_space = [];
+		if ( 0 === strpos( $key, 'public_spaces_' ) ) {
+			// If key contains 'title' string, then it's a title.
+			if ( false !== strpos( $key, 'title' ) ) {
+				$public_space['title'] = $value;
+			}
+
+			// If key contains 'description' string, then it's a description.
+			if ( false !== strpos( $key, 'description' ) ) {
+				$public_space['description'] = apply_filters( 'the_content', $value );
+			}
+
+			// If key contains 'image' string, then it's an image.
+			if ( false !== strpos( $key, 'image' ) ) {
+				$public_space['image_id'] = $value;
+			}
+
+			// Add it to the public spaces.
+			if ( ! empty( $public_space ) ) {
+				$public_spaces[] = $public_space;
+			}
+		}
+	}
+
+	// Return public spaces data.
+	return $public_spaces;
+}
+
+/**
+ * Get Cabin Options data.
+ *
+ * @param int[] $cabin_options_ids The cabin options IDs.
+ *
+ * @return mixed[] The Cabin Options data.
+ */
+function get_cabin_options( array $cabin_options_ids = [] ): array {
+	// Check if we have cabin options.
+	if ( empty( $cabin_options_ids ) ) {
+		return [];
+	}
+
+	// Get the cabin options.
+	$cabin_options = [];
+	
+	// Loop through the cabin options IDs.
+	foreach( $cabin_options_ids as $cabin_option_id ) {
+		$cabin_option = get_cabin_categories( $cabin_option_id );
+
+		// Skip if no post.
+		if ( empty( $cabin_option ) ) {
+			continue;
+		}
+
+		// Get the post and post meta.
+		$cabin_option_post       = $cabin_option['post'];
+		$cabin_option_meta       = $cabin_option['post_meta'];
+		$cabin_option_taxonomies = $cabin_option['post_taxonomies'];
+
+		// Prepare location data.
+		$related_decks_ids = array_map( 'intval', $cabin_option_meta['related_decks'] );
+		
+		// Loop through the related decks IDs.
+		$locations = [];
+		foreach( $related_decks_ids as $related_deck_id ) {
+			// Get the deck.
+			$related_deck = get_ship_deck( $related_deck_id );
+
+			// Skip if no post.
+			if ( empty( $related_deck ) ) {
+				continue;
+			}
+
+			// Get the post and post meta.
+			$related_deck_meta = $related_deck['post_meta'];
+
+			// Prepare location data.
+			$locations[] = $related_deck_meta['deck_name'];
+		}
+
+		// Prepare comma separated location.
+		$cabin_locations = implode( ', ', $locations );
+
+		// Prepare cabin option data.
+		$cabin_options[] = [
+			'id'          => $cabin_option_post->post_name,
+			'title'       => $cabin_option_meta['cabin_name'],
+			'image_id'    => $cabin_option_meta['cabin_images'][0],
+			'description' => apply_filters( 'the_content', $cabin_option_post->post_content ),
+			'details'     => [
+				[
+					'label' => 'Size',
+					'value' => $cabin_option_meta['cabin_category_size_range_from'] === $cabin_option_meta['cabin_category_size_range_to']
+						? $cabin_option_meta['cabin_category_size_range_from']
+						: $cabin_option_meta['cabin_category_size_range_from'] . ' - ' . $cabin_option_meta['cabin_category_size_range_to'],
+				],
+				[
+					'label' => 'Occupancy',
+					'value' => $cabin_option_meta['cabin_occupancy_pax_range_from'] === $cabin_option_meta['cabin_occupancy_pax_range_to']
+						? $cabin_option_meta['cabin_occupancy_pax_range_from']
+						: $cabin_option_meta['cabin_occupancy_pax_range_from'] . ' - ' . $cabin_option_meta['cabin_occupancy_pax_range_to'],
+				],
+				[
+					'label' => 'Bed Config.',
+					'value' => apply_filters( 'the_content', $cabin_option_meta['cabin_bed_configuration'] ),
+				],
+				[
+					'label' => 'Class',
+					'value' => $cabin_option_taxonomies['qrk_cabin_class'][0]['name'] ?? '',
+				],
+				[
+					'label' => 'Location',
+					'value' => $cabin_locations,
+				]
+			],
+		];
+	}
+
+	// Return cabin options data.
+	return $cabin_options;
 }
--- wp-content/themes/quarkexpeditions/src/front-end/components/parts/ships.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/parts/ships.blade.php
@@ -19,68 +19,68 @@
 
 	<x-tabs.content>
 		@foreach ( $ships as $ship )
-			@if ( ! empty( $ship['id'] ) ) 
+			@if ( ! empty( $ship['id'] ) )
 				<x-tabs.tab id="{{ $ship['id'] }}">
 					<x-section class="section--ship">
 						<x-section.heading>
 							<x-section.title :title="$ship['title'] ?? ''" align="left" />
 						</x-section.heading>
 						<x-section.description>
-							{{ $ship['description'] }}
+							{!! $ship['description'] !!}
 						</x-section.description>
 						<x-section.content>
-							{{ $ship['content'] }} // TODO: This is the content that needs to be replaced with two-columns for ship features & amenities.
+							{{ $ship['content'] }}
 						</x-section.content>
 					</x-section>
-					<x-drawer.drawer-open :drawer_id="{{ $ship['id'] }}">
+					<x-drawer.drawer-open :drawer_id="$ship['decks_id']">
 						<x-button type="button" size="big" color="black">
 							{{ __( 'View Deck Plans & Cabins', 'qrk' ) }}
 						</x-button>
 					</x-drawer.drawer-open>
-					<x-drawer :id="$ship['id']">
-						<x-drawer.heading>
-							<x-drawer.title :title="{{ __( 'Deck Plans & Cabins', 'qrk' ) }}" />
-						</x-drawer.heading>
+					<x-drawer :id="$ship['decks_id']" content_class="drawer__content--half-size" animation_direction="right">
+						<x-drawer.header>
+							<h3>{{ __( 'Deck Plans & Cabins', 'qrk' ) }}</h3>
+						</x-drawer.header>
 						<x-drawer.body>
 							<x-tabs>
 								<x-tabs.header>
 									@foreach ( $ship['decks'] as $deck )
-										<x-tabs.nav id="{{ $deck['id'] }}" title="{{ $deck['deck_name'] ?? '' }}" />
+										<x-tabs.nav id="{{ $deck['id'] }}" title="{{ $deck['title'] ?? '' }}" />
 									@endforeach
 								</x-tabs.header>
 								<x-tabs.content>
 									@foreach ( $ship['decks'] as $deck )
 										<x-tabs.tab id="{{ $deck['id'] }}">
-											@if ( ! empty( $deck['deck_plan_image_id'] ) )
-												<x-image :image_id="$deck['deck_plan_image_id']" :alt="$deck['deck_name'] ?? ''" />
+											@if ( ! empty( $deck['image_id'] ) )
+												<x-image :image_id="$deck['image_id']" :alt="$deck['title'] ?? ''" />
 											@endif
-											<x-content :content="$deck['deck_description'] ?? ''" />
-											@if ( ! empty( $deck['deck_options'] ) )
+											<x-content :content="$deck['description'] ?? ''" />
+											@if ( ! empty( $deck['cabin_options'] ) )
 												<h3>{{ __( 'Cabin Options', 'qrk' ) }}</h3>
 												<x-media-detail-cards>
-													foreach ( $deck['cabin_options'] as $cabin_option )
+													@foreach ( $deck['cabin_options'] as $cabin_option )
 														<x-media-detail-cards.card>
 															@if ( ! empty( $cabin_option['image_id'] ) )
 																<x-media-detail-cards.image :image_id="$cabin_option['image_id']" :alt="$cabin_option['title']" />
-															endif
+															@endif
 															<h5>{{ $cabin_option['title'] ?? '' }}</h5>
 															@if ( ! empty( $cabin_option['description'] ) )
 																<x-media-detail-cards.content>
-																	{{ $cabin_option['content'] }}
+																	{!! $cabin_option['description'] !!}
 																</x-media-detail-cards.content>
 															@endif
 															@if ( ! empty( $cabin_option['details'] ) )
-																</x-media-detail-cards.details>
-																	foreach ( $cabin_option['details'] as $detail )
-																		<x-media-detail-cards.detail-item label="{{ $detail['label'] ?? '' }}" value="{{ $detail['value'] ?? '' }}" />
-																	endforeach
+																<x-media-detail-cards.details>
+																	@foreach ( $cabin_option['details'] as $detail )
+																		<x-media-detail-cards.detail-item label="{{ $detail['label'] ?? '' }}" value="{!! $detail['value'] ?? '' !!}" />
+																	@endforeach
 																</x-media-detail-cards.details>
 															@endif
 														</x-media-detail-cards.card>
-													endforeach
+													@endforeach
 												</x-media-detail-cards>
 											@endif
-											@id ( ! empty( $deck['public_spaces'] ) )
+											@if ( ! empty( $deck['public_spaces'] ) )
 												<h3>{{ __( 'Public Spaces & Amenities', 'qrk' ) }}</h3>
 												<x-media-description-cards>
 													@foreach ( $deck['public_spaces'] as $public_spaces )
@@ -89,9 +89,9 @@
 																<x-media-description-cards.image :image_id="$public_spaces['image_id']" :alt="$public_spaces['title'] ?? ''" />
 															@endif
 															<x-media-description-cards.content>
-																<x-media-description-cards.title :title="{{ $public_spaces['title'] ?? '' }}" heading_level="5" />
+																<x-media-description-cards.title :title="$public_spaces['title'] ?? ''" heading_level="5" />
 																<x-media-description-cards.description>
-																	{{ $public_spaces['description'] ?? '' }}
+																	{!! $public_spaces['description'] ?? '' !!}
 																</x-media-description-cards.description>
 															</x-media-description-cards.content>
 														</x-media-description-cards.card>
