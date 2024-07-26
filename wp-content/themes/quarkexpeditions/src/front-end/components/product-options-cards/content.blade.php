6a9f11fdc7afba92287fee43a53ad25bdf9c8007
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-07-26 13:22:00 +0530

    QE-304 Add tooltip to cards

--- wp-content/themes/quarkexpeditions/src/front-end/components/product-options-cards/content.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/product-options-cards/content.blade.php
@@ -16,4 +16,10 @@
 
 <div @class( $classes )>
 	{!! $slot !!}
+	<div class="product-options-cards__tooltip">
+		<span>Inc. Transfer Package</span>
+		<x-tooltip icon="info">
+			Inc. Transfer Package
+		</x-tooltip>
+	</div>
 </div>
