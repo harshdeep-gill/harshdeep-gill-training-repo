8c56b6ab0131af42749cdc4d5e2c93a4e60df6cf
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-07-26 12:02:23 +0530

    QE-304 Style more details gallery

--- wp-content/themes/quarkexpeditions/src/front-end/components/product-options-cards/room-prices.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/product-options-cards/room-prices.blade.php
@@ -17,10 +17,22 @@
 @endphp
 
 <div @class( $classes )>
-	<div class="product-options-cards__room-prices-discounted">
-		<x-escape :content="$original_price" />
+	<div class="product-options-cards__room-prices-info">
+		<div class="product-options-cards__room-prices-discounted">
+			<x-escape :content="$original_price" />
+		</div>
+		<div class="product-options-cards__room-prices-original">
+			<x-escape :content="$discounted_price" />
+		</div>
 	</div>
-	<div class="product-options-cards__room-prices-original">
-		<x-escape :content="$discounted_price" />
-	</div>
+	<x-form.field class="product-options-cards__room-prices-selector">
+		<x-form.select label="Number of Rooms" name="">
+			<x-form.option value="">Select</x-form.option>
+				<x-form.option value="1" label="1">1</x-form.option>
+				<x-form.option value="2" label="2">2</x-form.option>
+				<x-form.option value="3" label="3">3</x-form.option>
+				<x-form.option value="4" label="4">4</x-form.option>
+				<x-form.option value="5" label="5">5</x-form.option>
+		</x-form.select>
+	</x-form.field>
 </div>
