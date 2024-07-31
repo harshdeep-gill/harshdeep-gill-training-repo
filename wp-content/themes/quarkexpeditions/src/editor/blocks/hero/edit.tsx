d57781f662ab0b3321f782fa1291e1b93e9b2401
Takshil Kunadia <takshilkunadia@gmail.com>
2024-07-31 18:20:25 +0530

    QE-338 Add Sync Post Thumbnail to hero Image block.

--- wp-content/themes/quarkexpeditions/src/editor/blocks/hero/edit.tsx
+++ wp-content/themes/quarkexpeditions/src/editor/blocks/hero/edit.tsx
@@ -2,16 +2,24 @@
  * WordPress dependencies.
  */
 import { __ } from '@wordpress/i18n';
+import { useEffect } from '@wordpress/element';
+import { store as editorStore } from '@wordpress/editor';
+import apiFetch from '@wordpress/api-fetch';
 import {
 	PanelBody,
 	RangeControl,
 	SelectControl,
+	ToggleControl,
 } from '@wordpress/components';
 import {
 	InspectorControls,
 	useBlockProps,
 	useInnerBlocksProps,
 } from '@wordpress/block-editor';
+import {
+	useSelect,
+	useDispatch,
+} from '@wordpress/data';
 
 /**
  * Styles.
@@ -44,6 +52,49 @@ const {
 import * as heroContent from './children/hero-content';
 
 /**
+ * Get formatted image details from a media object.
+ *
+ * @param {Object} media         Media object.
+ * @param {string} thumbnailSize Thumbnail size.
+ *
+ * @return {Object} Formatted image details.
+ */
+export function getImageDetails( media: Record<string, any>, thumbnailSize: string ): Record<string, any> {
+	// If media is not set, return empty object.
+	if ( ! media ) {
+		// Return empty object.
+		return {};
+	}
+
+	// Initialize src, width, and height.
+	let src, width, height;
+
+	// If media has sizes, get the thumbnail size.
+	if ( media.sizes ) {
+		// If thumbnail size is not found, set thumbnail size to full.
+		if ( ! media.sizes[ thumbnailSize ] ) {
+			// Set thumbnail size to full.
+			thumbnailSize = 'full';
+		}
+		width = media.sizes[ thumbnailSize ].width;
+		height = media.sizes[ thumbnailSize ].height;
+		src = media.sizes[ thumbnailSize ].url;
+	}
+
+	// Return formatted image details.
+	return {
+		id: media.id,
+		src,
+		width,
+		height,
+		alt: media.alt,
+		caption: media.caption,
+		title: media.title,
+		size: thumbnailSize,
+	};
+}
+
+/**
  * Edit Component.
  *
  * @param {Object}   props               Component properties.
@@ -52,7 +103,66 @@ import * as heroContent from './children/hero-content';
  * @param {Function} props.setAttributes Set block attributes.
  */
 export default function edit( { className, attributes, setAttributes }: BlockEditAttributes ): JSX.Element {
-	// eslint-disable-next-line react-hooks/rules-of-hooks
+	// Get the post editor store.
+	const { editPost } = useDispatch( editorStore );
+
+	// Get the post's featured image ID.
+	const postThumbnailId = useSelect(
+		( select: any ) => select( editorStore )?.getEditedPostAttribute?.( 'featured_media' ),
+		[],
+	);
+
+	// Sync post thumbnail whenever the image is changed from block.
+	useEffect( () => {
+		// Sync post thumbnail function.
+		async function syncPostThumbnail() {
+			// If post thumbnail is not synced, return.
+			if ( ! attributes.syncPostThumbnail || ! postThumbnailId || postThumbnailId === attributes.image?.id ) {
+				// Return.
+				return;
+			}
+
+			// Set attributes.
+			try {
+				// Get post thumbnail.
+				const postThumbnail = await apiFetch( {
+					path: `/gumponents/media/v1/get?id=${ postThumbnailId }`,
+				} );
+
+				// If post thumbnail is not found, return.
+				if ( ! postThumbnail ) {
+					// Return.
+					return;
+				}
+
+				// Set post thumbnail.
+				setAttributes( { image: getImageDetails( postThumbnail, 'large' ) } );
+			} catch ( error ) {
+				setAttributes( { image: null } );
+			}
+		}
+
+		// Sync post thumbnail.
+		syncPostThumbnail();
+	}, [ postThumbnailId, attributes.syncPostThumbnail, attributes.image?.id, setAttributes ] );
+
+	/**
+	 * Handle image change.
+	 *
+	 * @param {Object} image    Image.
+	 * @param {number} image.id Image ID.
+	 */
+	const handleImageChange = ( image: Record<string, any> ) => {
+		// Set attributes.
+		setAttributes( { image } );
+
+		// If post thumbnail is synced, update post thumbnail.
+		if ( attributes.syncPostThumbnail && image?.id ) {
+			editPost( { featured_media: image.id } );
+		}
+	};
+
+	// Set block props.
 	const blockProps = useBlockProps( {
 		className: classnames(
 			className,
@@ -63,7 +173,7 @@ export default function edit( { className, attributes, setAttributes }: BlockEdi
 		),
 	} );
 
-	// eslint-disable-next-line react-hooks/rules-of-hooks
+	// Set inner block props.
 	const innerBlockProps = useInnerBlocksProps(
 		{ className: 'hero__wrap' },
 		{
@@ -82,7 +192,7 @@ export default function edit( { className, attributes, setAttributes }: BlockEdi
 						value={ attributes.image ? attributes.image.id : null }
 						size="large"
 						help={ __( 'Choose an image', 'qrk' ) }
-						onChange={ ( image: object ) => setAttributes( { image } ) }
+						onChange={ handleImageChange }
 					/>
 					<RangeControl
 						label={ __( 'Overlay opacity in percent', 'qrk' ) }
@@ -112,6 +222,12 @@ export default function edit( { className, attributes, setAttributes }: BlockEdi
 							{ label: __( 'Center', 'qrk' ), value: 'center' },
 						] }
 						onChange={ ( textAlign: string ) => setAttributes( { textAlign } ) }
+					/>
+					<ToggleControl
+						label={ __( 'Sync with Post Thumbnail', 'qrk' ) }
+						checked={ attributes.syncPostThumbnail }
+						onChange={ ( syncPostThumbnail ) => setAttributes( { syncPostThumbnail } ) }
+						help={ __( 'Should the hero image be synced with the post thumbnail?', 'qrk' ) }
 					/>
 				</PanelBody>
 			</InspectorControls>
