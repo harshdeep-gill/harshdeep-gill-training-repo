432702e4bdb10152af9b555c487e396d7cbccf3a
Deepak Kumar <deepak.kumar852182@gmail.com>
2024-08-01 18:35:02 +0530

    QE-293 add test cases for request_departures

--- wp-content/mu-plugins/quark/quark-softrip/tests/class-test-softrip.php
+++ wp-content/mu-plugins/quark/quark-softrip/tests/class-test-softrip.php
@@ -1,168 +1,76 @@
 <?php
 /**
- * Softrip tests.
+ * Softrip test suite.
  *
  * @package quark-softrip
  */
 
 namespace Quark\Softrip;
 
-use WP_UnitTestCase;
 use WP_Error;
+use WP_UnitTestCase;
 
-use function Quark\Tests\Softrip\truncate_softrip_db_tables;
-
-/**
- * Class Test_Softrip.
- */
 class Test_Softrip extends WP_UnitTestCase {
-	/**
-	 * Tear down after class.
-	 *
-	 * @return void
-	 */
-	public static function tear_down_after_class(): void {
-		// Run parent.
-		parent::tear_down_after_class();
-		truncate_softrip_db_tables();
-	}
+    /**
+     * Test case for requesting departure from middleware.
+     *
+     * @covers \Quark\Softrip\request_departures()
+     *
+     * @return void
+     */
+    public function test_request_departures(): void {
+        // Setup mock response.
+        add_filter( 'pre_http_request', 'Quark\Tests\Softrip\mock_softrip_http_request', 10, 3 );
 
-	/**
-	 * Setup for tests.
-	 *
-	 * @return void
-	 */
-	public function set_up(): void {
-		// Run parent.
-		parent::set_up();
+        // Test case 1: No argument passed.
+        $result = request_departures();
+        $this->assertTrue( $result instanceof WP_Error );
+        $this->assertSame( 'qrk_softrip_departures_limit', $result->get_error_code() );
+        $this->assertSame( 'The maximum number of codes allowed is 5', $result->get_error_message() );
 
-		// Mock the response for the POST request.
-		add_filter( 'pre_http_request', 'Quark\Tests\Softrip\mock_softrip_http_request', 10, 3 );
-	}
+        // Test case 2: Empty array passed.
+        $result = request_departures( [] );
+        $this->assertTrue( $result instanceof WP_Error );
+        $this->assertSame( 'qrk_softrip_departures_limit', $result->get_error_code() );
+        $this->assertSame( 'The maximum number of codes allowed is 5', $result->get_error_message() );
 
-	/**
-	 * Tear down after tests.
-	 *
-	 * @return void
-	 */
-	public function tear_down(): void {
-		// Run parent.
-		parent::tear_down();
+        // Test case 3: Test code array with more than 5 elements.
+        $test_codes = [
+            'ABC-123',
+            'DEF-456',
+            'GHI-789',
+            'JKL-012',
+            'MNO-345',
+            'PQR-678',
+        ];
+        $result = request_departures( $test_codes );
+        $this->assertTrue( $result instanceof WP_Error );
+        $this->assertSame( 'qrk_softrip_departures_limit', $result->get_error_code() );
+        $this->assertSame( 'The maximum number of codes allowed is 5', $result->get_error_message() );
 
-		// Remove the filter.
-		remove_filter( 'pre_http_request', 'Quark\Tests\Softrip\mock_softrip_http_request' );
-	}
+        // Test case 4: Test code array with one element.
+        $test_codes = [ 'ABC-123' ];
+        $result = request_departures( $test_codes );
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'ABC-123', $result );
 
-	/**
-	 * Test for request_departures.
-	 *
-	 * @covers \Quark\Softrip\request_departures
-	 * @covers \Quark\Softrip\Softrip_Data_Adapter::do_request
-	 *
-	 * @return void
-	 */
-	public function test_request_departures(): void {
-		// Prepare data.
-		$raw_data = [
-			'ABC-123',
-			'HIJ-567',
-			'MNO-456',
-			'QWE-567',
-			'PQR-345',
-			'XYZ-234',
-		];
+        // Test case 5: Test code array with five elements with only a few valid.
+        $test_codes = [
+            'ABC-123',
+            'DEF-456',
+            'GHI-789',
+            'JKL-012',
+            'MNO-345',
+        ];
+        $result = request_departures( $test_codes );
+        $this->assertIsArray( $result );
+        $this->assertArrayHasKey( 'ABC-123', $result );
+        $this->assertArrayNotHasKey( 'DEF-456', $result ); // Invalid code.
+        $this->assertArrayNotHasKey( 'GHI-789', $result ); // Invalid code.
+        $this->assertArrayHasKey( 'JKL-012', $result );
+        $this->assertArrayNotHasKey( 'MNO-345', $result ); // Invalid code.
 
-		// Request departures.
-		$result = request_departures( $raw_data );
-
-		// Check the result.
-		$this->assertTrue( $result instanceof WP_Error );
-
-		// Request departures.
-		$result = request_departures( [ 'ABC-123' ] );
-
-		// assert the result.
-		$this->assertTrue( is_array( $result ) );
-		$this->assertArrayHasKey( 'ABC-123', $result );
-
-		// Request departures.
-		$result = request_departures( [ 'ABC-123', 'PQR-345' ] );
-
-		// assert the result.
-		$this->assertTrue( is_array( $result ) );
-		$this->assertArrayHasKey( 'ABC-123', $result );
-		$this->assertArrayHasKey( 'PQR-345', $result );
-
-		// Request departures.
-		$result = request_departures( [ 'NO-DATA-123' ] );
-
-		// assert the result.
-		$this->assertTrue( $result instanceof WP_Error );
-	}
-
-	/**
-	 * Test for get_departures.
-	 *
-	 * @covers \Quark\Softrip\get_db_tables_sql
-	 *
-	 * @return void
-	 */
-	public function test_get_db_tables_sql(): void {
-		// Create an instance.
-		$instance = new Softrip_DB();
-
-		// Get the tables.
-		$tables = $instance->get_db_tables_sql();
-
-		// Check the result.
-		$this->assertTrue( 5 === count( $tables ) );
-		$this->assertArrayHasKey( 'adventure_options', $tables );
-		$this->assertArrayHasKey( 'cabin_categories', $tables );
-		$this->assertArrayHasKey( 'occupancies', $tables );
-		$this->assertArrayHasKey( 'occupancy_prices', $tables );
-		$this->assertArrayHasKey( 'promos', $tables );
-	}
-
-	/**
-	 * Test for cron_add_schedule.
-	 *
-	 * @covers \Quark\Softrip\cron_add_schedule
-	 *
-	 * @return void
-	 */
-	public function test_cron_add_schedule(): void {
-		// Get the schedules.
-		$schedules = apply_filters( 'cron_schedules', [] );
-
-		// Check the result.
-		$this->assertIsArray( $schedules );
-		$this->assertNotEmpty( $schedules );
-		$this->assertArrayHasKey( SCHEDULE_RECCURANCE, $schedules );
-		$this->assertArrayHasKey( 'qrk_softrip_4_hourly', $schedules );
-	}
-
-	/**
-	 * Test for cron_schedule_sync.
-	 *
-	 * @covers \Quark\Softrip\cron_schedule_sync
-	 *
-	 * @return void
-	 */
-	public function test_cron_schedule_sync(): void {
-		// Clear any existing scheduled event for testing.
-		wp_clear_scheduled_hook( SCHEDULE_HOOK );
-		$timestamp = wp_next_scheduled( SCHEDULE_HOOK );
-		$this->assertFalse( $timestamp );
-
-		// Call the function to schedule the event.
-		cron_schedule_sync();
-
-		// Check if the event is scheduled.
-		$timestamp = wp_next_scheduled( SCHEDULE_HOOK );
-		$this->assertNotFalse( $timestamp );
-
-		// Verify the schedule interval.
-		$schedule = wp_get_schedule( SCHEDULE_HOOK );
-		$this->assertEquals( SCHEDULE_RECCURANCE, $schedule );
-	}
+        // Cleanup.
+        remove_filter( 'pre_http_request', 'Quark\Tests\Softrip\mock_softrip_http_request' );
+    }
 }
