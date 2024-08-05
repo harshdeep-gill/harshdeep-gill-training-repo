3d5e50432168b537b76ef5f351f02c4cf9d3ac51
Takshil Kunadia <takshilkunadia@gmail.com>
2024-08-05 18:50:22 +0530

    QE-369 Implement child block

---
+++ wp-content/themes/quarkexpeditions/src/editor/blocks/featured-media-accordions/children/item/edit.tsx
@@ -0,0 +1,101 @@
+/**
+ * WordPress dependencies.
+ */
+import { __ } from '@wordpress/i18n';
+import {
+	InspectorControls,
+	useBlockProps,
+	useInnerBlocksProps,
+	RichText,
+} from '@wordpress/block-editor';
+import { PanelBody } from '@wordpress/components';
+
+/**
+ * Internal dependencies.
+ */
+import icons from '../../../icons';
+
+/**
+ * External dependencies.
+ */
+import classnames from 'classnames';
+const { gumponents } = window;
+
+/**
+ * External components.
+ */
+const { SelectImage } = gumponents.components;
+
+/**
+ * Edit Component.
+ *
+ * @param {Object}   props               Component properties.
+ * @param {string}   props.className     Class name.
+ * @param {Array}    props.attributes    Block attributes.
+ * @param {Function} props.setAttributes Set block attributes.
+ */
+export default function Edit( { className, attributes, setAttributes }: BlockEditAttributes ) : JSX.Element {
+	// eslint-disable-next-line react-hooks/rules-of-hooks
+	const blockProps = useBlockProps( {
+		className: classnames( className, 'accordion__item' ),
+	} );
+
+	// eslint-disable-next-line react-hooks/rules-of-hooks
+	const innerBlockProps = useInnerBlocksProps(
+		{ className: 'accordion__content' },
+		{
+			allowedBlocks: [ 'core/paragraph' ],
+			template: [
+				[
+					'core/paragraph',
+					{
+						placeholder: __( 'Write Content…', 'qrk' ),
+						templateLock: 'all',
+						lock: { move: true, remove: true },
+					},
+				],
+			],
+
+			// @ts-ignore
+			orientation: 'horizontal',
+		}
+	);
+
+	// Return the block's markup.
+	return (
+		<>
+			<InspectorControls>
+				<PanelBody title={ __( 'Accordion Options', 'qrk' ) }>
+					<SelectImage
+						image={ attributes.image }
+						placeholder={ __( 'Choose an image', 'qrk' ) }
+						size="medium"
+						onChange={ ( image: Object ): void => {
+							// Set image.
+							setAttributes( { image: null } );
+							setAttributes( { image } );
+						} }
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<div { ...blockProps }>
+				<div className="accordion__handle">
+					<button className="accordion__handle-btn">
+						<RichText
+							tagName="h3"
+							className="h5 accordion__handle-btn-text body-text-large"
+							placeholder={ __( 'Title here…', 'qrk' ) }
+							value={ attributes.title }
+							onChange={ ( title ) => setAttributes( { title } ) }
+							allowedFormats={ [] }
+						/>
+						<span className="accordion__handle-icon">
+							{ icons.chevronLeft }
+						</span>
+					</button>
+				</div>
+				<div { ...innerBlockProps } />
+			</div>
+		</>
+	);
+}
