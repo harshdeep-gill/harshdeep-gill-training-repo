5e1133284164cc947d668d300fea87beb6774e74
Takshil Kunadia <takshilkunadia@gmail.com>
2024-08-05 18:50:39 +0530

    QE-369 Implement parent block

---
+++ wp-content/themes/quarkexpeditions/src/editor/blocks/featured-media-accordions/edit.tsx
@@ -0,0 +1,112 @@
+/**
+ * WordPress dependencies.
+ */
+import { __ } from '@wordpress/i18n';
+import {
+	useBlockProps,
+	useInnerBlocksProps,
+	InnerBlocks,
+} from '@wordpress/block-editor';
+import { useSelect } from '@wordpress/data';
+import { store as blockEditorStore } from '@wordpress/editor';
+
+/**
+ * Internal dependencies.
+ */
+import Section from '../../components/section';
+
+/**
+ * External dependencies.
+ */
+import classnames from 'classnames';
+const { gumponents } = window;
+
+/**
+ * External components.
+ */
+const { Img } = gumponents.components;
+
+/**
+ * Styles.
+ */
+import './editor.scss';
+
+/**
+ * Children blocks
+ */
+import * as item from './children/item';
+
+/**
+ * Edit Component.
+ *
+ * @param {Object} props           Component properties.
+ * @param {string} props.className Class name.
+ * @param {string} props.clientId  Client ID.
+ */
+export default function Edit( { className, clientId }: BlockEditAttributes ): JSX.Element {
+	// Get child blocks attributes.
+	const accordionMediaImage = useSelect(
+
+		// Get the image attribute from the child block.
+		( select: any ) => {
+			// Get the child blocks.
+			const childBlocks = select( blockEditorStore ).getBlocks( clientId );
+
+			// Find the child block that is selected.
+			const childBlock = childBlocks.find( ( block: any ) => {
+				// Check if the block is selected or has an inner block selected.
+				return select( blockEditorStore ).isBlockSelected( block.clientId ) || select( blockEditorStore ).hasSelectedInnerBlock( block.clientId, true );
+			} );
+
+			// Return the image attribute if the child block has an image.
+			return childBlock && 0 < Object.keys( childBlock.attributes.image ).length ? childBlock.attributes.image : null;
+		},
+		[],
+	);
+
+	// Set block props.
+	const blockProps = useBlockProps( {
+		className: classnames(
+			className,
+			'featured-media-accordions',
+		),
+	} );
+
+	// Set inner blocks props.
+	const innerBlockProps = useInnerBlocksProps(
+		{ className: 'accordion' },
+		{
+			allowedBlocks: [ item.name ],
+			template: [ [ item.name ], [ item.name ], [ item.name ] ],
+			renderAppender: InnerBlocks.ButtonBlockAppender,
+
+			// @ts-ignore
+			orientation: 'vertical',
+		}
+	);
+
+	// Return the block's markup.
+	return (
+		<div { ...blockProps }>
+			<Section>
+				<div className="two-columns grid">
+					<div className="two-columns__column">
+						<figure className="featured-media-accordions__image">
+							{
+								accordionMediaImage && (
+									<Img
+										value={ accordionMediaImage }
+										alt={ __( 'Accordion Image', 'qrk' ) }
+									/>
+								)
+							}
+						</figure>
+					</div>
+					<div className="two-columns__column">
+						<div { ...innerBlockProps } />
+					</div>
+				</div>
+			</Section>
+		</div>
+	);
+}
---
+++ wp-content/themes/quarkexpeditions/src/editor/blocks/featured-media-accordions/index.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Block: Featured Media Accordions.
+ *
+ * @package quark
+ */
+
+namespace Quark\Theme\Blocks\FeaturedMediaAccordions;
+
+use WP_Block;
+use WP_Block_List;
+
+const COMPONENT = 'parts.featured-media-accordions';
+
+/**
+ * Bootstrap this block.
+ *
+ * @return void
+ */
+function bootstrap(): void {
+	// Register the block.
+	register_block_type_from_metadata(
+		__DIR__,
+		[
+			'render_callback'   => __NAMESPACE__ . '\\render',
+			'skip_inner_blocks' => true,
+		]
+	);
+}
+
+/**
+ * Render this block.
+ *
+ * @param mixed[]  $attributes The block attributes.
+ * @param string   $content    The block content.
+ * @param WP_Block $block      The block instance.
+ *
+ * @return string The block markup.
+ */
+function render( array $attributes = [], string $content = '', WP_Block $block = null ): string {
+	// Check for block.
+	if ( ! $block instanceof WP_Block ) {
+		return $content;
+	}
+
+	// Initialize Accordions attributes.
+	$component_attributes = [];
+
+	// Process child blocks.
+	if ( $block->inner_blocks instanceof WP_Block_List ) {
+		// Initialize accordion items.
+		$accordion_items = [];
+
+		// Loop through the child blocks.
+		foreach ( $block->inner_blocks as $child_block ) {
+			// Accordion item attributes.
+			$accordion_item_attributes = [];
+
+			// Check for child block.
+			if ( ! $child_block instanceof WP_Block ) {
+				continue;
+			}
+
+			// Check for child block name.
+			if ( 'quark/featured-media-accordions-item' !== $child_block->name ) {
+				continue;
+			}
+
+			// Get the attributes.
+			$child_block_attributes = $child_block->attributes;
+
+			// Check for empty attributes.
+			if ( empty( $child_block_attributes ) ) {
+				continue;
+			}
+
+			// loop through the attributes.
+			foreach ( $child_block_attributes as $key => $value ) {
+				// Check for empty value.
+				if ( empty( $value ) ) {
+					continue;
+				}
+
+				// Check for array value.
+				switch ( $key ) {
+					// Process the title.
+					case 'title':
+						$accordion_item_attributes['title'] = $value;
+
+						// Create an id for the accordion item.
+						$accordion_item_attributes['id'] = 'accordion-item-' . $value;
+						break;
+
+					// Process the image.
+					case 'image':
+						$accordion_item_attributes['image_id'] = $value['id'];
+						break;
+				}
+			}
+
+			// Check for inner blocks.
+			if ( ! empty( $child_block->parsed_block['innerBlocks'] ) ) {
+				$accordion_item_attributes['content'] = implode( '', array_map( 'render_block', $child_block->parsed_block['innerBlocks'] ) );
+			}
+
+			// Add accordion item attributes.
+			$accordion_items[] = $accordion_item_attributes;
+		}
+	}
+
+	// Add accordion items.
+	$component_attributes['items'] = $accordion_items;
+
+	// Return rendered component.
+	return quark_get_component( COMPONENT, $component_attributes );
+}
