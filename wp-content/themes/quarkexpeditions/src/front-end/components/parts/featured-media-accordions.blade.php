5b4f9c5cb0a9a678acaf97c89eb7a155faf196f9
Takshil Kunadia <takshilkunadia@gmail.com>
2024-08-07 13:40:28 +0530

    QE-269 Add featured image for mobile

--- wp-content/themes/quarkexpeditions/src/front-end/components/parts/featured-media-accordions.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/parts/featured-media-accordions.blade.php
@@ -25,6 +25,9 @@
 				@endif
 				<x-featured-media-accordions.accordion :title="$item['title'] ?? ''" :id="$item['id'] ?? ''">
 					{!! $item['content'] ?? '' !!}
+					@if ( ! empty( $item['image_id'] ) )
+						<x-featured-media-accordions.featured-image :image_id="$item['image_id'] ?? ''" />
+					@endif
 				</x-featured-media-accordions.accordion>
 			@endforeach
 		</x-featured-media-accordions.accordions>
