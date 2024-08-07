751838e3d1396c2c94c9ca84dba019864c80cad0
Deepak Kumar <deepak.kumar852182@gmail.com>
2024-08-07 16:46:44 +0530

    QE-374 remove adventure option table sql from softrip-db class

--- wp-content/mu-plugins/quark/quark-softrip/inc/class-softrip-db.php
+++ wp-content/mu-plugins/quark/quark-softrip/inc/class-softrip-db.php
@@ -7,6 +7,8 @@
 
 namespace Quark\Softrip;
 
+use function Quark\Softrip\AdventureOptions\get_adventure_table_sql;
+
 /**
  * Class Softrip_DB.
  */
@@ -26,7 +28,7 @@ class Softrip_DB {
 	public function get_db_tables_sql(): array {
 		// Return the list of tables used.
 		return [
-			'adventure_options' => $this->get_adventure_table_sql(),
+			'adventure_options' => get_adventure_table_sql(),
 			'cabin_categories'  => $this->get_cabin_table_sql(),
 			'occupancies'       => $this->get_occupancies_table_sql(),
 			'occupancy_prices'  => $this->get_occupancy_prices_table_sql(),
@@ -93,39 +95,6 @@ class Softrip_DB {
 			availability_status VARCHAR(4) NOT NULL,
 			spaces_available INT NOT NULL,
 			UNIQUE KEY cabin_category_title_unique (title)
-		) $engine_collate";
-
-		// return the SQL.
-		return $sql;
-	}
-
-	/**
-	 * Get the adventure_options table create SQL.
-	 *
-	 * @return string
-	 */
-	private function get_adventure_table_sql(): string {
-		// Get the table name.
-		$table_name = $this->prefix_table_name( 'adventure_options' );
-
-		// Get the engine collate.
-		$engine_collate = $this->engine_collate();
-
-		// Build the SQL query.
-		$sql = "CREATE TABLE $table_name (
-			id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-			title VARCHAR(255) NOT NULL,
-			Departure BIGINT NOT NULL,
-			departure_id VARCHAR(20) NOT NULL,
-			package_id VARCHAR(20) NOT NULL,
-			option_id VARCHAR(10) NOT NULL,
-			spaces_available BIGINT NOT NULL,
-			adventure_option_term BIGINT NOT NULL,
-			price_per_person_usd BIGINT NOT NULL,
-			price_per_person_cad BIGINT NOT NULL,
-			price_per_person_aud BIGINT NOT NULL,
-			price_per_person_gbp BIGINT NOT NULL,
-			price_per_person_eur BIGINT NOT NULL
 		) $engine_collate";
 
 		// return the SQL.
