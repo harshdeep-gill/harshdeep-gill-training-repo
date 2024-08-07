769a22542c6aaeb236bacf1c3c91fb27184a6ba2
Deepak Kumar <deepak.kumar852182@gmail.com>
2024-08-07 16:45:24 +0530

    QE-374 prevent load term in ACF meta

--- wp-content/mu-plugins/quark/quark-departures/custom-fields/departures.php
+++ wp-content/mu-plugins/quark/quark-departures/custom-fields/departures.php
@@ -233,7 +233,7 @@ if ( function_exists( 'acf_add_local_field_group' ) ) :
 					'taxonomy'             => 'qrk_adventure_option_category',
 					'add_term'             => 0,
 					'save_terms'           => 0,
-					'load_terms'           => 1,
+					'load_terms'           => 0,
 					'return_format'        => 'object',
 					'field_type'           => 'multi_select',
 					'allow_null'           => 1,
