234d5e401fb08cdd101b1ed7ba7c9c9082577e93
Deepak Kumar <deepak.kumar852182@gmail.com>
2024-08-08 12:26:14 +0530

    QE-374 remove remove redundant tables

--- wp-content/mu-plugins/quark/quark-softrip/inc/class-softrip-db.php
+++ wp-content/mu-plugins/quark/quark-softrip/inc/class-softrip-db.php
@@ -21,8 +21,7 @@ class Softrip_DB {
 	 *
 	 * @return array{
 	 *      adventure_options: string,
-	 *      cabin_categories: string,
-	 *      occupancy_prices: string,
+	 *      cabins: string,
 	 *      promotions: string,
 	 * }
 	 */
@@ -30,33 +29,9 @@ class Softrip_DB {
 		// Return the list of tables used.
 		return [
 			'adventure_options' => get_adventure_table_sql(),
-			'cabin_categories'  => $this->get_cabin_table_sql(),
-			'occupancy_prices'  => $this->get_occupancy_prices_table_sql(),
 			'promotions'        => get_promotions_table_sql(),
 			'cabins'            => get_cabins_table_sql()
 		];
-	}
-
-	/**
-	 * Get the engine and collate.
-	 *
-	 * @return string
-	 */
-	private function engine_collate(): string {
-		// Get the $wpdb object.
-		global $wpdb;
-		$charset_collate = $wpdb->get_charset_collate();
-
-		// Set the engine and collate.
-		$engine_collate = 'ENGINE=InnoDB';
-
-		// If the charset_collate is not empty, add it to the engine_collate.
-		if ( ! empty( $charset_collate ) ) {
-			$engine_collate .= " $charset_collate";
-		}
-
-		// Return the engine and collate string.
-		return $engine_collate;
 	}
 
 	/**
@@ -69,63 +44,5 @@ class Softrip_DB {
 	public function prefix_table_name( string $name = '' ): string {
 		// Return the prefixed name.
 		return 'qrk_' . $name;
-	}
-
-	/**
-	 * Get the cabins table create SQL.
-	 *
-	 * @return string
-	 */
-	private function get_cabin_table_sql(): string {
-		// Get the table name.
-		$table_name = $this->prefix_table_name( 'cabin_categories' );
-
-		// Get the engine collate.
-		$engine_collate = $this->engine_collate();
-
-		// Build the SQL query.
-		$sql = "CREATE TABLE $table_name (
-			id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-			title VARCHAR(255) NOT NULL,
-			departure BIGINT NOT NULL,
-			cabin_category BIGINT NOT NULL,
-			package_id VARCHAR(45) NOT NULL,
-			departure_id VARCHAR(45) NOT NULL,
-			ship_code VARCHAR(10) NOT NULL,
-			cabin_category_id VARCHAR(45) NOT NULL,
-			availability_status VARCHAR(4) NOT NULL,
-			spaces_available INT NOT NULL,
-			UNIQUE KEY cabin_category_title_unique (title)
-		) $engine_collate";
-
-		// return the SQL.
-		return $sql;
-	}
-
-	/**
-	 * Get the occupancy_prices table create SQL.
-	 *
-	 * @return string
-	 */
-	private function get_occupancy_prices_table_sql(): string {
-		// Get the table name.
-		$table_name = $this->prefix_table_name( 'occupancy_prices' );
-
-		// Get the engine collate.
-		$engine_collate = $this->engine_collate();
-
-		// Build the SQL query.
-		$sql = "CREATE TABLE $table_name (
-			id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-			occupancy_id BIGINT NOT NULL,
-			currency_code VARCHAR(3) NOT NULL,
-			price_per_person DECIMAL(8, 2) NOT NULL,
-			total_price_per_person DECIMAL(8, 2) NOT NULL,
-			promotion_code VARCHAR(255) NOT NULL,
-			promo_price_per_person DECIMAL(8, 2) NOT NULL
-		) $engine_collate";
-
-		// return the SQL.
-		return $sql;
 	}
 }
