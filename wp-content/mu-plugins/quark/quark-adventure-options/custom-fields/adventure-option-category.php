f4b3ff8198e6006e5206a213fa2670632f62076d
Jignesh <jignesh.nakrani@rtcamp.com>
2024-08-13 12:32:37 +0530

    QE-354 allow adventure option image upload to main library

--- wp-content/mu-plugins/quark/quark-adventure-options/custom-fields/adventure-option-category.php
+++ wp-content/mu-plugins/quark/quark-adventure-options/custom-fields/adventure-option-category.php
@@ -54,7 +54,7 @@ if ( function_exists( 'acf_add_local_field_group' ) ) :
 						'id'    => '',
 					],
 					'return_format'     => 'array',
-					'library'           => 'uploadedTo',
+					'library'           => 'all',
 					'min_width'         => '',
 					'min_height'        => '',
 					'min_size'          => '',
