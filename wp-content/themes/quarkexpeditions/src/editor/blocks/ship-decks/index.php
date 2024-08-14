6096eeaf04ebdb9d9162766a4b38973ee4c4851c
Takshil Kunadia <takshilkunadia@gmail.com>
2024-08-14 16:45:09 +0530

    QE-349 Fix lint errors, static analysis suggestions

--- wp-content/themes/quarkexpeditions/src/editor/blocks/ship-decks/index.php
+++ wp-content/themes/quarkexpeditions/src/editor/blocks/ship-decks/index.php
@@ -7,9 +7,6 @@
 
 namespace Quark\Theme\Blocks\ShipDecks;
 
-use WP_Post;
-
-use function Quark\Expeditions\get_expedition_ship_ids;
 use function Quark\ShipDecks\get_deck_data;
 use function Quark\Ships\get_ship_data;
 
@@ -33,17 +30,11 @@ function bootstrap(): void {
 /**
  * Render this block.
  *
- * @param mixed[] $attributes The block attributes.
- *
  * @return string The block markup.
  */
-function render( array $attributes = [] ): string {
+function render(): string {
 	// Get ships ID.
 	$ship_id = absint( get_the_ID() );
-
-
-	// Get blog post cards data.
-	$ships_data = [];
 
 	// Get the ship data.
 	$ship = get_ship_data( $ship_id );
@@ -57,79 +48,76 @@ function render( array $attributes = [] ): string {
 	$ship_decks_ids = $ship['related_decks'];
 	$decks_data     = [];
 
-	// Get Decks associated with the ship.
-	if ( ! empty( $ship_decks_ids ) && is_array( $ship_decks_ids ) ) {
-		// Loop through the deck IDs.
-		foreach ( $ship_decks_ids as $deck_id ) {
-			// Get the deck data.
-			$deck_data = get_deck_data( $deck_id );
+	// Loop through the deck IDs.
+	foreach ( $ship_decks_ids as $deck_id ) {
+		// Get the deck data.
+		$deck_data = get_deck_data( $deck_id );
 
-			// Add labels to cabin options details.
-			if ( ! empty( $deck_data['cabin_options'] ) ) {
-				// Initialize cabin options data.
-				$cabin_options_data = [];
+		// Add labels to cabin options details.
+		if ( ! empty( $deck_data['cabin_options'] ) ) {
+			// Initialize cabin options data.
+			$cabin_options_data = [];
 
-				// Loop through the cabin options.
-				foreach ( $deck_data['cabin_options'] as $cabin_option ) {
-					// Initialize cabin option data.
-					$cabin_option_data            = $cabin_option;
-					$cabin_option_data['details'] = [];
+			// Loop through the cabin options.
+			foreach ( $deck_data['cabin_options'] as $cabin_option ) {
+				// Initialize cabin option data.
+				$cabin_option_data            = $cabin_option;
+				$cabin_option_data['details'] = [];
 
-					// Add Size.
-					if ( ! empty( $cabin_option['details']['size_from'] ) && ! empty( $cabin_option['details']['size_to'] ) ) {
-						$cabin_option_data['details'][] = [
-							'label' => __( 'Size', 'qrk' ),
-							'value' => $cabin_option['details']['size_from'] === $cabin_option['details']['size_to']
-								? $cabin_option['details']['size_from']
-								: $cabin_option['details']['size_from'] . ' - ' . $cabin_option['details']['size_to'],
-						];
-					}
-
-					// Add Occupancy.
-					if ( ! empty( $cabin_option['details']['occupancy_from'] ) && ! empty( $cabin_option['details']['occupancy_to'] ) ) {
-						$cabin_option_data['details'][] = [
-							'label' => __( 'Occupancy', 'qrk' ),
-							'value' => $cabin_option['details']['occupancy_from'] === $cabin_option['details']['occupancy_to']
-								? $cabin_option['details']['occupancy_from']
-								: $cabin_option['details']['occupancy_from'] . ' - ' . $cabin_option['details']['occupancy_to'],
-						];
-					}
-
-					// Add Bed configuration.
-					if ( ! empty( $cabin_option['details']['bed_configuration'] ) ) {
-						$cabin_option_data['details'][] = [
-							'label' => __( 'Bed Config.', 'qrk' ),
-							'value' => $cabin_option['details']['bed_configuration'],
-						];
-					}
-
-					// Add Class.
-					if ( ! empty( $cabin_option['details']['class'] ) ) {
-						$cabin_option_data['details'][] = [
-							'label' => __( 'Class', 'qrk' ),
-							'value' => $cabin_option['details']['class'],
-						];
-					}
-
-					// Add Location.
-					if ( ! empty( $cabin_option['details']['location'] ) ) {
-						$cabin_option_data['details'][] = [
-							'label' => __( 'Location', 'qrk' ),
-							'value' => $cabin_option['details']['location'],
-						];
-					}
-
-					// Add Data.
-					$cabin_options_data[] = $cabin_option_data;
+				// Add Size.
+				if ( ! empty( $cabin_option['details']['size_from'] ) && ! empty( $cabin_option['details']['size_to'] ) ) {
+					$cabin_option_data['details'][] = [
+						'label' => __( 'Size', 'qrk' ),
+						'value' => $cabin_option['details']['size_from'] === $cabin_option['details']['size_to']
+							? $cabin_option['details']['size_from']
+							: $cabin_option['details']['size_from'] . ' - ' . $cabin_option['details']['size_to'],
+					];
 				}
 
-				// Replace the cabin options data.
-				$deck_data['cabin_options'] = $cabin_options_data;
+				// Add Occupancy.
+				if ( ! empty( $cabin_option['details']['occupancy_from'] ) && ! empty( $cabin_option['details']['occupancy_to'] ) ) {
+					$cabin_option_data['details'][] = [
+						'label' => __( 'Occupancy', 'qrk' ),
+						'value' => $cabin_option['details']['occupancy_from'] === $cabin_option['details']['occupancy_to']
+							? $cabin_option['details']['occupancy_from']
+							: $cabin_option['details']['occupancy_from'] . ' - ' . $cabin_option['details']['occupancy_to'],
+					];
+				}
+
+				// Add Bed configuration.
+				if ( ! empty( $cabin_option['details']['bed_configuration'] ) ) {
+					$cabin_option_data['details'][] = [
+						'label' => __( 'Bed Config.', 'qrk' ),
+						'value' => $cabin_option['details']['bed_configuration'],
+					];
+				}
+
+				// Add Class.
+				if ( ! empty( $cabin_option['details']['class'] ) ) {
+					$cabin_option_data['details'][] = [
+						'label' => __( 'Class', 'qrk' ),
+						'value' => $cabin_option['details']['class'],
+					];
+				}
+
+				// Add Location.
+				if ( ! empty( $cabin_option['details']['location'] ) ) {
+					$cabin_option_data['details'][] = [
+						'label' => __( 'Location', 'qrk' ),
+						'value' => $cabin_option['details']['location'],
+					];
+				}
+
+				// Add Data.
+				$cabin_options_data[] = $cabin_option_data;
 			}
 
-			// Append the deck data to the ship data.
-			$decks_data[] = $deck_data;
+			// Replace the cabin options data.
+			$deck_data['cabin_options'] = $cabin_options_data;
 		}
+
+		// Append the deck data to the ship data.
+		$decks_data[] = $deck_data;
 	}
 
 	// Return built component.
