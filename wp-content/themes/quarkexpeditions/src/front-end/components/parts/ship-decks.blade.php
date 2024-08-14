a1a5835d04e6bd09d870f003ef633b600902875f
Takshil Kunadia <takshilkunadia@gmail.com>
2024-08-14 12:52:14 +0530

    QE-349 Replace description cards and use detail cards instead for publuc spaces

--- wp-content/themes/quarkexpeditions/src/front-end/components/parts/ship-decks.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/parts/ship-decks.blade.php
@@ -18,60 +18,64 @@
 		@foreach ( $decks as $deck )
 			<x-tabs.tab id="{{ $deck['id'] ?? '' }}">
 				@if ( ! empty( $deck['image_id'] ) )
-					<x-image :image_id="$deck['image_id']" :alt="$deck['title'] ?? ''" />
+					<figure>
+						<x-image :image_id="$deck['image_id']" :alt="$deck['title'] ?? ''" />
+					</figure>
 				@endif
+				<br />
 				<x-content :content="$deck['description'] ?? ''" />
 				@if ( ! empty( $deck['cabin_options'] ) )
-					<x-media-detail-cards title="{{ __( 'Cabin Options', 'qrk' ) }}">
+					<x-media-detail-cards title="{{ __( 'Cabin Details', 'qrk' ) }}">
 						@foreach ( $deck['cabin_options'] as $cabin_option )
 							<x-media-detail-cards.card>
-							<x-two-columns>
-								<x-two-columns.column>
-									@if ( ! empty( $cabin_option['image_id'] ) )
-										<x-media-detail-cards.image :image_id="$cabin_option['image_id']" :alt="$cabin_option['title'] ?? ''" />
-									@endif
-								</x-two-columns.column>
-								<x-two-columns.column>
-									<x-media-detail-cards.title :title="$cabin_option['title'] ?? ''" heading_level="5" />
-									@if ( ! empty( $cabin_option['description'] ) )
-										<x-media-detail-cards.content>
-											{!! $cabin_option['description'] ?? '' !!}
-										</x-media-detail-cards.content>
-									@endif
-									@if ( ! empty( $cabin_option['details'] ) )
-										<x-media-detail-cards.details>
-											@foreach ( $cabin_option['details'] as $detail )
-												<x-media-detail-cards.detail-item label="{{ $detail['label'] ?? '' }}" value="{!! $detail['value'] ?? '' !!}" />
-											@endforeach
-										</x-media-detail-cards.details>
-									@endif
-								</x-two-columns.column>
-							</x-two-columns>
+								<x-two-columns>
+									<x-two-columns.column>
+										@if ( ! empty( $cabin_option['image_id'] ) )
+											<x-media-detail-cards.image :image_id="$cabin_option['image_id']" :alt="$cabin_option['title'] ?? ''" />
+										@endif
+									</x-two-columns.column>
+									<x-two-columns.column>
+										<x-media-detail-cards.title :title="$cabin_option['title'] ?? ''" heading_level="5" />
+										@if ( ! empty( $cabin_option['description'] ) )
+											<x-media-detail-cards.content>
+												{!! $cabin_option['description'] ?? '' !!}
+											</x-media-detail-cards.content>
+										@endif
+										@if ( ! empty( $cabin_option['details'] ) )
+											<x-media-detail-cards.details>
+												@foreach ( $cabin_option['details'] as $detail )
+													<x-media-detail-cards.detail-item label="{{ $detail['label'] ?? '' }}" value="{!! $detail['value'] ?? '' !!}" />
+												@endforeach
+											</x-media-detail-cards.details>
+										@endif
+									</x-two-columns.column>
+								</x-two-columns>
 							</x-media-detail-cards.card>
 						@endforeach
 					</x-media-detail-cards>
 				@endif
 				@if ( ! empty( $deck['public_spaces'] ) )
-					<x-media-description-cards.title title="{{ __( 'Public Spaces & Amenities', 'qrk' ) }}" heading_level="4" />
-					<x-media-description-cards :desktop_carousel="true">
+					<x-media-detail-cards title="{{ __( 'Public Spaces', 'qrk' ) }}">
 						@foreach ( $deck['public_spaces'] as $public_spaces )
-							<x-media-description-cards.card>
+							<x-media-detail-cards.card>
 								<x-two-columns>
 									<x-two-columns.column>
-										<x-media-description-cards.image :image_id="$public_spaces['image'] ?? 0" :alt="$public_spaces['title'] ?? ''" />
+										@if ( ! empty( $public_spaces['image'] ) )
+											<x-media-description-cards.image :image_id="$public_spaces['image']" :alt="$public_spaces['title'] ?? ''" />
+										@endif
 									</x-two-columns.column>
 									<x-two-columns.column>
-										<x-media-description-cards.content>
-											<x-media-description-cards.title :title="$public_spaces['title'] ?? ''" heading_level="5" />
-											<x-media-description-cards.description>
+										<x-media-detail-cards.title :title="$public_spaces['title'] ?? ''" heading_level="5" />
+										@if ( ! empty( $public_spaces['description'] ) )
+											<x-media-detail-cards.content>
 												{!! $public_spaces['description'] ?? '' !!}
-											</x-media-description-cards.description>
-										</x-media-description-cards.content>
+											</x-media-detail-cards.content>
+										@endif
 									</x-two-columns.column>
 								</x-two-columns>
-							</x-media-description-cards.card>
+							</x-media-detail-cards.card>
 						@endforeach
-					</x-media-description-cards>
+					</x-media-detail-cards>
 				@endif
 			</x-tabs.tab>
 		@endforeach
