2fb7ef59b053267cee90a1abd5d432044072d25f
Takshil Kunadia <takshilkunadia@gmail.com>
2024-08-21 19:45:43 +0530

    QE-349 Fix: Media description cards

--- wp-content/themes/quarkexpeditions/src/front-end/components/parts/ship-decks.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/parts/ship-decks.blade.php
@@ -28,28 +28,22 @@
 					<x-media-detail-cards title="{{ __( 'Cabin Details', 'qrk' ) }}">
 						@foreach ( $deck['cabin_options'] as $cabin_option )
 							<x-media-detail-cards.card>
-								<x-two-columns :stack_on_tablet="true">
-									<x-two-columns.column>
-										@if ( ! empty( $cabin_option['image_id'] ) )
-											<x-media-detail-cards.image :image_id="$cabin_option['image_id']" :alt="$cabin_option['title'] ?? ''" />
-										@endif
-									</x-two-columns.column>
-									<x-two-columns.column>
-										<x-media-detail-cards.title :title="$cabin_option['title'] ?? ''" heading_level="5" />
-										@if ( ! empty( $cabin_option['description'] ) )
-											<x-media-detail-cards.content>
-												{!! $cabin_option['description'] ?? '' !!}
-											</x-media-detail-cards.content>
-										@endif
-										@if ( ! empty( $cabin_option['details'] ) )
-											<x-media-detail-cards.details>
-												@foreach ( $cabin_option['details'] as $detail )
-													<x-media-detail-cards.detail-item label="{{ $detail['label'] ?? '' }}" value="{!! $detail['value'] ?? '' !!}" />
-												@endforeach
-											</x-media-detail-cards.details>
-										@endif
-									</x-two-columns.column>
-								</x-two-columns>
+								@if ( ! empty( $cabin_option['image_id'] ) )
+									<x-media-detail-cards.image :image_id="$cabin_option['image_id']" :alt="$cabin_option['title'] ?? ''" />
+								@endif
+								<x-media-detail-cards.content>
+									<x-media-detail-cards.title :title="$cabin_option['title'] ?? ''" heading_level="5" />
+									@if ( ! empty( $cabin_option['description'] ) )
+											{!! $cabin_option['description'] ?? '' !!}
+									@endif
+									@if ( ! empty( $cabin_option['details'] ) )
+										<x-media-detail-cards.details>
+											@foreach ( $cabin_option['details'] as $detail )
+												<x-media-detail-cards.detail-item label="{{ $detail['label'] ?? '' }}" value="{!! $detail['value'] ?? '' !!}" />
+											@endforeach
+										</x-media-detail-cards.details>
+									@endif
+								</x-media-detail-cards.content>
 							</x-media-detail-cards.card>
 						@endforeach
 					</x-media-detail-cards>
@@ -58,21 +52,15 @@
 					<x-media-detail-cards title="{{ __( 'Public Spaces', 'qrk' ) }}">
 						@foreach ( $deck['public_spaces'] as $public_spaces )
 							<x-media-detail-cards.card>
-								<x-two-columns :stack_on_tablet="true">
-									<x-two-columns.column>
-										@if ( ! empty( $public_spaces['image'] ) )
-											<x-media-description-cards.image :image_id="$public_spaces['image']" :alt="$public_spaces['title'] ?? ''" />
-										@endif
-									</x-two-columns.column>
-									<x-two-columns.column>
+								@if ( ! empty( $public_spaces['image'] ) )
+									<x-media-description-cards.image :image_id="$public_spaces['image']" :alt="$public_spaces['title'] ?? ''" />
+								@endif
+								@if ( ! empty( $public_spaces['description'] ) )
+									<x-media-detail-cards.content>
 										<x-media-detail-cards.title :title="$public_spaces['title'] ?? ''" heading_level="5" />
-										@if ( ! empty( $public_spaces['description'] ) )
-											<x-media-detail-cards.content>
-												{!! $public_spaces['description'] ?? '' !!}
-											</x-media-detail-cards.content>
-										@endif
-									</x-two-columns.column>
-								</x-two-columns>
+										{!! $public_spaces['description'] ?? '' !!}
+									</x-media-detail-cards.content>
+								@endif
 							</x-media-detail-cards.card>
 						@endforeach
 					</x-media-detail-cards>
