60acf8d2695ad9c72ad20b006cad0e2f568d10e4
Takshil Kunadia <takshilkunadia@gmail.com>
2024-08-22 17:47:49 +0530

    QE-442 Add Expeditions editor controls

---
+++ wp-content/themes/quarkexpeditions/src/editor/blocks/expeditions/index.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Block Name: Expeditions.
+ *
+ * @package quark
+ */
+
+namespace Quark\Theme\Blocks\Expeditions;
+
+use WP_Block;
+
+const COMPONENT = 'parts.expeditions';
+
+/**
+ * Bootstrap this block.
+ *
+ * @return void
+ */
+function bootstrap(): void {
+	// Register the block.
+	register_block_type_from_metadata(
+		__DIR__,
+		[
+			'render_callback' => __NAMESPACE__ . '\\render',
+		]
+	);
+}
+
+/**
+ * Render this block.
+ *
+ * @param mixed[]  $attributes The block attributes.
+ * @param string   $content    The block content.
+ * @param WP_Block $block      The block instance.
+ *
+ * @return string The block markup.
+ */
+function render( array $attributes = [], string $content = '', WP_Block $block = null ): string {
+	// Check for block.
+	if ( ! $block instanceof WP_Block ) {
+		return $content;
+	}
+
+	// Render the block.
+	return $content;
+}
