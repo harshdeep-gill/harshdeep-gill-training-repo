4ef6477f8b0a691e4930ef204c5ace8c4cd7048a
Prithwiraj Ghosh <prithwiraj10@gmail.com>
2024-08-26 18:11:39 +0530

    QE-353 refactor: add fallbacks and translations

--- wp-content/themes/quarkexpeditions/src/front-end/components/book-departures-ships/filters/index.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/book-departures-ships/filters/index.blade.php
@@ -16,16 +16,16 @@
 	</x-form.field>
 	<x-form.field class="book-departures-ships__filters-sort">
 		<x-form.inline-dropdown label="Sort">
-			<x-form.option value="date-now" label="Date (upcoming to later)" selected="yes">
+			<x-form.option value="date-now" label="{{ __( 'Date (upcoming to later)', 'qrk' ) }}" selected="yes">
 				{{ __( 'Date (upcoming to later)', 'qrk' ) }}
 			</x-form.option>
-			<x-form.option value="date-later" label="Date (later to upcoming)">
+			<x-form.option value="date-later" label="{{ __( 'Date (later to upcoming)', 'qrk' ) }}">
 				{{ __( 'Date (later to upcoming)', 'qrk' ) }}
 			</x-form.option>
-			<x-form.option value="price-low" label="Price (low to high)">
+			<x-form.option value="price-low" label="{{ __( 'Price (low to high)', 'qrk' )  }}">
 				{{ __( 'Price (low to high)', 'qrk' ) }}
 			</x-form.option>
-			<x-form.option value="price-high" label="Price (high to low)">
+			<x-form.option value="price-high" label="{{ __( 'Price (high to low)', 'qrk' )  }}">
 				{{ __( 'Price (high to low)', 'qrk' ) }}
 			</x-form.option>
 		</x-form.inline-dropdown>
--- wp-content/themes/quarkexpeditions/src/front-end/components/parts/ship-departure-cards.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/parts/ship-departure-cards.blade.php
@@ -80,15 +80,17 @@
 								<x-departure-cards.specification-value>
 									<x-departure-cards.adventure-options>
 										@foreach( $card['paid_adventure_options'] as $option )
-											<x-departure-cards.adventure-option title="{{ $option }}"/>
+											<x-departure-cards.adventure-option title="{{ $option ?? '' }}"/>
 										@endforeach
 
 										<x-departure-cards.adventure-options-tooltip>
-											<ul>
-												@foreach( $card['paid_adventure_options'] as $option )
-													<li>{{ $option }}</li>
-												@endforeach
-											</ul>
+											@if ( ! empty( $card['paid_adventure_options'] ) )
+												<ul>
+													@foreach( $card['paid_adventure_options'] as $option )
+														<li>{{ $option }}</li>
+													@endforeach
+												</ul>
+											@endif
 										</x-departure-cards.adventure-options-tooltip>
 									</x-departure-cards.adventure-options>
 								</x-departure-cards.specification-value>
@@ -99,7 +101,7 @@
 					@if ( ! empty( $card['available_offers'] ) )
 						<x-departure-cards.offers title="Available Offers">
 							@foreach( $card['available_offers'] as $offer )
-								<x-departure-cards.offer title="{{ $offer }}"/>
+								<x-departure-cards.offer title="{{ $offer ?? '' }}"/>
 							@endforeach
 
 							<x-departure-cards.offers-modal title="{{ $card['expedition_name'] ?? '' }}">
@@ -119,147 +121,151 @@
 						discounted_price="{{ $card['lowest_price']['discounted_price'] ?? '' }}"
 					/>
 
-					@if( ! empty( $card['transfer_package_details'] && ! empty( $card['transfer_package_details']['sets'] ) ) )
+					@if ( ! empty( $card['transfer_package_details'] && ! empty( $card['transfer_package_details']['sets'] ) ) )
 						<x-departure-cards.transfer_package
 							drawer_id="{{ $departure_id }}"
 							drawer_title="Mandatory Transfer Package"
 						>
 							<p><strong>{{ $card['transfer_package_details']['title'] ?? '' }}</strong></p>
-							<ul>
-								@foreach( $card['transfer_package_details']['sets'] as $set )
-									<li>{!! $set !!}</li>
-								@endforeach
-							</ul>
-							<p><strong>Package
-									Price: {{ $card['transfer_package_details']['formatted_price'] ?? '' }}</strong></p>
+								<ul>
+									@foreach( $card['transfer_package_details']['sets'] as $set )
+										<li>{!! $set !!}</li>
+									@endforeach
+								</ul>
+							<p>
+								<strong>
+									{{ __( 'Package Price:', 'qrk') }} {{ $card['transfer_package_details']['formatted_price'] ?? '' }}
+								</strong>
+							</p>
 						</x-departure-cards.transfer_package>
 					@endif
-					<x-departure-cards.cta text="View Cabin Pricing & Options"/>
+					<x-departure-cards.cta text="{{ __( 'View Cabin Pricing & Options', 'qrk' ) }}"/>
 				</x-departure-cards.body-column>
 			</x-departure-cards.body>
 
-			@if( ! empty( $card['cabins'] ) && is_array( $card['cabins'] ) ) @endif
-			<x-departure-cards.more-details>
-				<h4>
-					<x-escape :content="__( 'Cabins Options', 'qrk' )"/>
-				</h4>
-				<x-product-options-cards>
-					<x-product-options-cards.cards>
-						@foreach( $card['cabins'] as $cabin_code => $cabin )
-							<x-product-options-cards.card details_id="{{  $cabin_code . '_' . $departure_id }}">
+			@if ( ! empty( $card['cabins'] ) && is_array( $card['cabins'] ) )
+				<x-departure-cards.more-details>
+					<h4>
+						<x-escape :content="__( 'Cabins Options', 'qrk' )"/>
+					</h4>
+					<x-product-options-cards>
+						<x-product-options-cards.cards>
+							@foreach( $card['cabins'] as $cabin_code => $cabin )
+								<x-product-options-cards.card details_id="{{  $cabin_code . '_' . $departure_id }}">
 
-								@if( ! empty( $cabin['gallery'] ) )
-									<x-product-options-cards.gallery :image_ids="$cabin['gallery']">
-										<x-product-options-cards.badge
-											type="{{ $cabin['type'] ?? __( 'standard', 'qrk' ) }}"
-										/>
-									</x-product-options-cards.gallery>
-								@endif
-
-								<x-product-options-cards.content>
-									<x-product-options-cards.title title="{{ $cabin['name'] ?? '' }}"/>
-
-									@if( ! empty( $cabin['specifications'] ) && is_array( $cabin['specifications'] ) )
-										<x-product-options-cards.specifications>
-											@if( ! empty( $cabin['specifications']['occupancy'] ) )
-												<x-product-options-cards.specification
-													label="Occupancy"
-													value="{{ $cabin['specifications']['occupancy'] }}"
-												/>
-											@endif
-
-											@if( ! empty( $cabin['specifications']['bed_configuration'] ) )
-												<x-product-options-cards.specification
-													label="Number of Beds"
-													value="{{ $cabin['specifications']['bed_configuration'] }}"
-												/>
-											@endif
-
-											@if( ! empty( $cabin['specifications']['location'] ) )
-												<x-product-options-cards.specification
-													label="Location"
-													value="{{ $cabin['specifications']['location'] }}"
-												/>
-											@endif
-
-											@if( ! empty( $cabin['specifications']['size'] ) )
-												<x-product-options-cards.specification
-													label="Cabin Size"
-													value="{{ $cabin['specifications']['size'] }}"
-												/>
-											@endif
-										</x-product-options-cards.specifications>
+									@if( ! empty( $cabin['gallery'] ) )
+										<x-product-options-cards.gallery :image_ids="$cabin['gallery']">
+											<x-product-options-cards.badge
+												type="{{ $cabin['type'] ?? __( 'standard', 'qrk' ) }}"
+											/>
+										</x-product-options-cards.gallery>
 									@endif
 
-									@if( ! empty( $cabin['from_price'] ) && is_array( $cabin['from_price'] ) )
-										<x-product-options-cards.price
-											original_price="{{ $cabin['from_price']['original_price'] ?? '' }}"
-											discounted_price="{{ $cabin['from_price']['discounted_price'] ?? '' }}"
-										/>
-									@endif
-								</x-product-options-cards.content>
-							</x-product-options-cards.card>
-						@endforeach
-					</x-product-options-cards.cards>
+									<x-product-options-cards.content>
+										<x-product-options-cards.title title="{{ $cabin['name'] ?? '' }}"/>
 
-					<x-product-options-cards.more-details>
-						@foreach( $card['cabins'] as $cabin_code => $cabin )
-							<x-product-options-cards.card-details
-								id="{{ $cabin_code . '_' . $departure_id }}">
-
-								@if( $cabin['name'] )
-									<x-product-options-cards.card-details-title title="{{ $cabin['name'] }}"/>
-								@endif
-
-								@if( ! empty( $cabin['description'] ) )
-									<x-product-options-cards.description>
-										{!! $cabin['description'] !!}
-									</x-product-options-cards.description>
-								@endif
-
-								@if( ! empty( $cabin['gallery'] ) )
-									<x-product-options-cards.gallery :image_ids="$cabin['gallery']" :full_size="true"/>
-								@endif
-
-								@if( ! empty( $cabin['occupancies'] ) && is_array( $cabin['occupancies'] ) )
-									<x-product-options-cards.rooms title="Select Rooms">
-										@foreach( $cabin['occupancies'] as $occupancy )
-											<x-product-options-cards.room>
-												<x-product-options-cards.room-title-container>
-													@if( ! empty( $occupancy['description'] ) )
-														<x-product-options-cards.room-title
-															title="{{ $occupancy['description'] }}"
-															no_of_guests="{{ $occupancy['no_of_guests'] ?? '' }}"
-														/>
-													@endif
-													<x-product-options-cards.room-subtitle subtitle="Price of the cabin for one guest"/>
-												</x-product-options-cards.room-title-container>
-
-												@if( ! empty( $occupancy['price'] ) && is_array( $occupancy['price'] ) )
-													<x-product-options-cards.room-prices
-														original_price="{{ $occupancy['price']['original_price'] }}"
-														discounted_price="{{ $occupancy['price']['discounted_price'] }}"
+										@if( ! empty( $cabin['specifications'] ) && is_array( $cabin['specifications'] ) )
+											<x-product-options-cards.specifications>
+												@if( ! empty( $cabin['specifications']['occupancy'] ) )
+													<x-product-options-cards.specification
+														label="Occupancy"
+														value="{{ $cabin['specifications']['occupancy'] }}"
 													/>
 												@endif
-											</x-product-options-cards.room>
-										@endforeach
-									</x-product-options-cards.rooms>
-								@endif
 
-								@if( ! empty( $cabin['promotions'] ) && is_array( $cabin['promotions'] ) )
-									<x-product-options-cards.discounts>
-										@foreach( $cabin['promotions'] as $promotion )
-											@if( ! empty( $promotion ) )
-												<x-product-options-cards.discount name="{{ $promotion }}"/>
-											@endif
-										@endforeach
-									</x-product-options-cards.discounts>
-								@endif
-							</x-product-options-cards.card-details>
-						@endforeach
-					</x-product-options-cards.more-details>
-				</x-product-options-cards>
-			</x-departure-cards.more-details>
+												@if( ! empty( $cabin['specifications']['bed_configuration'] ) )
+													<x-product-options-cards.specification
+														label="Number of Beds"
+														value="{{ $cabin['specifications']['bed_configuration'] }}"
+													/>
+												@endif
+
+												@if( ! empty( $cabin['specifications']['location'] ) )
+													<x-product-options-cards.specification
+														label="Location"
+														value="{{ $cabin['specifications']['location'] }}"
+													/>
+												@endif
+
+												@if( ! empty( $cabin['specifications']['size'] ) )
+													<x-product-options-cards.specification
+														label="Cabin Size"
+														value="{{ $cabin['specifications']['size'] }}"
+													/>
+												@endif
+											</x-product-options-cards.specifications>
+										@endif
+
+										@if( ! empty( $cabin['from_price'] ) && is_array( $cabin['from_price'] ) )
+											<x-product-options-cards.price
+												original_price="{{ $cabin['from_price']['original_price'] ?? '' }}"
+												discounted_price="{{ $cabin['from_price']['discounted_price'] ?? '' }}"
+											/>
+										@endif
+									</x-product-options-cards.content>
+								</x-product-options-cards.card>
+							@endforeach
+						</x-product-options-cards.cards>
+
+						<x-product-options-cards.more-details>
+							@foreach( $card['cabins'] as $cabin_code => $cabin )
+								<x-product-options-cards.card-details
+									id="{{ $cabin_code . '_' . $departure_id }}">
+
+									@if( $cabin['name'] )
+										<x-product-options-cards.card-details-title title="{{ $cabin['name'] }}"/>
+									@endif
+
+									@if( ! empty( $cabin['description'] ) )
+										<x-product-options-cards.description>
+											{!! $cabin['description'] !!}
+										</x-product-options-cards.description>
+									@endif
+
+									@if( ! empty( $cabin['gallery'] ) )
+										<x-product-options-cards.gallery :image_ids="$cabin['gallery']" :full_size="true"/>
+									@endif
+
+									@if( ! empty( $cabin['occupancies'] ) && is_array( $cabin['occupancies'] ) )
+										<x-product-options-cards.rooms title="Select Rooms">
+											@foreach( $cabin['occupancies'] as $occupancy )
+												<x-product-options-cards.room>
+													<x-product-options-cards.room-title-container>
+														@if( ! empty( $occupancy['description'] ) )
+															<x-product-options-cards.room-title
+																title="{{ $occupancy['description'] }}"
+																no_of_guests="{{ $occupancy['no_of_guests'] ?? '' }}"
+															/>
+														@endif
+														<x-product-options-cards.room-subtitle subtitle="Price of the cabin for one guest"/>
+													</x-product-options-cards.room-title-container>
+
+													@if( ! empty( $occupancy['price'] ) && is_array( $occupancy['price'] ) )
+														<x-product-options-cards.room-prices
+															original_price="{{ $occupancy['price']['original_price'] }}"
+															discounted_price="{{ $occupancy['price']['discounted_price'] }}"
+														/>
+													@endif
+												</x-product-options-cards.room>
+											@endforeach
+										</x-product-options-cards.rooms>
+									@endif
+
+									@if( ! empty( $cabin['promotions'] ) && is_array( $cabin['promotions'] ) )
+										<x-product-options-cards.discounts>
+											@foreach( $cabin['promotions'] as $promotion )
+												@if( ! empty( $promotion ) )
+													<x-product-options-cards.discount name="{{ $promotion }}"/>
+												@endif
+											@endforeach
+										</x-product-options-cards.discounts>
+									@endif
+								</x-product-options-cards.card-details>
+							@endforeach
+						</x-product-options-cards.more-details>
+					</x-product-options-cards>
+				</x-departure-cards.more-details>
+			@endif
 		</x-departure-cards.card>
 	@endforeach
 </x-departure-cards>
