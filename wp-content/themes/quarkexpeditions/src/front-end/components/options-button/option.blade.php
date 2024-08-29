b1743259e0aafbb2f9c3aed26956f3d691d592d6
Sagar Jadhav <sagarjadhav27@gmail.com>
2024-08-29 15:19:52 +0530

    QE-539 Component: Options Button

--- wp-content/themes/quarkexpeditions/src/front-end/components/options-button/option.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/options-button/option.blade.php
@@ -9,7 +9,7 @@
 @endphp
 
 <li class="options-button__option">
-	<x-maybe-link href="{{ $url }}">
+	<x-maybe-link href="{{ $url }}" class="options-button__option-link">
 		{!! $slot !!}
 	</x-maybe-link>
 </li>
