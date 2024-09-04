0cf45ad9532ec3e77323fa96cbf46a6462045262
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-09-05 02:26:09 +0530

    QE-429 Decouple remaining filters

---
+++ wp-content/themes/quarkexpeditions/src/front-end/components/dates-rates/filters/adventure-options.blade.php
@@ -0,0 +1,17 @@
+@props( [
+	'adventure_options' => [],
+] )
+
+@php
+	if ( empty( $adventure_options ) || ! is_array( $adventure_options ) ) {
+		return;
+	}
+@endphp
+
+<quark-dates-rates-filter-adventure-options>
+	<x-form.field-group>
+		@foreach ( $adventure_options as $filter_value => $filter_label )
+			<x-dates-rates.filters.checkbox name="adventure_options" :label="$filter_label" :value="$filter_value" data-label="{!! $filter_label !!}" />
+		@endforeach
+	</x-form.field-group>
+</quark-dates-rates-filter-adventure-options>
