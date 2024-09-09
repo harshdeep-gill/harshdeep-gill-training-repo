4cc8c92a060f5195a504b7d63ca12bfc17ec48f0
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-09-09 22:54:58 +0530

    QE-615 Remove checked/selected from currency selectors

--- wp-content/themes/quarkexpeditions/src/front-end/components/dates-rates/filters/currency-radios.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/dates-rates/filters/currency-radios.blade.php
@@ -1,6 +1,6 @@
 <quark-dates-rates-filter-currency-radios>
 	<x-form.field-group>
-		<x-form.radio name="currency" value="USD" label="$ USD" checked />
+		<x-form.radio name="currency" value="USD" label="$ USD" />
 		<x-form.radio name="currency" value="CAD" label="$ CAD" />
 		<x-form.radio name="currency" value="AUD" label="$ AUD" />
 		<x-form.radio name="currency" value="GBP" label="$ GBP" />
