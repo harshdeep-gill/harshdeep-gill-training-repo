26602ee1d56f31a61805a098e30ab71cad56be12
Takshil Kunadia <takshilkunadia@gmail.com>
2024-09-11 15:24:26 +0530

    QE-623 Add Media Text CTA Caroursel Block

---
+++ wp-content/themes/quarkexpeditions/src/editor/blocks/media-text-cta-carousel/edit.tsx
@@ -0,0 +1,49 @@
+/**
+ * WordPress dependencies
+ */
+import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
+ * Internal dependencies
+ */
+import * as mediaTextCta from '../media-text-cta';
+
+/**
+ * Edit component.
+ *
+ * @param {Object} props           Component properties.
+ * @param {Object} props.className Class name.
+ */
+export default function edit( { className }: BlockEditAttributes ): JSX.Element {
+	// Set the block props.
+	const blockProps = useBlockProps( {
+		className: classnames( className, 'media-text-cta-carousel' ),
+	} );
+
+	// Set the inner blocks props.
+	const innerBlockProps = useInnerBlocksProps( {
+		className: classnames( 'media-text-cta-carousel__slides' ),
+	},
+	{
+		allowedBlocks: [ mediaTextCta.name ],
+		template: [
+			[ mediaTextCta.name ],
+			[ mediaTextCta.name ],
+		],
+	} );
+
+	// Return the block's markup.
+	return (
+		<div { ...blockProps } >
+			<div className={ 'media-text-cta-carousel__track' }>
+				<div { ...innerBlockProps } />
+			</div>
+		</div>
+	);
+}
