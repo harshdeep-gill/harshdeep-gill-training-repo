69bf9c9b7d265a4fb0a883ceddc4d492237fe1f1
Takshil Kunadia <takshilkunadia@gmail.com>
2024-09-11 15:43:43 +0530

    QE-588 Refactor block styles & part

--- wp-content/themes/quarkexpeditions/src/front-end/components/parts/expeditions.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/parts/expeditions.blade.php
@@ -14,9 +14,6 @@
 			@if ( ! empty( $card['image_id'] ) )
 				<x-product-cards.image :image_id="$card['image_id']" />
 			@endif
-			@if ( ! empty( $card['review_rating'] ) && ! empty( $card['total_reviews'] ) )
-				<x-product-cards.reviews :review_rating="$card['review_rating']" :total_reviews="$card['total_reviews']" />
-			@endif
 			@if ( ! empty(  $card['itinerary_days'] ) )
 				@php
 					$duration = sprintf( __( '%d day itinerary', 'qrk' ), $card['itinerary_days'] )
@@ -25,14 +22,38 @@
 			@endif
 
 			@if ( ! empty( $card['title'] ) )
-				<x-product-cards.title :title="$card['title']" />
+				<x-maybe-link href="{{ $card['url'] }}">
+					<x-product-cards.title :title="$card['title']" />
+				</x-maybe-link>
 			@endif
 
 			@if ( ! empty( $card['original_price'] ) && ! empty( $card['discounted_price'] ) )
 				<x-product-cards.price
+					:title="__( 'Starting from (per person)', 'qrk' )"
 					:original_price="$card['original_price'] ?? ''"
 					:discounted_price="$card['discounted_price'] ?? ''"
 				/>
+			@endif
+
+			@if ( ! empty( $card['transfer_package'] ) )
+				<x-product-cards.transfer_package
+					drawer_id="transfer-package-{{ $loop->index }}"
+					drawer_title="{{ __( 'Mandatory Transfer package', 'qrk' ) }}"
+				>
+					<strong>{{ $card['transfer_package']['title'] }}</strong>
+
+					<ul>
+						@foreach ( $card['transfer_package']['sets'] as $item )
+							<li>{{ $item }}</li>
+						@endforeach
+					</ul>
+
+					@php
+						$price = sprintf( __( 'Package Price: %s', 'qrk' ), $card['transfer_package']['formatted_price'] );
+					@endphp
+					<strong> {{ $price }}</strong>
+
+				</x-product-cards.transfer-package>
 			@endif
 		</x-product-cards.card>
 	@endforeach
