d6df5622d9229bbbd9446f4eca98665fdfccb90d
Takshil Kunadia <takshilkunadia@gmail.com>
2024-09-17 14:19:34 +0530

    QE-643 Update block to process child block

--- wp-content/themes/quarkexpeditions/src/editor/blocks/cta-banner/edit.tsx
+++ wp-content/themes/quarkexpeditions/src/editor/blocks/cta-banner/edit.tsx
@@ -3,7 +3,7 @@
  */
 import { InspectorControls, useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
-import { PanelBody, ToggleControl } from '@wordpress/components';
+import { PanelBody, SelectControl } from '@wordpress/components';
 
 /**
  * External dependencies
@@ -14,7 +14,12 @@ const { gumponents } = window;
 /**
  * External components.
  */
-const { ImageControl, Img } = gumponents.components;
+const { ImageControl, Img, ColorPaletteControl } = gumponents.components;
+
+// Background colors.
+export const colors: { [key: string]: string }[] = [
+	{ name: __( 'Gray', 'qrk' ), color: '#F5F7FB', slug: 'gray' },
+];
 
 /**
  * Edit component.
@@ -30,7 +35,11 @@ export default function Edit( { className, attributes, setAttributes }: BlockEdi
 		className: classnames(
 			className,
 			'media-cta-banner',
-			attributes.darkMode ? 'media-cta-banner--dark color-context--dark' : 'media-cta-banner--light',
+			{
+				[ `media-cta-banner--${ attributes.appearance }` ]: attributes.appearance,
+				'color-context--dark': 'dark' === attributes.appearance,
+				[ `media-cta-banner--has-background-${ attributes.backgroundColor }` ]: attributes.backgroundColor && 'solid' === attributes.appearance,
+			}
 		),
 	} );
 
@@ -39,8 +48,9 @@ export default function Edit( { className, attributes, setAttributes }: BlockEdi
 		className: classnames( 'media-cta-banner__content' ),
 	},
 	{
-		allowedBlocks: [ 'core/paragraph', 'core/heading', 'quark/buttons' ],
+		allowedBlocks: [ 'quark/cta-banner-overline', 'core/paragraph', 'core/heading', 'quark/buttons' ],
 		template: [
+			[ 'quark/cta-banner-overline' ],
 			[ 'core/heading' ],
 			[ 'core/paragraph', { placeholder: __( 'Write description…', 'qrk' ) } ],
 			[ 'quark/buttons' ],
@@ -52,18 +62,42 @@ export default function Edit( { className, attributes, setAttributes }: BlockEdi
 		<>
 			<InspectorControls>
 				<PanelBody title={ __( 'CTA Banner Options', 'qrk' ) }>
-					<ImageControl
-						label={ __( 'Background Image', 'qrk' ) }
-						value={ attributes.backgroundImage ? attributes.backgroundImage.id : null }
-						size="large"
-						help={ __( 'Choose an image for this collage item.', 'qrk' ) }
-						onChange={ ( backgroundImage: object ) => setAttributes( { backgroundImage } ) }
+					<SelectControl
+						label={ __( 'Appearance', 'qrk' ) }
+						value={ attributes.appearance }
+						options={ [
+							{ label: __( 'Light', 'qrk' ), value: 'light' },
+							{ label: __( 'Dark', 'qrk' ), value: 'dark' },
+							{ label: __( 'Solid', 'qrk' ), value: 'solid' },
+						] }
+						onChange={ ( appearance: string ) => setAttributes( { appearance } ) }
 					/>
-					<ToggleControl
-						label={ __( 'Dark Mode?', 'qrk' ) }
-						checked={ attributes.darkMode }
-						onChange={ ( darkMode: boolean ) => setAttributes( { darkMode } ) }
-					/>
+					{ 'solid' !== attributes.appearance &&
+						<ImageControl
+							label={ __( 'Background Image', 'qrk' ) }
+							value={ attributes.backgroundImage ? attributes.backgroundImage.id : null }
+							size="large"
+							help={ __( 'Choose an image for this collage item.', 'qrk' ) }
+							onChange={ ( backgroundImage: object ) => setAttributes( { backgroundImage } ) }
+						/>
+					}
+					{ 'solid' === attributes.appearance &&
+						<ColorPaletteControl
+							label={ __( 'Background Color', 'qrk' ) }
+							help={ __( 'Select the background color.', 'qrk' ) }
+							value={ colors.find( ( color ) => color.slug === attributes.backgroundColor )?.color }
+							colors={ colors.filter( ( color ) => [ 'gray' ].includes( color.slug ) ) }
+							onChange={ ( backgroundColor: {
+								color: string;
+								slug: string;
+							} ): void => {
+								// Set the background color attribute.
+								if ( backgroundColor.slug && [ 'gray' ].includes( backgroundColor.slug ) ) {
+									setAttributes( { backgroundColor: backgroundColor.slug } );
+								}
+							} }
+						/>
+					}
 				</PanelBody>
 			</InspectorControls>
 			<div { ...blockProps } >
