299fa02eae6cbde580700557adb73b366b59f93c
Deepak Kumar <deepak.kumar852182@gmail.com>
2024-09-18 02:24:34 +0530

    QE-543 include cabin class and travelers filter in sidebar

--- wp-content/themes/quarkexpeditions/inc/search-filters.php
+++ wp-content/themes/quarkexpeditions/inc/search-filters.php
@@ -10,6 +10,7 @@ namespace Quark\Theme\Search_Filters;
 use function Quark\Search\Filters\build_filter_options;
 
 use const Quark\Search\Filters\ADVENTURE_OPTION_FILTER_KEY;
+use const Quark\Search\Filters\CABIN_CLASS_FILTER_KEY;
 use const Quark\Search\Filters\DESTINATION_FILTER_KEY;
 use const Quark\Search\Filters\DURATION_FILTER_KEY;
 use const Quark\Search\Filters\EXPEDITION_FILTER_KEY;
@@ -18,13 +19,14 @@ use const Quark\Search\Filters\LANGUAGE_FILTER_KEY;
 use const Quark\Search\Filters\MONTH_FILTER_KEY;
 use const Quark\Search\Filters\SEASON_FILTER_KEY;
 use const Quark\Search\Filters\SHIP_FILTER_KEY;
+use const Quark\Search\Filters\TRAVELERS_FILTER_KEY;
 
 /**
  * Get filter options for dates rates.
  *
  * @param mixed[] $selected_filter Selected filters.
  *
- * @return array<string, array<int, array{label: string, value: string|int, count: int}>>
+ * @return array<string, array<int, array{label: string, value: string|int, count?:int, children?: array<int, array{label: string, value:int|string, count?:int, parent_id: int|string}>}>>
  */
 function get_filters_for_dates_rates( array $selected_filter = [] ): array {
 	// Available filters on dates-rates.
@@ -47,7 +49,7 @@ function get_filters_for_dates_rates( array $selected_filter = [] ): array {
 /**
  * Get filter options for sidebar search filters.
  *
- * @return array<string, array<int, array{label: string, value: string|int, count: int}>>
+ * @return array<string, array<int, array{label: string, value: string|int, count?:int, children?: array<int, array{label: string, value:int|string, count?:int, parent_id: int|string}>}>>
  */
 function get_filters_for_sidebar_search(): array {
 	// Available filters on sidebar.
@@ -59,6 +61,8 @@ function get_filters_for_sidebar_search(): array {
 		SHIP_FILTER_KEY,
 		ITINERARY_LENGTH_FILTER_KEY,
 		LANGUAGE_FILTER_KEY,
+		CABIN_CLASS_FILTER_KEY,
+		TRAVELERS_FILTER_KEY,
 	];
 
 	// Build filter options.
