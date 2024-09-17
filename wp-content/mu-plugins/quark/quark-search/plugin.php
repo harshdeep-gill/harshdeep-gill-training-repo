a980a0b587bd404bef5dcc2fd3a7a2e02ba93c81
Deepak Kumar <deepak.kumar852182@gmail.com>
2024-09-18 01:19:34 +0530

    QE-543 trigger bootstrap function from plugin file

--- wp-content/mu-plugins/quark/quark-search/plugin.php
+++ wp-content/mu-plugins/quark/quark-search/plugin.php
@@ -19,3 +19,4 @@ require_once __DIR__ . '/inc/filters/namespace.php';
 // Kick it off.
 add_action( 'plugins_loaded', __NAMESPACE__ . '\\bootstrap' );
 add_action( 'plugins_loaded', __NAMESPACE__ . '\\Departures\\bootstrap' );
+add_action( 'plugins_loaded', __NAMESPACE__ . '\\Filters\\bootstrap' );
