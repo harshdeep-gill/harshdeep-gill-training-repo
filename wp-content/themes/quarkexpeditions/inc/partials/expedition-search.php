85d72d5d9e88b6d36e5d7ca9589aa52dcfd82535
Deepak Kumar <deepak.kumar852182@gmail.com>
2024-09-18 14:06:48 +0530

    QE-543 add partial for search result

---
+++ wp-content/themes/quarkexpeditions/inc/partials/expedition-search.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Partial: Expedition Search.
+ *
+ * @package quark
+ */
+
+namespace Quark\Theme\Partials\ExpeditionSearch;
+
+use function Quark\Localization\get_current_currency;
+use function Quark\Search\Departures\search;
+
+const PARTIAL_NAME = 'expedition-search';
+
+/**
+ * Bootstrap.
+ *
+ * @return void
+ */
+function bootstrap(): void {
+	// Hook this partial in.
+	add_filter( 'qrk_get_partial', __NAMESPACE__ . '\\render', 10, 3 );
+}
+
+/**
+ * Render the partial.
+ *
+ * @param mixed[] $output Partial output.
+ * @param string  $name   Partial name.
+ * @param mixed[] $data   Partial data.
+ *
+ * @return mixed[]
+ */
+function render( array $output = [], string $name = '', array $data = [] ): array {
+	// Check for partial name.
+	if ( PARTIAL_NAME !== $name ) {
+		return $output;
+	}
+
+	// Current currency.
+	$currency = get_current_currency();
+
+	// Init selected filters.
+	$selected_filter = [
+		'posts_per_load' => 8,
+	];
+
+	// Verify and get selected filters.
+	if ( ! empty( $data['selectedFilters'] ) && is_array( $data['selectedFilters'] ) ) {
+		$selected_filter = wp_parse_args(
+			$data['selectedFilters'],
+			$selected_filter
+		);
+	}
+
+	// Search for Departure post.
+	$search_results = search( $selected_filter );
+
+	// Build component attributes.
+	$attributes = [
+		'cards' => [], // @todo implement get_expedition_search_cards_data after partial markup is developed - https://tuispecialist.atlassian.net/browse/QE-646
+	];
+
+	// Return rendered partial.
+	return [
+		'markup'          => quark_get_component( 'parts.expedition-search', $attributes ),
+		'noResultsMarkup' => __( 'No results found', 'qrk' ),
+		'data'            => [
+			'resultCount'    => $search_results['result_count'],
+			'page'           => $search_results['current_page'],
+			'nextPage'       => $search_results['next_page'],
+			'remainingCount' => $search_results['remaining_count'],
+		],
+	];
+}
