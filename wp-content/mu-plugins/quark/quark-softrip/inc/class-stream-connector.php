efbcfd678aec541de7cb9a2ece1116565149bb79
Deepak Kumar <deepak.kumar852182@gmail.com>
2024-09-22 23:55:59 +0530

    QE-581 add stream log for update,expired and no update

--- wp-content/mu-plugins/quark/quark-softrip/inc/class-stream-connector.php
+++ wp-content/mu-plugins/quark/quark-softrip/inc/class-stream-connector.php
@@ -29,6 +29,9 @@ class Stream_Connector extends Connector {
 	public $actions = [
 		'quark_softrip_sync_initiated',
 		'quark_softrip_sync_completed',
+		'quark_softrip_sync_departure_updated',
+		'quark_softrip_sync_departure_expired',
+		'quark_softrip_sync_departure_no_updates',
 	];
 
 	/**
@@ -63,6 +66,9 @@ class Stream_Connector extends Connector {
 		return [
 			'sync_initiated' => __( 'Sync Initiated', 'qrk' ),
 			'sync_completed' => __( 'Sync Completed', 'qrk' ),
+			'sync_departure_updated' => __( 'Departure Updated', 'qrk' ),
+			'sync_departure_expired' => __( 'Departure Expired', 'qrk' ),
+			'sync_departure_no_updates' => __( 'Departure No Updates', 'qrk' ),
 		];
 	}
 
@@ -127,6 +133,128 @@ class Stream_Connector extends Connector {
 			absint( wp_unique_id() ),
 			'softrip_sync',
 			'sync_completed'
+		);
+	}
+
+	/**
+	 * Get departure update fields mapping.
+	 *
+	 * @return string[]
+	 */
+	public function get_departure_update_fields_mapping(): array {
+		return [
+			'adventure_options' => __( 'Adventure Options', 'qrk' ),
+			'promotions'        => __( 'Promotions', 'qrk' ),
+			'occupancies'       => __( 'Occupancies', 'qrk' ),
+			'departure_post'    => __( 'Departure Post', 'qrk' ),
+		];
+	}
+
+	/**
+	 * Callback for `quark_softrip_sync_departure_updated` action.
+	 *
+	 * @param mixed[] $data Data passed to the action.
+	 *
+	 * @return void
+	 */
+	public function callback_quark_softrip_sync_departure_updated( array $data = [] ): void {
+		// Validate data.
+		if ( empty( $data ) || empty( $data['post_id'] ) || empty( $data['updated_fields'] ) || empty( $data['softrip_id'] ) ) {
+			return;
+		}
+
+		// Update fields mapping.
+		$fields_mapping = $this->get_departure_update_fields_mapping();
+
+		// Validate what's updated.
+		$updated_field_labels = array_filter(
+			array_map(
+				function ( $field, $is_updated ) use ( $fields_mapping ) {
+					return $is_updated ? $fields_mapping[ $field ] : false;
+				},
+				array_keys( $data['updated_fields'] ),
+				array_values( $data['updated_fields'] )
+			)
+		);
+
+		// Prepare message.
+		$message = sprintf(
+			// translators: %1$s: Softrip ID, %2$s: Updated Fields, %3$d: Post ID.
+			__( 'Departure Updated: %1$s | ID: %3$d | Fields: %2$s', 'qrk' ),
+			strval( $data['softrip_id'] ),
+			implode( ', ', $updated_field_labels ),
+			absint( $data['post_id'] )
+		);
+
+		// Log action.
+		$this->log(
+			$message,
+			[],
+			absint( wp_unique_id() ),
+			'softrip_sync',
+			'sync_departure_updated'
+		);
+	}
+
+	/**
+	 * Callback for `quark_softrip_sync_departure_expired` action.
+	 *
+	 * @param mixed[] $data Data passed to the action.
+	 *
+	 * @return void
+	 */
+	public function callback_quark_softrip_sync_departure_expired( array $data = [] ): void {
+		// Validate data.
+		if ( empty( $data ) || empty( $data['post_id'] ) || empty( $data['softrip_id'] ) ) {
+			return;
+		}
+
+		// Prepare message.
+		$message = sprintf(
+			// translators: %1$s: Softrip ID, %2$d: Post ID.
+			__( 'Departure Expired: %1$s | ID: %2$d', 'qrk' ),
+			strval( $data['softrip_id'] ),
+			absint( $data['post_id'] )
+		);
+
+		// Log action.
+		$this->log(
+			$message,
+			[],
+			absint( wp_unique_id() ),
+			'softrip_sync',
+			'sync_departure_expired'
+		);
+	}
+
+	/**
+	 * Callback for `quark_softrip_sync_departure_no_updates` action.
+	 *
+	 * @param mixed[] $data Data passed to the action.
+	 *
+	 * @return void
+	 */
+	public function callback_quark_softrip_sync_departure_no_updates( array $data = [] ): void {
+		// Validate data.
+		if ( empty( $data ) || empty( $data['post_id'] ) || empty( $data['softrip_id'] ) ) {
+			return;
+		}
+
+		// Prepare message.
+		$message = sprintf(
+			// translators: %1$s: Softrip ID, %2$d: Post ID.
+			__( 'Departure No Updates: %1$s | ID: %2$d', 'qrk' ),
+			strval( $data['softrip_id'] ),
+			absint( $data['post_id'] )
+		);
+
+		// Log action.
+		$this->log(
+			$message,
+			[],
+			absint( wp_unique_id() ),
+			'softrip_sync',
+			'sync_departure_no_updates'
 		);
 	}
 }
