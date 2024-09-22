e3c55ec4b59abd0bb1a5cfb513095131eaebcb5c
Deepak Kumar <deepak.kumar852182@gmail.com>
2024-09-23 00:05:20 +0530

    QE-581 change order of get_initiated_via

--- wp-content/mu-plugins/quark/quark-softrip/inc/namespace.php
+++ wp-content/mu-plugins/quark/quark-softrip/inc/namespace.php
@@ -344,14 +344,14 @@ function do_sync( array $itinerary_post_ids = [], array $specific_departure_post
  * @return string
  */
 function get_initiated_via(): string {
-	// Check if in CLI.
-	if ( defined( 'WP_CLI' ) && true === WP_CLI ) {
-		return 'CLI';
-	}
-
 	// Check if in cron.
 	if ( defined( 'DOING_CRON' ) && true === DOING_CRON ) {
 		return 'cron';
+	}
+
+	// Check if in CLI.
+	if ( defined( 'WP_CLI' ) && true === WP_CLI ) {
+		return 'CLI';
 	}
 
 	// Default to manually.
