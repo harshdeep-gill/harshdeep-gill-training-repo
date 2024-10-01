682673c38254618984e3c2709b08483ba3b0ef3b
Deepak Kumar <deepak.kumar852182@gmail.com>
2024-10-02 00:10:08 +0530

    QE-581 add urgent push bash scripts

---
+++ .bin/urgent-ingestor-push-qa.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x
+set -euo pipefail
+
+# Add SSH Key
+eval $(ssh-agent -s)
+ssh-agent -a /tmp/ssh_agent.sock > /dev/null
+echo "$PANTHEON_SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
+mkdir -p ~/.ssh && echo "StrictHostKeyChecking no" >> ~/.ssh/config
+
+# Run Cron
+terminus wp quark-expeditions.qa -- quark-ingestor push urgent
