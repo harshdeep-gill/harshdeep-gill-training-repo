8dc65dd7add217bf25549becadac82da3ce3b411
BadalChhipa3 <badal.chhipa@travelopia.com>
2024-10-04 17:40:00 +0530

    QE-513 Component Component Form-CCPA-Do-Not-Sell: removed unnecessary code

--- wp-content/themes/quarkexpeditions/src/front-end/components/form-do-not-sell/index.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/form-do-not-sell/index.blade.php
@@ -19,32 +19,30 @@
 >
 	<div class="form-do-not-sell__content">
 		<div class="form-do-not-sell__form">
+			<h3 class="form-do-not-selln__title">Register Your Email Now</h4>
 			<p class="form-do-not-sell__instructions">
-				<x-escape content="Fill the form below, and we will get back to you on the same or next business day." />
+				<x-escape content="In addition to your photographic journal, voyage video, Captain’s log, bios, and daily itineraries you will receive an exciting perk for you or your friends plus other future offers." />
 			</p>
-
-			<div class="form-do-not-sell__fields">
-				<x-form.row>
-					<x-form.field :validation="[ 'required', 'no-empty-spaces' ]">
-						<x-form.input type="text" label="First Name" placeholder="Enter First Name" name="fields[First_Name__c]" />
-					</x-form.field>
-				</x-form.row>
-				<x-form.row>
-					<x-form.field :validation="[ 'required', 'no-empty-spaces' ]">
-						<x-form.input type="text" label="Last Name" placeholder="Enter Last Name" name="fields[Last_Name__c]" />
-					</x-form.field>
-				</x-form.row>
-				<x-form.row>
-					<x-form.field :validation="[ 'required', 'email' ]">
-						<x-form.input type="email" label="Email" placeholder="Enter Email" name="fields[Email_Address__c]" />
-					</x-form.field>
-				</x-form.row>
-				<x-form.row>
-					<x-form.field-group :validation="[ 'checkbox-group-required' ]">
-						<x-form.checkbox name="fields[I_am_a_California_resident__c]" label="I am a California resident." />
-					</x-form.field-group>
-				</x-form.row>
-			</div>
+			<x-form.row>
+				<x-form.field :validation="[ 'required', 'no-empty-spaces' ]">
+					<x-form.input type="text" label="First Name" placeholder="Enter First Name" name="fields[First_Name__c]" />
+				</x-form.field>
+			</x-form.row>
+			<x-form.row>
+				<x-form.field :validation="[ 'required', 'no-empty-spaces' ]">
+					<x-form.input type="text" label="Last Name" placeholder="Enter Last Name" name="fields[Last_Name__c]" />
+				</x-form.field>
+			</x-form.row>
+			<x-form.row>
+				<x-form.field :validation="[ 'required', 'email' ]">
+					<x-form.input type="email" label="Email" placeholder="Enter Email" name="fields[Email_Address__c]" />
+				</x-form.field>
+			</x-form.row>
+			<x-form.row>
+				<x-form.field-group :validation="[ 'checkbox-group-required' ]">
+					<x-form.checkbox name="fields[I_am_a_California_resident__c]" label="I am a California resident." />
+				</x-form.field-group>
+			</x-form.row>
 		</div>
 
 		<x-form.buttons>
