ef3e905a1509879ff901a61d96cc6932d63fa41c
Sagar Jadhav <sagarjadhav27@gmail.com>
2024-10-07 16:30:06 +0530

    QE-326 Component: Currency Switcher - Feedback

--- wp-content/themes/quarkexpeditions/src/front-end/components/currency-switcher/index.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/currency-switcher/index.blade.php
@@ -13,7 +13,8 @@
 <quark-currency-switcher @class( $classes )>
 	<x-form.field>
 		<x-form.select label="{{ __( 'Change Currency', 'qrk' ) }}">
-			<x-form.option value="USD" label="USD" selected="yes">USD</x-form.option>
+			<x-form.option value="test" label="Ask us about our adventure options any dietary requirements specific departure dates in mind">Ask us about our adventure options, any dietary requirements, specific departure dates in mind, etc.</x-form.option>
+			<x-form.option value="USD" label="USD">USD</x-form.option>
 			<x-form.option value="CAD" label="CAD">CAD</x-form.option>
 			<x-form.option value="AUD" label="AUD">AUD</x-form.option>
 			<x-form.option value="GBP" label="GBP">GBP</x-form.option>
