08712eeb6af1da0081814d784a47178b8fc6e78d
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-10-08 17:43:10 +0530

    QE-675 Add image url to destination filter data

--- wp-content/themes/quarkexpeditions/src/front-end/components/expedition-search/sidebar-filters/checkbox.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/expedition-search/sidebar-filters/checkbox.blade.php
@@ -1,9 +1,10 @@
 @props( [
-	'name'   => '',
-	'label'  => '',
-	'value'  => '',
-	'count'  => '0',
-	'parent' => 0,
+	'name'      => '',
+	'label'     => '',
+	'value'     => '',
+	'count'     => '0',
+	'parent'    => 0,
+	'image_url' => '',
 ] )
 
 @php
@@ -15,7 +16,7 @@
 @endphp
 
 @if ( empty( $parent ) )
-	<x-form.checkbox :name="$name" :label="$label" :value="$value" data-label="{{ $data_label }}" />
+	<x-form.checkbox :name="$name" :label="$label" :value="$value" data-label="{{ $data_label }}" data-image-url="{!! esc_url( $image_url ) !!}" />
 @else
-	<x-form.checkbox :name="$name" :label="$label" :value="$value" data-label="{{ $data_label }}" data-parent="{{ $parent }}" />
+	<x-form.checkbox :name="$name" :label="$label" :value="$value" data-label="{{ $data_label }}" data-parent="{{ $parent }}" data-image-url="{!! esc_url( $image_url ) !!}" />
 @endif
--- wp-content/themes/quarkexpeditions/src/front-end/components/expedition-search/sidebar-filters/content/expedition/destinations.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/expedition-search/sidebar-filters/content/expedition/destinations.blade.php
@@ -20,18 +20,64 @@
 						@continue
 					@endif
 
+					@php
+						$image_args = [
+							'size' => [
+								'height' => 72,
+								'width'  => 72,
+							],
+							'transform' => [
+								'crop'    => 'fill',
+								'gravity' => 'auto',
+								'quality' => 90,
+							]
+						];
+
+						$image_url = '';
+
+						if ( ! empty( $destination['image_id'] ) ) {
+							$image_url = quark_dynamic_image_url( array_merge( [ 'id' => $destination[ 'image_id' ] ], $image_args ) );
+						}
+					@endphp
+
 					@if ( empty( $destination['children'] ) || ! is_array( $destination['children'] ) )
-						<x-expedition-search.sidebar-filters.checkbox name="destinations" :label="$destination['label']" :value="$destination['value']" :count="$destination['count']" />
+						<x-expedition-search.sidebar-filters.checkbox
+							name="destinations"
+							:label="$destination['label']"
+							:value="$destination['value']"
+							:count="$destination['count']"
+							:image_url="$image_url"
+						/>
 					@else
 						<div class="expedition-search__sidebar-filters-with-children">
-							<x-expedition-search.sidebar-filters.checkbox name="destinations" :label="$destination['label']" :value="$destination['value']" :count="$destination['count']" />
-
+							<x-expedition-search.sidebar-filters.checkbox
+								name="destinations"
+								:label="$destination['label']"
+								:value="$destination['value']"
+								:count="$destination['count']"
+								:image_url="$image_url"
+							/>
 							<x-form.field-group class="expedition-search__sidebar-filters-children">
 								@foreach ( $destination['children'] as $child )
 									@if ( empty( $child['label'] ) || empty( $child['value'] ) || ! isset( $child['count'] ) || empty( $child['parent_id'] ) )
 										@continue
 									@endif
-									<x-expedition-search.sidebar-filters.checkbox name="destinations" :label="$child['label']" :value="$child['value']" :count="$child['count']" :parent="$child['parent_id']" />
+									@php
+										$child_image_url = '';
+
+										if ( ! empty( $child['image_id'] ) ) {
+											$child_image_url = quark_dynamic_image_url( array_merge( [ 'id' => $child['image_id'] ], $image_args ) );
+										}
+									@endphp
+									<x-expedition-search.sidebar-filters.checkbox
+										name="destinations"
+										:label="$child['label']"
+										:value="$child['value']"
+										:count="$child['count']"
+										:parent="$child['parent_id']"
+										:image_url="$child_image_url"
+									/>
+
 								@endforeach
 							</x-form.field-group>
 						</div>
