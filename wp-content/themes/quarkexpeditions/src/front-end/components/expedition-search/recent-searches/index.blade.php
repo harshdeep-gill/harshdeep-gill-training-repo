ba349c1dae2d453de5e25d9b8bf0535966c46b4f
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-10-08 18:24:23 +0530

    QE-675 Add template to recent searches

--- wp-content/themes/quarkexpeditions/src/front-end/components/expedition-search/recent-searches/index.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/expedition-search/recent-searches/index.blade.php
@@ -5,14 +5,15 @@
 <div class="expedition-seach__recent-searches">
 	<h4 class="h4"><x-escape :content="$title" /></h4>
 	<temlate>
-		<x-mini-cards-list>
-			<x-mini-cards-list.card>
-				<x-mini-cards-list.card-image image_id="120" />
-				<x-mini-cards-list.card-info>
-					<x-mini-cards-list.card-title title="Antarctic Peninsula" />
-					<x-mini-cards-list.card-date date="June 2024" />
-				</x-mini-cards-list.card-info>
-			</x-mini-cards-list.card>
-		</x-mini-cards-list>
+		<x-mini-cards-list.card>
+			<x-mini-cards-list.card-image image_id="120" />
+			<x-mini-cards-list.card-info>
+				<x-mini-cards-list.card-title title="Antarctic Peninsula" />
+				<x-mini-cards-list.card-date date="June 2024" />
+			</x-mini-cards-list.card-info>
+		</x-mini-cards-list.card>
 	</temlate>
+
+	<x-mini-cards-list>
+	</x-mini-cards-list>
 </div>
