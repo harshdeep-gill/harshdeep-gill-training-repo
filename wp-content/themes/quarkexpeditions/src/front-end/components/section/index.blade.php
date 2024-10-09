9fc362105d8bda8d1e99e5bffa001721df271db1
BadalChhipa3 <badal.chhipa@travelopia.com>
2024-10-09 14:38:23 +0530

    QE-464 Component Section Background-Image: Hotfix

--- wp-content/themes/quarkexpeditions/src/front-end/components/section/index.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/section/index.blade.php
@@ -8,6 +8,7 @@
 	'background_color' => 'gray',
 	'padding'          => false,
 	'wrap'             => false,
+	'image_id'         => 0,
 ] )
 
 @php
@@ -43,6 +44,10 @@
 				$classes[] = sprintf( 'section--has-background-%s', $background_color );
 			}
 		}
+	}
+
+	if ( ! empty( $image_id ) ) {
+		$classes[] = 'full-width';
 	}
 
 	if ( ! empty( $padding ) && true === boolval( $padding ) ) {
