45e624dbf1c53254fd24656efd51cd92766b79f5
Deepak Kumar <deepak.kumar852182@gmail.com>
2024-10-09 13:28:24 +0530

    QE-748 add workflow for production

---
+++ .bin/ingestor-urgent-push-production.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x
+set -euo pipefail
+
+# Add SSH Key
+eval $(ssh-agent -s)
+ssh-agent -a /tmp/ssh_agent.sock > /dev/null
+echo "$PANTHEON_SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
+mkdir -p ~/.ssh && echo "StrictHostKeyChecking no" >> ~/.ssh/config
+
+# Run Command
+terminus wp quark-expeditions.live -- quark-ingestor push urgent
