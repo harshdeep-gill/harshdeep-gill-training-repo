0b34229edd4e0567eaf2df8ec738a58eb54af578
Jignesh <jignesh.nakrani@rtcamp.com>
2024-10-09 16:05:04 +0530

    QE-765 SEO: Remove use of header tags in header block

--- wp-content/themes/quarkexpeditions/src/editor/blocks/featured-media-accordions/children/item/edit.tsx
+++ wp-content/themes/quarkexpeditions/src/editor/blocks/featured-media-accordions/children/item/edit.tsx
@@ -80,7 +80,7 @@ export default function Edit( { className, attributes, setAttributes }: BlockEdi
 				<div className="accordion__handle">
 					<button className="accordion__handle-btn">
 						<RichText
-							tagName="h3"
+							tagName="p"
 							className="h5 accordion__handle-btn-text body-text-large"
 							placeholder={ __( 'Title here…', 'qrk' ) }
 							value={ attributes.title }
