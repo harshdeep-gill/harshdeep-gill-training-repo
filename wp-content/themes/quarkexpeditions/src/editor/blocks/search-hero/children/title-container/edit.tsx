a8f3a4859006d528e57e5636c3645e787f617cf0
Takshil Kunadia <takshilkunadia@gmail.com>
2024-10-14 20:01:11 +0530

    QE-487 Add child block - title container

---
+++ wp-content/themes/quarkexpeditions/src/editor/blocks/search-hero/children/title-container/edit.tsx
@@ -0,0 +1,45 @@
+/**
+ * WordPress dependencies.
+ */
+import { __ } from '@wordpress/i18n';
+import {
+	useBlockProps,
+	useInnerBlocksProps,
+} from '@wordpress/block-editor';
+
+/**
+ * External dependencies.
+ */
+import classnames from 'classnames';
+
+/**
+ * Child Blocks.
+ */
+import * as title from '../title';
+import * as overline from '../overline';
+import * as subtitle from '../subtitle';
+
+/**
+ * Edit Component.
+ *
+ * @param {Object} props           Component properties.
+ * @param {string} props.className Class name.
+ */
+export default function Edit( { className }: BlockEditAttributes ): JSX.Element {
+	// Set block props.
+	const blockProps = useBlockProps( {
+		className: classnames( className, 'search-hero__title-container' ),
+	} );
+
+	// Set inner block props.
+	const innerBlockProps = useInnerBlocksProps(
+		{ ...blockProps },
+		{
+			allowedBlocks: [ title.name, overline.name, subtitle.name ],
+			template: [ [ title.name ], [ overline.name ], [ subtitle.name ] ],
+		}
+	);
+
+	// Return the block's markup.
+	return <div { ...innerBlockProps } />;
+}
