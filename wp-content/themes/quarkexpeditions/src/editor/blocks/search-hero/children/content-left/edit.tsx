9b4f506cc0d0ff2ce305c933c28f12b2c3d27dd0
Takshil Kunadia <takshilkunadia@gmail.com>
2024-10-14 20:01:48 +0530

    QE-487 Add child block - content left

---
+++ wp-content/themes/quarkexpeditions/src/editor/blocks/search-hero/children/content-left/edit.tsx
@@ -0,0 +1,49 @@
+/**
+ * WordPress dependencies.
+ */
+import { __ } from '@wordpress/i18n';
+import {
+	useBlockProps,
+	useInnerBlocksProps,
+} from '@wordpress/block-editor';
+
+/**
+ * External dependencies.
+ */
+import classnames from 'classnames';
+
+/**
+ * Internal dependencies.
+ */
+import * as thumbnailCards from '../../../thumbnail-cards';
+
+/**
+ * Child Blocks.
+ */
+import * as titleContainer from '../title-container';
+import * as searchBar from '../search-bar';
+
+/**
+ * Edit Component.
+ *
+ * @param {Object} props           Component properties.
+ * @param {string} props.className Class name.
+ */
+export default function Edit( { className }: BlockEditAttributes ): JSX.Element {
+	// Set block props.
+	const blockProps = useBlockProps( {
+		className: classnames( className, 'search-hero__left' ),
+	} );
+
+	// Set inner block props.
+	const innerBlockProps = useInnerBlocksProps(
+		{ ...blockProps },
+		{
+			allowedBlocks: [ titleContainer.name, searchBar.name, thumbnailCards.name ],
+			template: [ [ titleContainer.name ], [ searchBar.name ], [ thumbnailCards.name ] ],
+		}
+	);
+
+	// Return the block's markup.
+	return <div { ...innerBlockProps } />;
+}
