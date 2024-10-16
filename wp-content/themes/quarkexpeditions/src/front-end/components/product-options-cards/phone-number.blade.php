4667479b7ce5c6dd5a2426a997c0096320dfc0c3
dishitpala <dishitpala@gmail.com>
2024-10-17 03:16:28 +0530

    QE-800 Update phone-number component

--- wp-content/themes/quarkexpeditions/src/front-end/components/product-options-cards/phone-number.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/product-options-cards/phone-number.blade.php
@@ -1,9 +1,14 @@
 @props( [
-	'phone_number' => '',
+	'phone_number' => quark_get_template_data( 'dynamic_phone_number', [] )['default_phone_number'] ?? '',
 	'text'         => '',
 ] )
 
 @php
+	if ( empty( $text ) ) {
+		$text = __( 'Give us a Call' );
+		$text = ! empty( $phone_number ) ? sprintf( '%s: %s', $text, $phone_number ) : $text;
+	}
+
 	if ( empty( $phone_number ) || empty( $text ) ) {
 		return;
 	}
