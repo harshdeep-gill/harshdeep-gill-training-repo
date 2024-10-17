06b64d85f6bf2826758458f070e518ae62043acc
BadalChhipa3 <badal.chhipa@travelopia.com>
2024-10-17 18:38:20 +0530

    QE-835 Component Product-options-Cards: show pricing functionality updated

--- wp-content/themes/quarkexpeditions/src/front-end/components/product-options-cards/price.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/product-options-cards/price.blade.php
@@ -14,18 +14,41 @@
 	if ( ! empty( $class ) ) {
 		$classes[] = $class;
 	}
+
+	// Initialize a new variable to test conditions.
+	$is_discounted = false;
+
+	// Checking if the discounted price is different from the original or not.
+	if ( ! empty( $discounted_price ) ) {
+		if ( $discounted_price !== $original_price ) {
+			$is_discounted = true;
+		} else {
+			$is_discounted = false;
+		}
+	}
 @endphp
 
 <div @class( $classes )>
-	<p>{{ __( 'From', 'qrk' ) }}</p>
-	<div class="product-options-cards__price-info">
-		<div class="product-options-cards__price-discounted">
+	<p class="product-options-cards__price-title" discounted="{{ $is_discounted }}">{{ __( 'From', 'qrk' ) }}</p>
+
+	@if ( ! empty( $is_discounted ) )
+			<div class="product-options-cards__price-info">
+				<div class="product-options-cards__price-discounted">
+					<h5>
+						<x-escape :content="$discounted_price" />
+					</h5>{{ __( 'per person', 'qrk' ) }}
+				</div>
+				<div class="product-options-cards__price-original">
+					<x-escape :content="$original_price" />
+				</div>
+			</div>
+	@endif
+
+	@if ( empty( $is_discounted ) )
+		<div class="product-options-cards__price">
 			<h5>
-				<x-escape :content="$discounted_price" />
+				<x-escape :content="$original_price" />
 			</h5>{{ __( 'per person', 'qrk' ) }}
 		</div>
-		<div class="product-options-cards__price-original">
-			<x-escape :content="$original_price" />
-		</div>
-	</div>
+	@endif
 </div>
