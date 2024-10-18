0c216e92459f55f33d13dd6b738849093f9ac2df
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-10-18 20:16:45 +0530

    QE-842 Populate GA client id in forms

--- wp-content/themes/quarkexpeditions/src/front-end/components/form/tracking.ts
+++ wp-content/themes/quarkexpeditions/src/front-end/components/form/tracking.ts
@@ -3,7 +3,7 @@
  */
 
 // Campaign params.
-export const campaignParams: string[] = [ 'utm_source', 'utm_term', 'utm_campaign', 'utm_keyword', 'utm_content', 'utm_theme', 'utm_medium', 'gclid', 'msclkid', 'fbid', 'fbclid', 'ga_client_id' ];
+export const campaignParams: string[] = [ 'utm_source', 'utm_term', 'utm_campaign', 'utm_keyword', 'utm_content', 'utm_theme', 'utm_medium', 'gclid', 'msclkid', 'fbid', 'fbclid' ];
 
 /**
  * Get campaign params from URL.
@@ -30,14 +30,39 @@ export const getCampaignParamsFromUrl = (): any => {
 };
 
 /**
+ * Get _ga cookie and return the value.
+ *
+ * @return {string} _ga cookie value.
+ */
+export const getGaCookie = (): string => {
+	// Get the cookie.
+	let gaCookie: string = document.cookie.split( '; ' ).find( ( cookie ) => cookie.startsWith( '_ga=GA1.1.' ) ) ?? '';
+
+	// Check if we have a cookie.
+	if ( gaCookie ) {
+		// Split the cookie.
+		const splitCookie = gaCookie.split( '_ga=GA1.1.' );
+
+		// Check if we got two values
+		if ( splitCookie.length === 2 ) {
+			gaCookie = splitCookie[ 1 ];
+		}
+	}
+
+	// Return the cookie.
+	return gaCookie;
+};
+
+/**
  * Initialize tracking.
  */
 export const initializeTracking = (): void => {
 	// Get URL campaign params.
 	const urlCampaignParams = getCampaignParamsFromUrl();
+	const gaCookie = getGaCookie();
 
 	// Trigger an event.
-	window.dispatchEvent( new CustomEvent( 'visitor-tracked', { detail: urlCampaignParams } ) );
+	window.dispatchEvent( new CustomEvent( 'visitor-tracked', { detail: { urlCampaignParams, gaCookie } } ) );
 };
 
 // Track when DOM is ready.
