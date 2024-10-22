678326700078124cf3cafed15b56bebad4eb18d3
BadalChhipa3 <badal.chhipa@travelopia.com>
2024-10-22 17:28:14 +0530

    QE-896 Component Section: updated narrow attribute in block

--- wp-content/themes/quarkexpeditions/src/editor/blocks/section/index.php
+++ wp-content/themes/quarkexpeditions/src/editor/blocks/section/index.php
@@ -45,7 +45,7 @@ function render( array $attributes = [], string $content = '' ): string {
 		'gradient_position' => $attributes['gradientPosition'],
 		'gradient_color'    => $attributes['gradientColor'],
 		'padding'           => $attributes['hasPadding'],
-		'narrow'            => $attributes['isNarrow'],
+		'narrow'            => ! $attributes['backgroundImage'] ? $attributes['isNarrow'] : '',
 		'slot'              => $content,
 		'heading_link'      => [],
 		'has_heading_link'  => $attributes['hasHeadingLink'],
