57f47157d8bc62f02e77106d3a6debaaa32fb1c6
Takshil Kunadia <takshilkunadia@gmail.com>
2024-10-24 17:24:36 +0530

    QE-936 Enable Pagination context

--- wp-content/themes/quarkexpeditions/src/editor/blocks/staff-members/index.php
+++ wp-content/themes/quarkexpeditions/src/editor/blocks/staff-members/index.php
@@ -58,7 +58,6 @@ function render( array $attributes = [] ): string {
 		'post_status'            => 'publish',
 		'fields'                 => 'ids',
 		'posts_per_page'         => $attributes['totalPosts'],
-		'no_found_rows'          => true,
 		'update_post_meta_cache' => false,
 		'update_post_term_cache' => false,
 		'orderby'                => 'date',
