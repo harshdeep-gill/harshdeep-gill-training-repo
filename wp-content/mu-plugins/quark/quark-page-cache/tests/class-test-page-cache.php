2a79215cece4c92c8f66a836dd6ad697e545f0a7
Deepak Kumar <deepak.kumar852182@gmail.com>
2024-10-28 15:02:47 +0530

    QE-1020 fix phpcs errors

--- wp-content/mu-plugins/quark/quark-page-cache/tests/class-test-page-cache.php
+++ wp-content/mu-plugins/quark/quark-page-cache/tests/class-test-page-cache.php
@@ -16,58 +16,60 @@ use function Quark\PageCache\flush_and_warm_up_page_cache;
  */
 class Test_Page_Cache extends WP_UnitTestCase {
 
-    /**
-     * Time took.
-     *
-     * @var mixed
-     */
-    private $time_took;
+	/**
+	 * Time took.
+	 *
+	 * @var mixed
+	 */
+	private $time_took;
 
-    /**
-     * Test flush_and_warm_up_page_cache function.
-     *
-     * @covers \Quark\PageCache\flush_and_warm_up_page_cache
-     *
-     * @return void
-     */
-    public function test_flush_and_warm_up_page_cache() {
-        // Attach the hook.
-        add_action( 'quark_page_cache_flushed', [ $this, 'quark_page_cache_flushed' ] );
+	/**
+	 * Test flush_and_warm_up_page_cache function.
+	 *
+	 * @covers \Quark\PageCache\flush_and_warm_up_page_cache
+	 *
+	 * @return void
+	 */
+	public function test_flush_and_warm_up_page_cache(): void {
+		// Attach the hook.
+		add_action( 'quark_page_cache_flushed', [ $this, 'quark_page_cache_flushed' ] );
 
-        // Time took should be empty.
-        $this->assertEmpty( $this->time_took );
+		// Time took should be empty.
+		$this->assertEmpty( $this->time_took );
 
-        // Test that the action is not fired before the function is called.
-        $this->assertEmpty( did_action('quark_page_cache_flushed') );
-        $this->assertEmpty( $this->time_took );
+		// Test that the action is not fired before the function is called.
+		$this->assertEmpty( did_action( 'quark_page_cache_flushed' ) );
+		$this->assertEmpty( $this->time_took );
 
-        // Call the function.
-        flush_and_warm_up_page_cache();
+		// Call the function.
+		flush_and_warm_up_page_cache();
 
-        // Test that the action is fired after the function is called.
-        $this->assertNotEmpty( did_action('quark_page_cache_flushed') );
-        $this->assertNotEmpty( $this->time_took );
-        $this->assertIsScalar( $this->time_took );
-        $this->assertIsFloat( $this->time_took );
+		// Test that the action is fired after the function is called.
+		$this->assertNotEmpty( did_action( 'quark_page_cache_flushed' ) );
+		$this->assertNotEmpty( $this->time_took );
+		$this->assertIsScalar( $this->time_took );
+		$this->assertIsFloat( $this->time_took );
 
-        // Reset the hook.
-        remove_action( 'quark_page_cache_flushed', [ $this, 'quark_page_cache_flushed' ] );
+		// Reset the hook.
+		remove_action( 'quark_page_cache_flushed', [ $this, 'quark_page_cache_flushed' ] );
 
-        // Reset variable.
-        $this->time_took = null;
-    }
+		// Reset variable.
+		$this->time_took = null;
+	}
 
-    /**
-     * Hook on `quark_page_cache_flushed`.
-     *
-     * @param mixed[] $data Data.
-     *
-     * @return void
-     */
-    public function quark_page_cache_flushed( array $data = [] ) {
-        $this->assertNotEmpty( $data );
-        $this->assertArrayHasKey( 'time_took', $data );
+	/**
+	 * Hook on `quark_page_cache_flushed`.
+	 *
+	 * @param mixed[] $data Data.
+	 *
+	 * @return void
+	 */
+	public function quark_page_cache_flushed( array $data = [] ): void {
+		// Validate.
+		$this->assertNotEmpty( $data );
+		$this->assertArrayHasKey( 'time_took', $data );
 
-        $this->time_took = $data['time_took'];
-    }
+		// Set the time took.
+		$this->time_took = $data['time_took'];
+	}
 }
