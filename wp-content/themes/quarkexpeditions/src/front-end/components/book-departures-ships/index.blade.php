785f8a9adc8155f3669c2263d4429f039a9b6e39
Sagar Jadhav <sagarjadhav27@gmail.com>
2024-10-29 11:59:14 +0530

    QE-1031 Cabins Options broken on Ship Pages

--- wp-content/themes/quarkexpeditions/src/front-end/components/book-departures-ships/index.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/book-departures-ships/index.blade.php
@@ -2,6 +2,8 @@
 	if ( empty( $slot ) ) {
 		return;
 	}
+
+	quark_component_enqueue_assets( 'product-options-cards' );
 @endphp
 
 <quark-book-departures-ships class="book-departures-ships" loading="false">
