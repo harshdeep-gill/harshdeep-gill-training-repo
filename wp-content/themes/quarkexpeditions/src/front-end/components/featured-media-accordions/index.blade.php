f9750df444270e9282179ebc7568415eeaaeedd9
Junaid Bhura <junaid.bhura@travelopia.com>
2024-10-29 11:41:51 +1100

    QE-1028 fix featured media accordions spacing

--- wp-content/themes/quarkexpeditions/src/front-end/components/featured-media-accordions/index.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/featured-media-accordions/index.blade.php
@@ -10,8 +10,10 @@
 	}
 @endphp
 
-<quark-featured-media-accordions @class( $classes )>
-	<x-two-columns>
-		{!! $slot !!}
-	</x-two-columns>
-</quark-featured-media-accordions>
+<x-section>
+	<quark-featured-media-accordions @class( $classes )>
+		<x-two-columns>
+			{!! $slot !!}
+		</x-two-columns>
+	</quark-featured-media-accordions>
+</x-section>
