4944ea5d94ce954874398dafe0dc93b5d18cfa7f
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-12-26 15:14:28 +0530

    QE-1192 Add font prop to hero and search hero component subtitle and description components

--- wp-content/themes/quarkexpeditions/src/front-end/components/search-hero/sub-title.blade.php
+++ wp-content/themes/quarkexpeditions/src/front-end/components/search-hero/sub-title.blade.php
@@ -1,6 +1,7 @@
 @props( [
-	'title'      => '',
-	'text_color' => '',
+	'title'          => '',
+	'text_color'     => '',
+	'use_promo_font' => false,
 ] )
 
 @php
@@ -12,6 +13,10 @@
 
 	if ( ! empty( $text_color ) && 'white' === $text_color ) {
 		$classes[] = 'color-context--dark';
+	}
+
+	if ( ! empty( $use_promo_font ) ) {
+		$classes[] = 'font-family--promo';
 	}
 @endphp
 
