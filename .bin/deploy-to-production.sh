8e815687eb2c62bfcd8f509ee87ba7c74bffbf02
Deepak Kumar <deepak.kumar852182@gmail.com>
2025-01-30 15:33:16 +0530

    QE-1278 update env in deployment

--- .bin/deploy-to-production.sh
+++ .bin/deploy-to-production.sh
@@ -11,7 +11,7 @@ mkdir -p ~/.ssh && touch ~/.ssh/config
 grep -qxF 'StrictHostKeyChecking no' ~/.ssh/config || echo "StrictHostKeyChecking no" >> ~/.ssh/config
 
 # Deploy to test, backup live and deploy to live -- note:  because -e is set, if any of these commands fail, the script will exit, which we ideally expect.
-terminus env:deploy --no-interaction -- quark-expeditions.dev
+terminus env:deploy --no-interaction -- quark-expeditions.test
 terminus backup:create --element all --no-interaction -- quark-expeditions.live
 terminus env:deploy --no-interaction -- quark-expeditions.live
 
