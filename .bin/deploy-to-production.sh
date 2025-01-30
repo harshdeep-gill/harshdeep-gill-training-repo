0b7e15460a074a4cb397f594302d8307a7384669
Prithwiraj Ghosh <prithwiraj10@gmail.com>
2025-01-30 11:16:37 +0530

    QE-1278 update: github deploy workflow

---
+++ .bin/deploy-to-production.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+SITE_NAME=${1:-'qa'}
+
+# Add SSH Key
+eval $(ssh-agent -s)
+rm -rf /tmp/ssh_agent.sock
+ssh-agent -a /tmp/ssh_agent.sock > /dev/null
+echo "$PANTHEON_SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
+mkdir -p ~/.ssh && touch ~/.ssh/config
+grep -qxF 'StrictHostKeyChecking no' ~/.ssh/config || echo "StrictHostKeyChecking no" >> ~/.ssh/config
+
+# Deploy to test, backup live and deploy to live -- note:  because -e is set, if any of these commands fail, the script will exit, which we ideally expect.
+terminus env:deploy --no-interaction -- quark-expeditions.dev
+terminus backup:create --element all --no-interaction -- quarkexpeditions.live
+terminus env:deploy --no-interaction -- quarkexpeditions.live
+
+set +x
