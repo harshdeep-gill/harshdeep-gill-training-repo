0d83e4e65e172668a7fa1a7fbc970d398631075a
Abhishek Singh <abhisheksinghxix@gmail.com>
2025-01-31 14:18:40 +0530

    QE-1275 Remove redundant function

--- wp-content/themes/quarkexpeditions/src/front-end/components/site-banner/index.ts
+++ wp-content/themes/quarkexpeditions/src/front-end/components/site-banner/index.ts
@@ -17,18 +17,6 @@ export default class QuarkSiteBannerElement extends HTMLElement {
 		// Setup banner height property.
 		document.querySelector<HTMLElement>( ':root' )?.style.setProperty( '--site-banner-height', this.clientHeight + 'px' );
 	}
-
-	/**
-	 * Event: Body Scroll.
-	 */
-	onBodyScroll() : void {
-		// Check if entries exist.
-		if ( 50 < document.body.scrollTop ) {
-			this.classList.add( 'lp-header--compact' );
-		} else if ( ! document.body.scrollTop ) {
-			this.classList.remove( 'lp-header--compact' );
-		}
-	}
 }
 
 /**
